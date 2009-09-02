kiddreads/Avalon

commit 1a8f3d5b3763edb6e080b6b44cb236cada4f774d

Debugger for DX9 plugin.
Debugger panel is displayed only if Dolphin is started with /d option.
Dump features are not implemented.
Pause/Resume buttons works, only if the plugin is built in DEBUG or DEBUGFAST configuration, not in RELEASE. These features are really only for devs, not for regular gamers.

You will be able to pause frame by frame, or by n frames, or by n primitive flushes. Other pausing options are not implemented yet.

When other pausing and dumping features are implemented, debugging will be much easier.

I have changed the DX9 project setting to use unicode character set in order to use wxWidge tools. If this causes Dolphin building problems for you, check your project setting.txt




git-svn-id: https://dolphin-emu.googlecode.com/svn/trunk@4154 8ced0084-cf51-0410-be5f-012b33b47a6e

iCube/Source/Plugins/Plugin_VideoDX9/Plugin_VideoDX9.vcproj
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="Windows-1252"?>
 <VisualStudioProject
 	ProjectType="Visual C++"
-	Version="9,00"
+	Version="9.00"
 	Name="Plugin_VideoDX9"
 	ProjectGUID="{636FAD5F-02D1-4E9A-BE67-FB8EA99B9A18}"
 	RootNamespace="Plugin_VideoDX9"
@@ -26,7 +26,7 @@
 			InheritedPropertySheets="$(VCInstallDir)VCProjectDefaults\UpgradeFromVC71.vsprops"
 			UseOfMFC="0"
 			ATLMinimizesCRunTimeLibraryUsage="false"
-			CharacterSet="2"
+			CharacterSet="1"
 			WholeProgramOptimization="1"
 			>
 			<Tool
@@ -58,7 +58,7 @@
 				FavorSizeOrSpeed="1"
 				OmitFramePointers="false"
 				WholeProgramOptimization="true"
-				AdditionalIncludeDirectories="../../PluginSpecs;../../Core/Common/Src;../../Core/VideoCommon/Src;../../../Externals"
+				AdditionalIncludeDirectories="../../PluginSpecs;../../Core/Common/Src;../../Core/VideoCommon/Src;../../../Externals;..\..\..\Externals\wxWidgets\Include;..\..\..\Externals\wxWidgets\Include\msvc"
 				PreprocessorDefinitions="_WIN32;WIN32;NDEBUG;_WINDOWS;_USRDLL;VIDEO_DIRECTX9_EXPORTS;_CRT_SECURE_NO_DEPRECATE;_SECURE_SCL=0"
 				StringPooling="true"
 				RuntimeLibrary="0"
@@ -92,11 +92,11 @@
 			<Tool
 				Name="VCLinkerTool"
 				AdditionalOptions="&quot;..\..\core\common\win32\release\common.lib&quot;"
-				AdditionalDependencies="dsound.lib odbc32.lib odbccp32.lib comctl32.lib d3d9.lib d3dx9.lib winmm.lib vfw32.lib"
+				AdditionalDependencies="dsound.lib odbc32.lib odbccp32.lib comctl32.lib d3d9.lib d3dx9.lib winmm.lib cg.lib cgD3D9.lib vfw32.lib wxbase28u.lib wxmsw28u_core.lib"
 				OutputFile="..\..\..\Binary\Win32\Plugins\Plugin_VideoDX9.dll"
 				LinkIncremental="1"
 				SuppressStartupBanner="true"
-				AdditionalLibraryDirectories="../../../Externals/Cg"
+				AdditionalLibraryDirectories="../../../Externals/Cg;&quot;..\..\..\Externals\wxWidgets\lib\vc_lib\$(PlatformName)&quot;"
 				GenerateManifest="false"
 				ProgramDatabaseFile="$(PlatformName)\$(ConfigurationName)\$(TargetName).pdb"
 				RandomizedBaseAddress="1"
@@ -134,7 +134,7 @@
 			InheritedPropertySheets="$(VCInstallDir)VCProjectDefaults\UpgradeFromVC71.vsprops"
 			UseOfMFC="0"
 			ATLMinimizesCRunTimeLibraryUsage="false"
-			CharacterSet="2"
+			CharacterSet="1"
 			WholeProgramOptimization="1"
 			>
 			<Tool
@@ -160,13 +160,14 @@
 			/>
 			<Tool
 				Name="VCCLCompilerTool"
+				AdditionalOptions="/MP"
 				Optimization="3"
 				InlineFunctionExpansion="2"
 				EnableIntrinsicFunctions="true"
 				FavorSizeOrSpeed="1"
 				OmitFramePointers="false"
 				WholeProgramOptimization="true"
-				AdditionalIncludeDirectories="../../../Externals;../../PluginSpecs;../../Core/Common/Src;../../Core/VideoCommon/Src"
+				AdditionalIncludeDirectories="../../../Externals;../../PluginSpecs;../../Core/Common/Src;../../Core/VideoCommon/Src;..\..\..\Externals\wxWidgets\Include;..\..\..\Externals\wxWidgets\Include\msvc"
 				PreprocessorDefinitions="WIN32;NDEBUG;_WINDOWS;_USRDLL;VIDEO_DIRECTX9_EXPORTS;_CRT_SECURE_NO_DEPRECATE;_SECURE_SCL=0"
 				StringPooling="true"
 				ExceptionHandling="1"
@@ -200,11 +201,11 @@
 			/>
 			<Tool
 				Name="VCLinkerTool"
-				AdditionalDependencies="dsound.lib odbc32.lib odbccp32.lib comctl32.lib d3d9.lib d3dx9.lib winmm.lib vfw32.lib"
+				AdditionalDependencies="cg.lib cgD3D9.lib dsound.lib odbc32.lib odbccp32.lib comctl32.lib d3d9.lib d3dx9.lib winmm.lib vfw32.lib rpcrt4.lib wxbase28u.lib wxmsw28u_core.lib"
 				OutputFile="..\..\..\Binary\x64\Plugins\Plugin_VideoDX9.dll"
 				LinkIncremental="1"
 				SuppressStartupBanner="true"
-				AdditionalLibraryDirectories=""
+				AdditionalLibraryDirectories="..\..\..\Externals\Cg64;&quot;..\..\..\Externals\wxWidgets\lib\vc_lib\$(PlatformName)&quot;"
 				GenerateManifest="false"
 				GenerateDebugInformation="true"
 				ProgramDatabaseFile="$(PlatformName)\$(ConfigurationName)\$(TargetName).pdb"
@@ -243,7 +244,7 @@
 			InheritedPropertySheets="$(VCInstallDir)VCProjectDefaults\UpgradeFromVC71.vsprops"
 			UseOfMFC="0"
 			ATLMinimizesCRunTimeLibraryUsage="false"
-			CharacterSet="2"
+			CharacterSet="1"
 			>
 			<Tool
 				Name="VCPreBuildEventTool"
@@ -269,7 +270,7 @@
 			<Tool
 				Name="VCCLCompilerTool"
 				Optimization="0"
-				AdditionalIncludeDirectories="../../PluginSpecs;../../Core/Common/Src;../../Core/VideoCommon/Src;../../../Externals"
+				AdditionalIncludeDirectories="../../PluginSpecs;../../Core/Common/Src;../../Core/VideoCommon/Src;../../../Externals;..\..\..\Externals\wxWidgets\Include;..\..\..\Externals\wxWidgets\Include\msvc"
 				PreprocessorDefinitions="_WIN32;WIN32;_DEBUG;_WINDOWS;_USRDLL;VIDEO_DIRECTX9_EXPORTS;_CRT_SECURE_NO_DEPRECATE;_SECURE_SCL=0"
 				BasicRuntimeChecks="3"
 				RuntimeLibrary="1"
@@ -299,11 +300,11 @@
 			<Tool
 				Name="VCLinkerTool"
 				AdditionalOptions="&quot;..\..\core\common\win32\debug\common.lib&quot;"
-				AdditionalDependencies="dsound.lib odbc32.lib odbccp32.lib comctl32.lib d3d9.lib d3dx9.lib winmm.lib vfw32.lib"
+				AdditionalDependencies="dsound.lib odbc32.lib odbccp32.lib comctl32.lib d3d9.lib d3dx9.lib winmm.lib cg.lib cgD3D9.lib vfw32.lib rpcrt4.lib wxbase28ud.lib wxmsw28ud_core.lib"
 				OutputFile="..\..\..\Binary\Win32/Plugins\Plugin_VideoDX9D.dll"
 				LinkIncremental="1"
 				SuppressStartupBanner="true"
-				AdditionalLibraryDirectories="../../../Externals/Cg"
+				AdditionalLibraryDirectories="../../../Externals/Cg;&quot;..\..\..\Externals\wxWidgets\lib\vc_lib\$(PlatformName)&quot;"
 				GenerateManifest="false"
 				GenerateDebugInformation="true"
 				ProgramDatabaseFile="$(PlatformName)\$(ConfigurationName)\$(TargetName).pdb"
@@ -397,11 +398,11 @@
 			/>
 			<Tool
 				Name="VCLinkerTool"
-				AdditionalDependencies="dsound.lib odbc32.lib odbccp32.lib comctl32.lib d3d9.lib d3dx9.lib winmm.lib vfw32.lib"
+				AdditionalDependencies="cg.lib cgD3D9.lib dsound.lib odbc32.lib odbccp32.lib comctl32.lib d3d9.lib d3dx9.lib winmm.lib vfw32.lib"
 				OutputFile="..\..\..\Binary\x64\Plugins\Plugin_VideoDX9D.dll"
 				LinkIncremental="1"
 				SuppressStartupBanner="true"
-				AdditionalLibraryDirectories=""
+				AdditionalLibraryDirectories="..\..\..\Externals\Cg64"
 				GenerateManifest="false"
 				GenerateDebugInformation="true"
 				ProgramDatabaseFile="$(PlatformName)\$(ConfigurationName)\$(TargetName).pdb"
@@ -440,7 +441,7 @@
 			InheritedPropertySheets="$(VCInstallDir)VCProjectDefaults\UpgradeFromVC71.vsprops"
 			UseOfMFC="0"
 			ATLMinimizesCRunTimeLibraryUsage="false"
-			CharacterSet="2"
+			CharacterSet="1"
 			WholeProgramOptimization="1"
 			>
 			<Tool
@@ -472,7 +473,7 @@
 				FavorSizeOrSpeed="1"
 				OmitFramePointers="false"
 				WholeProgramOptimization="true"
-				AdditionalIncludeDirectories="../../PluginSpecs;../../Core/Common/Src;../../Core/VideoCommon/Src;../../../Externals"
+				AdditionalIncludeDirectories="../../PluginSpecs;../../Core/Common/Src;../../Core/VideoCommon/Src;../../../Externals;..\..\..\Externals\wxWidgets\Include;..\..\..\Externals\wxWidgets\Include\msvc"
 				PreprocessorDefinitions="_WIN32;WIN32;NDEBUG;_WINDOWS;_USRDLL;VIDEO_DIRECTX9_EXPORTS;_CRT_SECURE_NO_DEPRECATE;_SECURE_SCL=0;DEBUGFAST"
 				StringPooling="true"
 				RuntimeLibrary="0"
@@ -505,13 +506,14 @@
 			/>
 			<Tool
 				Name="VCLinkerTool"
-				AdditionalDependencies="dsound.lib odbc32.lib odbccp32.lib comctl32.lib d3d9.lib d3dx9.lib winmm.lib vfw32.lib"
+				AdditionalDependencies="dsound.lib odbc32.lib odbccp32.lib comctl32.lib d3d9.lib d3dx9.lib winmm.lib cg.lib cgD3D9.lib vfw32.lib wxbase28u.lib wxmsw28u_core.lib"
 				OutputFile="..\..\..\Binary\Win32\Plugins\Plugin_VideoDX9DF.dll"
 				LinkIncremental="1"
 				SuppressStartupBanner="true"
-				AdditionalLibraryDirectories="../../../Externals/Cg"
+				AdditionalLibraryDirectories="../../../Externals/Cg;&quot;..\..\..\Externals\wxWidgets\lib\vc_lib\$(PlatformName)&quot;"
 				GenerateManifest="false"
-				IgnoreDefaultLibraryNames="LIBCMTD.lib"
+				IgnoreDefaultLibraryNames="LIBCMTD.lib wxbase28u.lib"
+				GenerateDebugInformation="true"
 				ProgramDatabaseFile="$(PlatformName)\$(ConfigurationName)\$(TargetName).pdb"
 				RandomizedBaseAddress="1"
 				DataExecutionPrevention="0"
@@ -548,7 +550,7 @@
 			InheritedPropertySheets="$(VCInstallDir)VCProjectDefaults\UpgradeFromVC71.vsprops"
 			UseOfMFC="0"
 			ATLMinimizesCRunTimeLibraryUsage="false"
-			CharacterSet="2"
+			CharacterSet="1"
 			WholeProgramOptimization="0"
 			>
 			<Tool
@@ -574,13 +576,14 @@
 			/>
 			<Tool
 				Name="VCCLCompilerTool"
+				AdditionalOptions="/MP4"
 				Optimization="3"
 				InlineFunctionExpansion="2"
 				EnableIntrinsicFunctions="true"
 				FavorSizeOrSpeed="1"
 				OmitFramePointers="false"
 				WholeProgramOptimization="true"
-				AdditionalIncludeDirectories="../../../Externals;../../PluginSpecs;../../Core/Common/Src;../../Core/VideoCommon/Src"
+				AdditionalIncludeDirectories="../../../Externals;../../PluginSpecs;../../Core/Common/Src;../../Core/VideoCommon/Src;..\..\..\Externals\wxWidgets\Include;..\..\..\Externals\wxWidgets\Include\msvc"
 				PreprocessorDefinitions="WIN32;NDEBUG;_WINDOWS;_USRDLL;VIDEO_DIRECTX9_EXPORTS;_CRT_SECURE_NO_DEPRECATE;_SECURE_SCL=0;;DEBUGFAST"
 				StringPooling="true"
 				ExceptionHandling="1"
@@ -614,11 +617,11 @@
 			/>
 			<Tool
 				Name="VCLinkerTool"
-				AdditionalDependencies="dsound.lib odbc32.lib odbccp32.lib comctl32.lib d3d9.lib d3dx9.lib winmm.lib vfw32.lib"
+				AdditionalDependencies="cg.lib cgD3D9.lib dsound.lib odbc32.lib odbccp32.lib comctl32.lib d3d9.lib d3dx9.lib winmm.lib vfw32.lib rpcrt4.lib wxbase28u.lib wxmsw28u_core.lib"
 				OutputFile="..\..\..\Binary\x64\Plugins\Plugin_VideoDX9DF.dll"
 				LinkIncremental="1"
 				SuppressStartupBanner="true"
-				AdditionalLibraryDirectories=""
+				AdditionalLibraryDirectories="..\..\..\Externals\Cg64;&quot;..\..\..\Externals\wxWidgets\lib\vc_lib\$(PlatformName)&quot;"
 				GenerateManifest="false"
 				GenerateDebugInformation="true"
 				ProgramDatabaseFile="$(PlatformName)\$(ConfigurationName)\$(TargetName).pdb"
@@ -1285,6 +1288,18 @@
 			</File>
 			<File
 				RelativePath=".\Src\DlgSettings.h"
+				>
+			</File>
+		</Filter>
+		<Filter
+			Name="Debugger"
+			>
+			<File
+				RelativePath=".\Src\Debugger\Debugger.cpp"
+				>
+			</File>
+			<File
+				RelativePath=".\Src\Debugger\Debugger.h"
 				>
 			</File>
 		</Filter>

iCube/Source/Plugins/Plugin_VideoDX9/Src/VertexManager.cpp
@@ -16,7 +16,6 @@
 // http://code.google.com/p/dolphin-emu/
 
 #include "Common.h"
-#include "FileUtil.h"
 
 #include "D3DBase.h"
 
@@ -36,6 +35,9 @@
 
 #include "BPStructs.h"
 #include "XFStructs.h"
+
+#include "debugger/debugger.h"
+
 
 using namespace D3D;
 
@@ -82,8 +84,6 @@ bool Init()
 	collection = C_NOTHING;
 	fakeVBuffer = new u8[MAXVBUFFERSIZE];
 	fakeIBuffer = new u16[MAXIBUFFERSIZE];
-	memset(fakeVBuffer, 0, MAXVBUFFERSIZE);
-	memset(fakeIBuffer, 0, MAXIBUFFERSIZE * 2);
 	CreateDeviceObjects();
 	VertexManager::s_pCurBufferPointer = fakeVBuffer;
 	return true;
@@ -146,6 +146,9 @@ void AddVertices(int _primitive, int _numVertices)
 		//We are NOT collecting the right type.
 		Flush();
 
+		// Copy the extra verts that we lost.
+		memcpy(s_pCurBufferPointer, fakeVBuffer, _numVertices * g_nativeVertexFmt->GetVertexStride());
+
 		collection = type;
 		u16 *ptr = 0;
 		if (type != C_POINTS)
@@ -155,7 +158,7 @@ void AddVertices(int _primitive, int _numVertices)
 			AddIndices(_primitive, _numVertices);
 		}
 		if (_numVertices >= MAXVBUFFERSIZE)
-			MessageBox(NULL, "Too many vertices for the buffer", "Dolphin DX9 Video Plugin", MB_OK);
+			MessageBoxA(NULL, "Too many vertices for the buffer", "Dolphin DX9 Video Plugin", MB_OK);
 	}
 	else  // We are collecting the right type, keep going
 	{
@@ -165,7 +168,7 @@ void AddVertices(int _primitive, int _numVertices)
 		int last = indexGen.GetNumVerts();
 		AddIndices(_primitive, _numVertices);
 		if (_numVertices >= MAXVBUFFERSIZE)
-			MessageBox(NULL, "Too many vertices for the buffer", "Dolphin DX9 Video Plugin", MB_OK);
+			MessageBoxA(NULL, "Too many vertices for the buffer", "Dolphin DX9 Video Plugin", MB_OK);
 	}
 }
 
@@ -321,7 +324,9 @@ void Flush()
 
 		collection = C_NOTHING;
 		VertexManager::s_pCurBufferPointer = fakeVBuffer;
+		DEBUGGER_PAUSE_COUNT_N(NEXT_FLUSH);
 	}
+	//DX9DEBUGGER_PAUSE_IF(NEXT_FLUSH);
 }
 
 }  // namespace

iCube/Source/Dolphin.sln
@@ -217,6 +217,7 @@ Global
 		{F0B874CB-4476-4199-9315-8343D05AE684}.Release|x64.ActiveCfg = Release|x64
 		{F0B874CB-4476-4199-9315-8343D05AE684}.Release|x64.Build.0 = Release|x64
 		{636FAD5F-02D1-4E9A-BE67-FB8EA99B9A18}.Debug|Win32.ActiveCfg = Debug|Win32
+		{636FAD5F-02D1-4E9A-BE67-FB8EA99B9A18}.Debug|Win32.Build.0 = Debug|Win32
 		{636FAD5F-02D1-4E9A-BE67-FB8EA99B9A18}.Debug|x64.ActiveCfg = Debug|x64
 		{636FAD5F-02D1-4E9A-BE67-FB8EA99B9A18}.Debug|x64.Build.0 = Debug|x64
 		{636FAD5F-02D1-4E9A-BE67-FB8EA99B9A18}.DebugFast|Win32.ActiveCfg = DebugFast|Win32
@@ -232,6 +233,7 @@ Global
 		{636FAD5F-02D1-4E9A-BE67-FB8EA99B9A18}.Release|x64.ActiveCfg = Release|x64
 		{636FAD5F-02D1-4E9A-BE67-FB8EA99B9A18}.Release|x64.Build.0 = Release|x64
 		{9A183B48-ECC2-4121-876A-9B3793686073}.Debug|Win32.ActiveCfg = Debug|Win32
+		{9A183B48-ECC2-4121-876A-9B3793686073}.Debug|Win32.Build.0 = Debug|Win32
 		{9A183B48-ECC2-4121-876A-9B3793686073}.Debug|x64.ActiveCfg = Debug|x64
 		{9A183B48-ECC2-4121-876A-9B3793686073}.Debug|x64.Build.0 = Debug|x64
 		{9A183B48-ECC2-4121-876A-9B3793686073}.DebugFast|Win32.ActiveCfg = DebugFast|Win32
@@ -295,6 +297,7 @@ Global
 		{B7F1A9FB-BEA8-416E-9460-AE35A6A5165C}.Release|x64.ActiveCfg = Release|x64
 		{B7F1A9FB-BEA8-416E-9460-AE35A6A5165C}.Release|x64.Build.0 = Release|x64
 		{CFDCEE0E-FA45-4F72-9FCC-0B88F5A75160}.Debug|Win32.ActiveCfg = Debug|Win32
+		{CFDCEE0E-FA45-4F72-9FCC-0B88F5A75160}.Debug|Win32.Build.0 = Debug|Win32
 		{CFDCEE0E-FA45-4F72-9FCC-0B88F5A75160}.Debug|x64.ActiveCfg = Debug|x64
 		{CFDCEE0E-FA45-4F72-9FCC-0B88F5A75160}.Debug|x64.Build.0 = Debug|x64
 		{CFDCEE0E-FA45-4F72-9FCC-0B88F5A75160}.DebugFast|Win32.ActiveCfg = DebugFast|Win32
@@ -374,6 +377,7 @@ Global
 		{E5D1F0C0-AA07-4841-A4EB-4CF4DAA6B0FA}.Release|x64.ActiveCfg = Release|x64
 		{E5D1F0C0-AA07-4841-A4EB-4CF4DAA6B0FA}.Release|x64.Build.0 = Release|x64
 		{521498BE-6089-4780-8223-E67C22F4E068}.Debug|Win32.ActiveCfg = Debug|Win32
+		{521498BE-6089-4780-8223-E67C22F4E068}.Debug|Win32.Build.0 = Debug|Win32
 		{521498BE-6089-4780-8223-E67C22F4E068}.Debug|x64.ActiveCfg = Debug|x64
 		{521498BE-6089-4780-8223-E67C22F4E068}.Debug|x64.Build.0 = Debug|x64
 		{521498BE-6089-4780-8223-E67C22F4E068}.DebugFast|Win32.ActiveCfg = DebugFast|Win32
@@ -421,6 +425,7 @@ Global
 		{33546D62-7F34-4EA6-A88E-D538B36E16BF}.Release|x64.ActiveCfg = Release|x64
 		{33546D62-7F34-4EA6-A88E-D538B36E16BF}.Release|x64.Build.0 = Release|x64
 		{8D612734-FAA5-4B8A-804F-4DEA2367D495}.Debug|Win32.ActiveCfg = Debug|Win32
+		{8D612734-FAA5-4B8A-804F-4DEA2367D495}.Debug|Win32.Build.0 = Debug|Win32
 		{8D612734-FAA5-4B8A-804F-4DEA2367D495}.Debug|x64.ActiveCfg = Debug|x64
 		{8D612734-FAA5-4B8A-804F-4DEA2367D495}.Debug|x64.Build.0 = Debug|x64
 		{8D612734-FAA5-4B8A-804F-4DEA2367D495}.DebugFast|Win32.ActiveCfg = DebugFast|Win32
@@ -452,6 +457,7 @@ Global
 		{C7E5D50A-2916-464B-86A7-E10B3CC88ADA}.Release|x64.ActiveCfg = Release|x64
 		{C7E5D50A-2916-464B-86A7-E10B3CC88ADA}.Release|x64.Build.0 = Release|x64
 		{58E81545-241B-416E-8088-E62452EB25FA}.Debug|Win32.ActiveCfg = Debug|Win32
+		{58E81545-241B-416E-8088-E62452EB25FA}.Debug|Win32.Build.0 = Debug|Win32
 		{58E81545-241B-416E-8088-E62452EB25FA}.Debug|x64.ActiveCfg = Debug|x64
 		{58E81545-241B-416E-8088-E62452EB25FA}.DebugFast|Win32.ActiveCfg = DebugFast|Win32
 		{58E81545-241B-416E-8088-E62452EB25FA}.DebugFast|x64.ActiveCfg = DebugFast|x64
@@ -476,6 +482,7 @@ Global
 		{FBAFB369-07EB-4460-9CAD-08BE5789DAB6}.Release|x64.ActiveCfg = Release|x64
 		{FBAFB369-07EB-4460-9CAD-08BE5789DAB6}.Release|x64.Build.0 = Release|x64
 		{3D8156A9-64D1-4C8E-ADBE-1B319030E4A4}.Debug|Win32.ActiveCfg = Debug|Win32
+		{3D8156A9-64D1-4C8E-ADBE-1B319030E4A4}.Debug|Win32.Build.0 = Debug|Win32
 		{3D8156A9-64D1-4C8E-ADBE-1B319030E4A4}.Debug|x64.ActiveCfg = Debug|x64
 		{3D8156A9-64D1-4C8E-ADBE-1B319030E4A4}.Debug|x64.Build.0 = Debug|x64
 		{3D8156A9-64D1-4C8E-ADBE-1B319030E4A4}.DebugFast|Win32.ActiveCfg = DebugFast|Win32
@@ -562,9 +569,7 @@ Global
 		{823DDC98-42D5-4A38-88CF-9DC06C788AE4}.Release|x64.ActiveCfg = Release|x64
 		{823DDC98-42D5-4A38-88CF-9DC06C788AE4}.Release|x64.Build.0 = Release|x64
 		{40C636FA-B5BF-4D67-ABC8-376B524A7551}.Debug|Win32.ActiveCfg = Debug|Win32
-		{40C636FA-B5BF-4D67-ABC8-376B524A7551}.Debug|Win32.Build.0 = Debug|Win32
 		{40C636FA-B5BF-4D67-ABC8-376B524A7551}.Debug|x64.ActiveCfg = Debug|x64
-		{40C636FA-B5BF-4D67-ABC8-376B524A7551}.Debug|x64.Build.0 = Debug|x64
 		{40C636FA-B5BF-4D67-ABC8-376B524A7551}.DebugFast|Win32.ActiveCfg = Debug|Win32
 		{40C636FA-B5BF-4D67-ABC8-376B524A7551}.DebugFast|Win32.Build.0 = Debug|Win32
 		{40C636FA-B5BF-4D67-ABC8-376B524A7551}.DebugFast|x64.ActiveCfg = Debug|x64

iCube/Source/Plugins/Plugin_VideoDX9/Src/D3DBase.cpp
@@ -228,12 +228,12 @@ namespace D3D
 			adapter, 
 			D3DDEVTYPE_HAL, 
 			wnd,
-			D3DCREATE_HARDWARE_VERTEXPROCESSING,
+			D3DCREATE_HARDWARE_VERTEXPROCESSING | D3DCREATE_MULTITHREADED,
 			// |D3DCREATE_MULTITHREADED /* | D3DCREATE_PUREDEVICE*/,
 			//D3DCREATE_SOFTWARE_VERTEXPROCESSING ,
 			&d3dpp, &dev ) ) )
 		{
-			MessageBox(wnd,
+			MessageBoxA(wnd,
 				"Sorry, but it looks like your 3D accelerator is too old,\n"
 				"or doesn't support features that Dolphin requires.\n"
 				"Falling back to software vertex processing.\n", 
@@ -242,12 +242,12 @@ namespace D3D
 				adapter, 
 				D3DDEVTYPE_HAL, 
 				wnd,
-				D3DCREATE_SOFTWARE_VERTEXPROCESSING,
+				D3DCREATE_SOFTWARE_VERTEXPROCESSING | D3DCREATE_MULTITHREADED,
 				// |D3DCREATE_MULTITHREADED /* | D3DCREATE_PUREDEVICE*/,
 				//D3DCREATE_SOFTWARE_VERTEXPROCESSING ,
 				&d3dpp, &dev ) ) )
 			{
-				MessageBox(wnd,
+				MessageBoxA(wnd,
 					"Software VP failed too. Upgrade your graphics card.", 
 					"Dolphin Direct3D plugin", MB_OK | MB_ICONERROR);
 				return E_FAIL;
@@ -263,7 +263,7 @@ namespace D3D
 
 		if (caps.NumSimultaneousRTs < 2)
 		{
-			MessageBox(0, "Warning - your graphics card does not support multiple render targets.", 0, 0);
+			MessageBoxA(0, "Warning - your graphics card does not support multiple render targets.", 0, 0);
 		}
 
 		// Device state would normally be set here
@@ -309,19 +309,19 @@ namespace D3D
 		switch (err) 
 		{
 		case D3DERR_DEVICELOST:
-			MessageBox(0, "Device Lost", "D3D ERROR", 0);
+			MessageBoxA(0, "Device Lost", "D3D ERROR", 0);
 			break;
 		case D3DERR_INVALIDCALL:
-			MessageBox(0, "Invalid Call", "D3D ERROR", 0);
+			MessageBoxA(0, "Invalid Call", "D3D ERROR", 0);
 			break;
 		case D3DERR_DRIVERINTERNALERROR:
-			MessageBox(0, "Driver Internal Error", "D3D ERROR", 0);
+			MessageBoxA(0, "Driver Internal Error", "D3D ERROR", 0);
 			break;
 		case D3DERR_OUTOFVIDEOMEMORY:
-			MessageBox(0, "Out of vid mem", "D3D ERROR", 0);
+			MessageBoxA(0, "Out of vid mem", "D3D ERROR", 0);
 			break;
 		default:
-			// MessageBox(0,"Other error or success","ERROR",0);
+			// MessageBoxA(0,"Other error or success","ERROR",0);
 			break;
 		}
 	}

iCube/Source/Plugins/Plugin_VideoDX9/Src/D3DShader.cpp
@@ -45,7 +45,7 @@ LPDIRECT3DVERTEXSHADER9 CompileVertexShader(const char *code, int len)
 			std::string hello = (char*)errorBuffer->GetBufferPointer();
 			hello += "\n\n";
 			hello += code;
-			MessageBox(0, hello.c_str(), "Error assembling vertex shader", MB_ICONERROR);
+			MessageBoxA(0, hello.c_str(), "Error assembling vertex shader", MB_ICONERROR);
 		}
 		vShader = 0;
 	}
@@ -57,7 +57,7 @@ LPDIRECT3DVERTEXSHADER9 CompileVertexShader(const char *code, int len)
 			hr = D3D::dev->CreateVertexShader((DWORD *)shaderBuffer->GetBufferPointer(), &vShader);
 		if ((FAILED(hr) || vShader == 0) && g_Config.bShowShaderErrors)
 		{
-			MessageBox(0, code, (char*)errorBuffer->GetBufferPointer(),MB_ICONERROR);
+			MessageBoxA(0, code, (char*)errorBuffer->GetBufferPointer(),MB_ICONERROR);
 		}
 	}
 
@@ -89,7 +89,7 @@ LPDIRECT3DPIXELSHADER9 CompilePixelShader(const char *code, int len)
 			std::string hello = (char*)errorBuffer->GetBufferPointer();
 			hello += "\n\n";
 			hello += code;
-			MessageBox(0, hello.c_str(), "Error assembling pixel shader", MB_ICONERROR);
+			MessageBoxA(0, hello.c_str(), "Error assembling pixel shader", MB_ICONERROR);
 		}
 		pShader = 0;
 	}
@@ -99,7 +99,7 @@ LPDIRECT3DPIXELSHADER9 CompilePixelShader(const char *code, int len)
 		HRESULT hr = D3D::dev->CreatePixelShader((DWORD *)shaderBuffer->GetBufferPointer(), &pShader);
 		if ((FAILED(hr) || pShader == 0) && g_Config.bShowShaderErrors)
 		{
-			MessageBox(0, "damn", "error creating pixelshader", MB_ICONERROR);
+			MessageBoxA(0, "damn", "error creating pixelshader", MB_ICONERROR);
 		}
 	}
 

iCube/Source/Plugins/Plugin_VideoDX9/Src/D3DUtil.cpp
@@ -83,7 +83,7 @@ namespace D3D
 		HFONT hFont = CreateFont(nHeight, 0, 0, 0, dwBold, 0,
 			FALSE, FALSE, DEFAULT_CHARSET, OUT_DEFAULT_PRECIS,
 			CLIP_DEFAULT_PRECIS, ANTIALIASED_QUALITY,
-			VARIABLE_PITCH, "Tahoma");
+			VARIABLE_PITCH, _T("Tahoma"));
 
 		if (NULL == hFont)
 			return E_FAIL;
@@ -106,7 +106,7 @@ namespace D3D
 		for (char c=0; c<127-32; c++)
 		{
 			str[0] = c+32;
-			GetTextExtentPoint32(hDC, str, 1, &size);
+			GetTextExtentPoint32A(hDC, str, 1, &size);
 
 			if ((int)(x+size.cx+1) > m_dwTexWidth)
 			{
@@ -114,7 +114,7 @@ namespace D3D
 				y += size.cy+1;
 			}
 
-			ExtTextOut(hDC, x+1, y+0, ETO_OPAQUE | ETO_CLIPPED, NULL, str, 1, NULL);
+			ExtTextOutA(hDC, x+1, y+0, ETO_OPAQUE | ETO_CLIPPED, NULL, str, 1, NULL);
 
 			m_fTexCoords[c][0] = ((float)(x+0))/m_dwTexWidth;
 			m_fTexCoords[c][1] = ((float)(y+0))/m_dwTexHeight;
@@ -215,8 +215,9 @@ namespace D3D
 
 		dev->SetPixelShader(0);
 		dev->SetVertexShader(0);
-		// dev->SetVertexDeclaration(0);
+		dev->SetVertexDeclaration(0);
 		
+		// dev->SetFVF(D3DFVF_FONT2DVERTEX);
 		Renderer::SetFVF(D3DFVF_FONT2DVERTEX);
 
 		for (int i = 0; i < 6; i++) {
@@ -379,8 +380,9 @@ namespace D3D
 			{x2-0.5f, y2-0.5f, 0, 1, color, u2, v2},
 			{x1-0.5f, y2-0.5f, 0, 1, color, u1, v2},
 		};
-		dev->SetPixelShader(NULL);
-		dev->SetVertexShader(NULL);
+		dev->SetPixelShader(0);
+		dev->SetVertexShader(0);
+		dev->SetVertexDeclaration(0);
 		
 		Renderer::SetFVF(D3DFVF_XYZRHW | D3DFVF_DIFFUSE | D3DFVF_TEX1);
 		dev->DrawPrimitiveUP(D3DPT_TRIANGLEFAN, 2, coords, sizeof(Q2DVertex));

commit 013ca671da3e4cb5438ad9fa1a85893e01a39e07

Fix the DEBUG builds that were broken in r4154.
git-svn-id: https://dolphin-emu.googlecode.com/svn/trunk@4155 8ced0084-cf51-0410-be5f-012b33b47a6e

iCube/Source/Plugins/Plugin_VideoDX9/Plugin_VideoDX9.vcproj
@@ -343,7 +343,7 @@
 			InheritedPropertySheets="$(VCInstallDir)VCProjectDefaults\UpgradeFromVC71.vsprops"
 			UseOfMFC="0"
 			ATLMinimizesCRunTimeLibraryUsage="false"
-			CharacterSet="2"
+			CharacterSet="1"
 			>
 			<Tool
 				Name="VCPreBuildEventTool"
@@ -369,7 +369,7 @@
 			<Tool
 				Name="VCCLCompilerTool"
 				Optimization="0"
-				AdditionalIncludeDirectories="../../../Externals;../../PluginSpecs;../../Core/Common/Src;../../Core/VideoCommon/Src"
+				AdditionalIncludeDirectories="../../PluginSpecs;../../Core/Common/Src;../../Core/VideoCommon/Src;../../../Externals;..\..\..\Externals\wxWidgets\Include;..\..\..\Externals\wxWidgets\Include\msvc"
 				PreprocessorDefinitions="WIN32;_DEBUG;_WINDOWS;_USRDLL;VIDEO_DIRECTX9_EXPORTS;_CRT_SECURE_NO_DEPRECATE;_SECURE_SCL=0"
 				BasicRuntimeChecks="3"
 				RuntimeLibrary="1"
@@ -398,11 +398,11 @@
 			/>
 			<Tool
 				Name="VCLinkerTool"
-				AdditionalDependencies="cg.lib cgD3D9.lib dsound.lib odbc32.lib odbccp32.lib comctl32.lib d3d9.lib d3dx9.lib winmm.lib vfw32.lib"
+				AdditionalDependencies="cg.lib cgD3D9.lib dsound.lib odbc32.lib odbccp32.lib comctl32.lib d3d9.lib d3dx9.lib winmm.lib vfw32.lib rpcrt4.lib wxbase28ud.lib wxmsw28ud_core.lib"
 				OutputFile="..\..\..\Binary\x64\Plugins\Plugin_VideoDX9D.dll"
 				LinkIncremental="1"
 				SuppressStartupBanner="true"
-				AdditionalLibraryDirectories="..\..\..\Externals\Cg64"
+				AdditionalLibraryDirectories="..\..\..\Externals\Cg64;&quot;..\..\..\Externals\wxWidgets\lib\vc_lib\$(PlatformName)&quot;"
 				GenerateManifest="false"
 				GenerateDebugInformation="true"
 				ProgramDatabaseFile="$(PlatformName)\$(ConfigurationName)\$(TargetName).pdb"

iCube/Source/Plugins/Plugin_VideoDX9/Src/VertexManager.cpp
@@ -16,6 +16,7 @@
 // http://code.google.com/p/dolphin-emu/
 
 #include "Common.h"
+#include "FileUtil.h"
 
 #include "D3DBase.h"
 