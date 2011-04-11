kiddreads/Avalon

commit 2f88e2b62d15f98a98fd80edb5bfc2eeceabff91

Rework XF register loading a bit and change how registers are arranged in memory. This removes the assumption that all data for a viewport or projection matrix will be available when index 0 is loaded. Fixes issue 3688 and probably breaks old save states (sorry).
git-svn-id: https://dolphin-emu.googlecode.com/svn/trunk@7083 8ced0084-cf51-0410-be5f-012b33b47a6e

dolphin-ios/Source/Core/VideoCommon/Src/PixelShaderManager.cpp
@@ -24,8 +24,6 @@
 #include "VideoConfig.h"
 
 #include "RenderBase.h"
-
-static float GC_ALIGNED16(s_fMaterials[16]);
 static int s_nColorsChanged[2]; // 0 - regular colors, 1 - k colors
 static int s_nIndTexMtxChanged;
 static bool s_bAlphaChanged;
@@ -36,7 +34,6 @@ static bool s_bFogColorChanged;
 static bool s_bFogParamChanged;
 static bool s_bFogRangeAdjustChanged;
 static int nLightsChanged[2]; // min,max
-static float lastDepthRange[2]; // 0 = far z, 1 = far - near
 static float lastRGBAfull[2][4][4];
 static u8 s_nTexDimsChanged;
 static u8 s_nIndTexScaleChanged;
@@ -142,8 +139,16 @@ void PixelShaderManager::SetConstants()
 
 	if (s_bZBiasChanged || s_bDepthRangeChanged) 
 	{
-        //ERROR_LOG("pixel=%x,%x, bias=%x\n", bpmem.zcontrol.pixel_format, bpmem.ztex2.type, lastZBias);
-		SetPSConstant4f(C_ZBIAS+1, lastDepthRange[0] / 16777216.0f, lastDepthRange[1] / 16777216.0f, 0, (float)(lastZBias)/16777215.0f);
+        // reversed gxsetviewport(xorig, yorig, width, height, nearz, farz)
+		// [0] = width/2
+		// [1] = height/2
+		// [2] = 16777215 * (farz - nearz)
+		// [3] = xorig + width/2 + 342
+		// [4] = yorig + height/2 + 342
+		// [5] = 16777215 * farz
+
+		//ERROR_LOG("pixel=%x,%x, bias=%x\n", bpmem.zcontrol.pixel_format, bpmem.ztex2.type, lastZBias);
+        SetPSConstant4f(C_ZBIAS+1, xfregs.rawViewport[2] / 16777216.0f, xfregs.rawViewport[5] / 16777216.0f, 0, (float)(lastZBias)/16777215.0f);
 		s_bZBiasChanged = s_bDepthRangeChanged = false;
     }
 
@@ -252,48 +257,66 @@ void PixelShaderManager::SetConstants()
 		s_bFogRangeAdjustChanged = false;
 	}
 
-	if (g_ActiveConfig.bEnablePixelLigting && g_ActiveConfig.backend_info.bSupportsPixelLighting && nLightsChanged[0] >= 0) // config check added because the code in here was crashing for me inside SetPSConstant4f
+	if (g_ActiveConfig.bEnablePixelLigting && g_ActiveConfig.backend_info.bSupportsPixelLighting)  // config check added because the code in here was crashing for me inside SetPSConstant4f
 	{
-		// lights don't have a 1 to 1 mapping, the color component needs to be converted to 4 floats
-		int istart = nLightsChanged[0] / 0x10;
-		int iend = (nLightsChanged[1] + 15) / 0x10;
-		const float* xfmemptr = (const float*)&xfmem[0x10 * istart + XFMEM_LIGHTS];
-
-		for (int i = istart; i < iend; ++i)
+		if (nLightsChanged[0] >= 0)
 		{
-			u32 color = *(const u32*)(xfmemptr + 3);
-			float NormalizationCoef = 1 / 255.0f;
-			SetPSConstant4f(C_PLIGHTS + 5 * i,
-				((color >> 24) & 0xFF) * NormalizationCoef,
-				((color >> 16) & 0xFF) * NormalizationCoef,
-				((color >> 8)  & 0xFF) * NormalizationCoef,
-				((color)       & 0xFF) * NormalizationCoef);
-			xfmemptr += 4;
+			// lights don't have a 1 to 1 mapping, the color component needs to be converted to 4 floats
+			int istart = nLightsChanged[0] / 0x10;
+			int iend = (nLightsChanged[1] + 15) / 0x10;
+			const float* xfmemptr = (const float*)&xfmem[0x10 * istart + XFMEM_LIGHTS];
 
-			for (int j = 0; j < 4; ++j, xfmemptr += 3)
+			for (int i = istart; i < iend; ++i)
 			{
-				if (j == 1 &&
-					fabs(xfmemptr[0]) < 0.00001f &&
-					fabs(xfmemptr[1]) < 0.00001f &&
-					fabs(xfmemptr[2]) < 0.00001f)
+				u32 color = *(const u32*)(xfmemptr + 3);
+				float NormalizationCoef = 1 / 255.0f;
+				SetPSConstant4f(C_PLIGHTS + 5 * i,
+					((color >> 24) & 0xFF) * NormalizationCoef,
+					((color >> 16) & 0xFF) * NormalizationCoef,
+					((color >> 8)  & 0xFF) * NormalizationCoef,
+					((color)       & 0xFF) * NormalizationCoef);
+				xfmemptr += 4;
+
+				for (int j = 0; j < 4; ++j, xfmemptr += 3)
 				{
-					// dist attenuation, make sure not equal to 0!!!
-					SetPSConstant4f(C_PLIGHTS+5*i+j+1, 0.00001f, xfmemptr[1], xfmemptr[2], 0);
+					if (j == 1 &&
+						fabs(xfmemptr[0]) < 0.00001f &&
+						fabs(xfmemptr[1]) < 0.00001f &&
+						fabs(xfmemptr[2]) < 0.00001f)
+					{
+						// dist attenuation, make sure not equal to 0!!!
+						SetPSConstant4f(C_PLIGHTS+5*i+j+1, 0.00001f, xfmemptr[1], xfmemptr[2], 0);
+					}
+					else
+						SetPSConstant4fv(C_PLIGHTS+5*i+j+1, xfmemptr);
 				}
-				else
-					SetPSConstant4fv(C_PLIGHTS+5*i+j+1, xfmemptr);
 			}
+
+			nLightsChanged[0] = nLightsChanged[1] = -1;
 		}
 
-		nLightsChanged[0] = nLightsChanged[1] = -1;
-	}
-	if (nMaterialsChanged)
-	{
-		for (int i = 0; i < 4; ++i)
-			if (nMaterialsChanged & (1 << i))
-				SetPSConstant4fv(C_PMATERIALS + i, &s_fMaterials[4 * i]);
+		if (nMaterialsChanged)
+		{
+			float GC_ALIGNED16(material[4]);
+			float NormalizationCoef = 1 / 255.0f;
 
-		nMaterialsChanged = 0;
+			for (int i = 0; i < 4; ++i)
+			{
+				if (nMaterialsChanged & (1 << i))
+				{
+					u32 data = *(xfregs.ambColor + i);
+
+					material[0] = ((data >> 24) & 0xFF) * NormalizationCoef;
+					material[1] = ((data >> 16) & 0xFF) * NormalizationCoef;
+					material[2] = ((data >>  8) & 0xFF) * NormalizationCoef;
+					material[3] = ( data        & 0xFF) * NormalizationCoef;
+
+					SetPSConstant4fv(C_PMATERIALS + i, material);
+				}
+			}
+
+			nMaterialsChanged = 0;
+		}
 	}
 }
 
@@ -367,39 +390,9 @@ void PixelShaderManager::SetZTextureBias(u32 bias)
     }
 }
 
-void PixelShaderManager::SetViewport(float* viewport,int VIndex)
+void PixelShaderManager::SetViewportChanged()
 {
-	// reversed gxsetviewport(xorig, yorig, width, height, nearz, farz)
-    // [0] = width/2
-    // [1] = height/2
-    // [2] = 16777215 * (farz - nearz)
-    // [3] = xorig + width/2 + 342
-    // [4] = yorig + height/2 + 342
-    // [5] = 16777215 * farz
-
-	if(VIndex <= 0)
-	{
-		if (lastDepthRange[0] != viewport[5] || lastDepthRange[1] != viewport[2])
-		{
-			lastDepthRange[0] = viewport[5];
-			lastDepthRange[1] = viewport[2];
-
-			s_bDepthRangeChanged = true;
-		}
-	}
-	else
-	{
-		if (VIndex == 2 && lastDepthRange[1] != viewport[0])
-		{
-			lastDepthRange[1] = viewport[0];
-			s_bDepthRangeChanged = true;
-		}
-		else if(VIndex == 5 && lastDepthRange[0] != viewport[0])
-		{
-			lastDepthRange[0] = viewport[0];
-			s_bDepthRangeChanged = true;
-		}
-	}
+	s_bDepthRangeChanged = true;
 }
 
 void PixelShaderManager::SetIndTexScaleChanged(u8 stagemask)
@@ -463,14 +456,7 @@ void PixelShaderManager::InvalidateXFRange(int start, int end)
 	}
 }
 
-void PixelShaderManager::SetMaterialColor(int index, u32 data)
+void PixelShaderManager::SetMaterialColorChanged(int index)
 {
-	int ind = index * 4;
-
 	nMaterialsChanged  |= (1 << index);
-	float NormalizationCoef = 1 / 255.0f;
-	s_fMaterials[ind++] = ((data >> 24) & 0xFF) * NormalizationCoef;
-	s_fMaterials[ind++] = ((data >> 16) & 0xFF) * NormalizationCoef;
-	s_fMaterials[ind++] = ((data >>  8) & 0xFF) * NormalizationCoef;
-	s_fMaterials[ind]   = ( data        & 0xFF) * NormalizationCoef;
 }

dolphin-ios/Source/Core/VideoCommon/Src/VertexShaderManager.cpp
@@ -33,8 +33,6 @@
 #include "VertexManagerBase.h"
 
 #include "RenderBase.h"
-
-static float GC_ALIGNED16(s_fMaterials[16]);
 float GC_ALIGNED16(g_fProjectionMatrix[16]);
 
 // track changes
@@ -237,9 +235,23 @@ void VertexShaderManager::SetConstants()
 
 	if (nMaterialsChanged)
 	{
+		float GC_ALIGNED16(material[4]);
+		float NormalizationCoef = 1 / 255.0f;
+
 		for (int i = 0; i < 4; ++i)
+		{
 			if (nMaterialsChanged & (1 << i))
-				SetVSConstant4fv(C_MATERIALS + i, &s_fMaterials[4 * i]);
+			{
+				u32 data = *(xfregs.ambColor + i);
+
+				material[0] = ((data >> 24) & 0xFF) * NormalizationCoef;
+				material[1] = ((data >> 16) & 0xFF) * NormalizationCoef;
+				material[2] = ((data >>  8) & 0xFF) * NormalizationCoef;
+				material[3] = ( data        & 0xFF) * NormalizationCoef;
+
+				SetVSConstant4fv(C_MATERIALS + i, material);
+			}
+		}
 
 		nMaterialsChanged = 0;
 	}
@@ -530,47 +542,19 @@ void VertexShaderManager::SetTexMatrixChangedB(u32 Value)
 	}
 }
 
-void VertexShaderManager::SetViewport(float* _Viewport, int constantIndex)
-{
-	if(constantIndex <= 0)
-	{
-		memcpy(xfregs.rawViewport, _Viewport, sizeof(xfregs.rawViewport));
-	}
-	else
-	{
-		xfregs.rawViewport[constantIndex] = _Viewport[0];
-	}
-	bViewportChanged = true;
-}
-
 void VertexShaderManager::SetViewportChanged()
 {
 	bViewportChanged = true;
 }
 
-void VertexShaderManager::SetProjection(float* _pProjection, int constantIndex)
+void VertexShaderManager::SetProjectionChanged()
 {
-	if(constantIndex <= 0)
-	{
-		memcpy(xfregs.rawProjection, _pProjection, sizeof(xfregs.rawProjection));
-	}
-	else
-	{
-		xfregs.rawProjection[constantIndex] = _pProjection[0];
-	}
 	bProjectionChanged = true;
 }
 
-void VertexShaderManager::SetMaterialColor(int index, u32 data)
+void VertexShaderManager::SetMaterialColorChanged(int index)
 {
-	int ind = index * 4;
-
 	nMaterialsChanged  |= (1 << index);
-	float NormalizationCoef = 1 / 255.0f;
-	s_fMaterials[ind++] = ((data >> 24) & 0xFF) * NormalizationCoef;
-	s_fMaterials[ind++] = ((data >> 16) & 0xFF) * NormalizationCoef;
-	s_fMaterials[ind++] = ((data >>  8) & 0xFF) * NormalizationCoef;
-	s_fMaterials[ind]   = ( data        & 0xFF) * NormalizationCoef;
 }
 
 void VertexShaderManager::TranslateView(float x, float y)

dolphin-ios/Source/Core/VideoCommon/Src/XFMemory.h
@@ -173,6 +173,32 @@ union PostMtxInfo
     u32 hex;
 };
 
+union NumColorChannel
+{
+	struct
+	{
+		u32 numColorChans : 2;
+	};
+	u32 hex;
+};
+
+union NumTexGen
+{
+	struct
+	{
+		u32 numTexGens : 4;
+	};
+	u32 hex;
+};
+
+union DualTexInfo
+{
+	struct
+	{
+		u32 enabled : 1;
+	};
+	u32 hex;
+};
 
 
 
@@ -199,16 +225,6 @@ struct Light
     };
 };
 
-
-
-struct ColorChannel
-{
-    u32 ambColor;
-    u32 matColor;
-    LitChannel color;
-    LitChannel alpha;
-};
-
 struct Viewport
 {
 	float wd;
@@ -219,26 +235,38 @@ struct Viewport
 	float farZ;
 };
 
-struct TexCoordInfo
-{
-    TexMtxInfo texmtxinfo;
-    PostMtxInfo postmtxinfo;
-};
-
 struct XFRegisters
 {
-    int numTexGens;
-    int nNumChans;
-    INVTXSPEC hostinfo; // number of textures,colors,normals from vertex input
-    ColorChannel colChans[2]; //C0A0 C1A1
-    TexCoordInfo texcoords[8];
-    bool bEnableDualTexTransform;
-	float rawViewport[6];
-	float rawProjection[7];	
+    u32 error;                      // 0x1000
+    u32 diag;                       // 0x1001
+    u32 state0;                     // 0x1002
+    u32 state1;                     // 0x1003
+    u32 xfClock;                    // 0x1004
+    u32 clipDisable;                // 0x1005
+    u32 perf0;                      // 0x1006
+    u32 perf1;                      // 0x1007
+    INVTXSPEC hostinfo;             // 0x1008 number of textures,colors,normals from vertex input
+    NumColorChannel numChan;        // 0x1009
+    u32 ambColor[2];                // 0x100a, 0x100b
+    u32 matColor[2];                // 0x100c, 0x100d
+    LitChannel color[2];            // 0x100e, 0x100f
+    LitChannel alpha[2];            // 0x1010, 0x1011
+    DualTexInfo dualTexTrans;       // 0x1012
+    u32 unk3;                       // 0x1013
+    u32 unk4;                       // 0x1014
+    u32 unk5;                       // 0x1015
+    u32 unk6;                       // 0x1016
+    u32 unk7;                       // 0x1017
+    u32 MatrixIndexA;				// 0x1018
+    u32 MatrixIndexB;				// 0x1019
+    float rawViewport[6];           // 0x101a - 0x101f
+    float rawProjection[7];         // 0x1020 - 0x1026
+    u32 unk8[24];                   // 0x1027 - 0x103e
+    NumTexGen numTexGen;            // 0x103f
+    TexMtxInfo texMtxInfo[8];       // 0x1040 - 0x1047
+    u32 unk9[8];                    // 0x1048 - 0x104f
+    PostMtxInfo postMtxInfo[8];     // 0x1050 - 0x1057
 };
-
-
-
 
 
 extern XFRegisters xfregs;

dolphin-ios/Source/Plugins/Plugin_VideoOGL/Src/VertexManager.cpp
@@ -95,27 +95,27 @@ void VertexManager::vFlush()
 	Flushed=true;
 	VideoFifo_CheckEFBAccess();
 #if defined(_DEBUG) || defined(DEBUGFAST) 
-	PRIM_LOG("frame%d:\n texgen=%d, numchan=%d, dualtex=%d, ztex=%d, cole=%d, alpe=%d, ze=%d", g_ActiveConfig.iSaveTargetId, xfregs.numTexGens,
-		xfregs.nNumChans, (int)xfregs.bEnableDualTexTransform, bpmem.ztex2.op,
+	PRIM_LOG("frame%d:\n texgen=%d, numchan=%d, dualtex=%d, ztex=%d, cole=%d, alpe=%d, ze=%d", g_ActiveConfig.iSaveTargetId, xfregs.numTexGen.numTexGens,
+		xfregs.numChan.numColorChans, xfregs.dualTexTrans.enabled, bpmem.ztex2.op,
 		bpmem.blendmode.colorupdate, bpmem.blendmode.alphaupdate, bpmem.zmode.updateenable);
 
-	for (int i = 0; i < xfregs.nNumChans; ++i) 
+	for (unsigned int i = 0; i < xfregs.numChan.numColorChans; ++i) 
 	{
-		LitChannel* ch = &xfregs.colChans[i].color;
+		LitChannel* ch = &xfregs.color[i];
 		PRIM_LOG("colchan%d: matsrc=%d, light=0x%x, ambsrc=%d, diffunc=%d, attfunc=%d", i, ch->matsource, ch->GetFullLightMask(), ch->ambsource, ch->diffusefunc, ch->attnfunc);
-		ch = &xfregs.colChans[i].alpha;
+		ch = &xfregs.alpha[i];
 		PRIM_LOG("alpchan%d: matsrc=%d, light=0x%x, ambsrc=%d, diffunc=%d, attfunc=%d", i, ch->matsource, ch->GetFullLightMask(), ch->ambsource, ch->diffusefunc, ch->attnfunc);
 	}
 
-	for (int i = 0; i < xfregs.numTexGens; ++i) 
+	for (unsigned int i = 0; i < xfregs.numTexGen.numTexGens; ++i) 
 	{
-		TexMtxInfo tinfo = xfregs.texcoords[i].texmtxinfo;
+		TexMtxInfo tinfo = xfregs.texMtxInfo[i];
 		if (tinfo.texgentype != XF_TEXGEN_EMBOSS_MAP) tinfo.hex &= 0x7ff;
 		if (tinfo.texgentype != XF_TEXGEN_REGULAR) tinfo.projection = 0;
 
 		PRIM_LOG("txgen%d: proj=%d, input=%d, gentype=%d, srcrow=%d, embsrc=%d, emblght=%d, postmtx=%d, postnorm=%d",
 			i, tinfo.projection, tinfo.inputform, tinfo.texgentype, tinfo.sourcerow, tinfo.embosssourceshift, tinfo.embosslightshift,
-			xfregs.texcoords[i].postmtxinfo.index, xfregs.texcoords[i].postmtxinfo.normalize);
+			xfregs.postMtxInfo[i].index, xfregs.postMtxInfo[i].normalize);
 	}
 
 	PRIM_LOG("pixel: tev=%d, ind=%d, texgen=%d, dstalpha=%d, alphafunc=0x%x", bpmem.genMode.numtevstages+1, bpmem.genMode.numindstages,

dolphin-ios/Source/Core/VideoCommon/Src/PixelShaderGen.cpp
@@ -41,7 +41,7 @@ void GetPixelShaderId(PIXELSHADERUID *uid, DSTALPHA_MODE dstAlphaMode)
 		if (bpmem.tevorders[i/2].getEnable(i & 1))
 		{
 			int texcoord = bpmem.tevorders[i / 2].getTexCoord(i & 1);
-			if (xfregs.texcoords[texcoord].texmtxinfo.projection)
+			if (xfregs.texMtxInfo[i].projection)
 				projtexcoords |= 1 << texcoord;
 		}
 	}
@@ -66,12 +66,12 @@ void GetPixelShaderId(PIXELSHADERUID *uid, DSTALPHA_MODE dstAlphaMode)
 	if(g_ActiveConfig.bEnablePixelLigting && g_ActiveConfig.backend_info.bSupportsPixelLighting)
 	{
 		for (int i = 0; i < 2; ++i) {
-			uid->values[3 + i] = xfregs.colChans[i].color.enablelighting ?
-				(u32)xfregs.colChans[i].color.hex :
-				(u32)xfregs.colChans[i].color.matsource;
-			uid->values[3 + i] |= (xfregs.colChans[i].alpha.enablelighting ?
-				(u32)xfregs.colChans[i].alpha.hex :
-				(u32)xfregs.colChans[i].alpha.matsource) << 15;
+			uid->values[3 + i] = xfregs.color[i].enablelighting ?
+				(u32)xfregs.color[i].hex :
+				(u32)xfregs.color[i].matsource;
+			uid->values[3 + i] |= (xfregs.alpha[i].enablelighting ?
+				(u32)xfregs.alpha[i].hex :
+				(u32)xfregs.alpha[i].matsource) << 15;
 		}
 	}
 	uid->values[4] |= (g_ActiveConfig.bEnablePixelLigting && g_ActiveConfig.backend_info.bSupportsPixelLighting) << 31;
@@ -552,7 +552,7 @@ const char *GeneratePixelShaderCode(DSTALPHA_MODE dstAlphaMode, API_TYPE ApiType
 		}
 		else
 		{
-			for (int i = 0; i < xfregs.numTexGens; ++i)
+			for (unsigned int i = 0; i < xfregs.numTexGen.numTexGens; ++i)
 				WRITE(p, ",\n  in float%d uv%d : TEXCOORD%d", i < 4 ? 4 : 3 , i, i);
 		}
 	}
@@ -591,7 +591,7 @@ const char *GeneratePixelShaderCode(DSTALPHA_MODE dstAlphaMode, API_TYPE ApiType
 	
 	if(g_ActiveConfig.bEnablePixelLigting && g_ActiveConfig.backend_info.bSupportsPixelLighting)
 	{
-		if (xfregs.numTexGens < 7) 
+		if (xfregs.numTexGen.numTexGens < 7) 
 		{
 			WRITE(p,"float3 _norm0 = normalize(Normal.xyz);\n\n");
 			WRITE(p,"float3 pos = float3(clipPos.x,clipPos.y,Normal.w);\n");
@@ -607,10 +607,10 @@ const char *GeneratePixelShaderCode(DSTALPHA_MODE dstAlphaMode, API_TYPE ApiType
 		"float3 ldir, h;\n"
 		"float dist, dist2, attn;\n");
 		// lights/colors
-		for (int j = 0; j < xfregs.nNumChans; j++)
+		for (unsigned int j = 0; j < xfregs.numChan.numColorChans; j++)
 		{
-			const LitChannel& color = xfregs.colChans[j].color;
-			const LitChannel& alpha = xfregs.colChans[j].alpha;
+			const LitChannel& color = xfregs.color[j];
+			const LitChannel& alpha = xfregs.alpha[j];
 
 			WRITE(p, "{\n");
 			
@@ -730,7 +730,7 @@ const char *GeneratePixelShaderCode(DSTALPHA_MODE dstAlphaMode, API_TYPE ApiType
 		for (int i = 0; i < numTexgen; ++i)
 		{
 			// optional perspective divides
-			if (xfregs.texcoords[i].texmtxinfo.projection == XF_TEXPROJ_STQ)
+			if (xfregs.texMtxInfo[i].projection == XF_TEXPROJ_STQ)
 			{
 				WRITE(p, "if (uv%d.z)", i);
 				WRITE(p, "	uv%d.xy = uv%d.xy / uv%d.z;\n", i, i, i);

dolphin-ios/Source/Core/VideoCommon/Src/PixelShaderManager.h
@@ -39,7 +39,7 @@ public:
 	static void SetDestAlpha(const ConstantAlpha& alpha);
 	static void SetTexDims(int texmapid, u32 width, u32 height, u32 wraps, u32 wrapt);
 	static void SetZTextureBias(u32 bias);
-	static void SetViewport(float* viewport,int VIndex = -1);
+	static void SetViewportChanged();
 	static void SetIndMatrixChanged(int matrixidx);
 	static void SetTevKSelChanged(int id);
 	static void SetZTextureTypeChanged();
@@ -50,7 +50,7 @@ public:
 	static void SetFogRangeAdjustChanged();
 	static void SetColorMatrix(const float* pmatrix);
 	static void InvalidateXFRange(int start, int end);
-	static void SetMaterialColor(int index, u32 data);
+	static void SetMaterialColorChanged(int index);
 	
 };
 

commit 0da37dc86f50ed4e3293ebea2a5feab718ed3546

Fix an error introduced in r7083. In the pixel shader manager the farZ and zRange of the z bias were flipped. Switched to using the viewport struct rather than a raw float array to hopefully avoid such confusion in the future. Fixes issue 4060.
git-svn-id: https://dolphin-emu.googlecode.com/svn/trunk@7446 8ced0084-cf51-0410-be5f-012b33b47a6e

dolphin-ios/Source/Core/VideoCommon/Src/PixelShaderManager.cpp
@@ -148,7 +148,7 @@ void PixelShaderManager::SetConstants()
 		// [5] = 16777215 * farz
 
 		//ERROR_LOG("pixel=%x,%x, bias=%x\n", bpmem.zcontrol.pixel_format, bpmem.ztex2.type, lastZBias);
-        SetPSConstant4f(C_ZBIAS+1, xfregs.rawViewport[2] / 16777216.0f, xfregs.rawViewport[5] / 16777216.0f, 0, (float)(lastZBias)/16777215.0f);
+		SetPSConstant4f(C_ZBIAS+1, xfregs.viewport.farZ / 16777216.0f, xfregs.viewport.zRange / 16777216.0f, 0, (float)(lastZBias)/16777215.0f);
 		s_bZBiasChanged = s_bDepthRangeChanged = false;
     }
 
@@ -245,14 +245,14 @@ void PixelShaderManager::SetConstants()
 			//bpmem.fogRange.Base.Center : center of the viewport in x axis. observation: bpmem.fogRange.Base.Center = realcenter + 342;
 			int center = ((u32)bpmem.fogRange.Base.Center) - 342;
 			// normalice center to make calculations easy
-			float ScreenSpaceCenter = center / (2.0f * xfregs.rawViewport[0]);
+			float ScreenSpaceCenter = center / (2.0f * xfregs.viewport.wd);
 			ScreenSpaceCenter = (ScreenSpaceCenter * 2.0f) - 1.0f;
 			//bpmem.fogRange.K seems to be  a table of precalculated coeficients for the adjust factor
 			//observations: bpmem.fogRange.K[0].LO apears to be the lowest value and bpmem.fogRange.K[4].HI the largest
 			// they always seems to be larger than 256 so my teory is :
 			// they are the coeficients from the center to th e border of the screen
 			// so to simplify i use the hi coeficient as K in the shader taking 256 as the scale
-			SetPSConstant4f(C_FOG + 2, ScreenSpaceCenter, (float)Renderer::EFBToScaledX((int)(2.0f * xfregs.rawViewport[0])), bpmem.fogRange.K[4].HI / 256.0f,0.0f);
+			SetPSConstant4f(C_FOG + 2, ScreenSpaceCenter, (float)Renderer::EFBToScaledX((int)(2.0f * xfregs.viewport.wd)), bpmem.fogRange.K[4].HI / 256.0f,0.0f);
 		}		
 		s_bFogRangeAdjustChanged = false;
 	}

dolphin-ios/Source/Core/VideoCommon/Src/VertexShaderManager.cpp
@@ -299,7 +299,7 @@ void VertexShaderManager::SetConstants()
 	if (bViewportChanged)
 	{
 		bViewportChanged = false;
-		SetVSConstant4f(C_DEPTHPARAMS,xfregs.rawViewport[5]/ 16777216.0f,xfregs.rawViewport[2]/ 16777216.0f,0.0f,0.0f);
+		SetVSConstant4f(C_DEPTHPARAMS,xfregs.viewport.farZ / 16777216.0f,xfregs.viewport.zRange / 16777216.0f,0.0f,0.0f);
 		// This is so implementation-dependent that we can't have it here.
 		UpdateViewport();
 	}

dolphin-ios/Source/Core/VideoCommon/Src/XFMemory.h
@@ -229,7 +229,7 @@ struct Viewport
 {
 	float wd;
 	float ht;
-	float nearZ;
+	float zRange;
 	float xOrig;
 	float yOrig;
 	float farZ;
@@ -259,7 +259,7 @@ struct XFRegisters
     u32 unk7;                       // 0x1017
     u32 MatrixIndexA;				// 0x1018
     u32 MatrixIndexB;				// 0x1019
-    float rawViewport[6];           // 0x101a - 0x101f
+    Viewport viewport;              // 0x101a - 0x101f
     float rawProjection[7];         // 0x1020 - 0x1026
     u32 unk8[24];                   // 0x1027 - 0x103e
     NumTexGen numTexGen;            // 0x103f

dolphin-ios/Source/Plugins/Plugin_VideoDX11/Src/Render.cpp
@@ -675,10 +675,10 @@ void Renderer::UpdateViewport()
 
 	// TODO: ceil, floor or just cast to int?
 	// TODO: Directly use the floats instead of rounding them?
-	int X = Renderer::EFBToScaledX((int)ceil(xfregs.rawViewport[3] - xfregs.rawViewport[0] - scissorXOff)) + Renderer::TargetStrideX();
-	int Y = Renderer::EFBToScaledY((int)ceil(xfregs.rawViewport[4] + xfregs.rawViewport[1] - scissorYOff)) + Renderer::TargetStrideY();
-	int Width = Renderer::EFBToScaledX((int)ceil(2.0f * xfregs.rawViewport[0]));
-	int Height = Renderer::EFBToScaledY((int)ceil(-2.0f * xfregs.rawViewport[1]));
+	int X = Renderer::EFBToScaledX((int)ceil(xfregs.viewport.xOrig - xfregs.viewport.wd - scissorXOff)) + Renderer::TargetStrideX();
+	int Y = Renderer::EFBToScaledY((int)ceil(xfregs.viewport.yOrig + xfregs.viewport.ht - scissorYOff)) + Renderer::TargetStrideY();
+	int Width = Renderer::EFBToScaledX((int)ceil(2.0f * xfregs.viewport.wd));
+	int Height = Renderer::EFBToScaledY((int)ceil(-2.0f * xfregs.viewport.ht));
 	if (Width < 0)
 	{
 		X += Width;
@@ -741,8 +741,8 @@ void Renderer::UpdateViewport()
 
 	// Some games set invalids values for z min and z max so fix them to the max an min alowed and let the shaders do this work
 	D3D11_VIEWPORT vp = CD3D11_VIEWPORT(newx, newy, newwidth, newheight,
-										0.f,	// (xfregs.rawViewport[5] - xfregs.rawViewport[2]) / 16777216.0f;
-										1.f);   //  xfregs.rawViewport[5] / 16777216.0f;
+										0.f,	// (xfregs.viewport.farZ - xfregs.viewport.zRange) / 16777216.0f;
+										1.f);   //  xfregs.viewport.farZ / 16777216.0f;
 	D3D::g_context->RSSetViewports(1, &vp);
 }
 

dolphin-ios/Source/Plugins/Plugin_VideoDX11/Src/VertexManager.cpp
@@ -133,8 +133,8 @@ void VertexManager::Draw(UINT stride)
 	{
 		float lineWidth = float(bpmem.lineptwidth.linesize) / 6.f;
 		float texOffset = LINE_PT_TEX_OFFSETS[bpmem.lineptwidth.lineoff];
-		float vpWidth = 2.0f * xfregs.rawViewport[0];
-		float vpHeight = -2.0f * xfregs.rawViewport[1];
+		float vpWidth = 2.0f * xfregs.viewport.wd;
+		float vpHeight = -2.0f * xfregs.viewport.ht;
 
 		if (m_lineShader.SetShader(g_nativeVertexFmt->m_components, lineWidth, texOffset, vpWidth, vpHeight))
 		{
@@ -149,8 +149,8 @@ void VertexManager::Draw(UINT stride)
 	{
 		float pointSize = float(bpmem.lineptwidth.pointsize) / 6.f;
 		float texOffset = LINE_PT_TEX_OFFSETS[bpmem.lineptwidth.pointoff];
-		float vpWidth = 2.0f * xfregs.rawViewport[0];
-		float vpHeight = -2.0f * xfregs.rawViewport[1];
+		float vpWidth = 2.0f * xfregs.viewport.wd;
+		float vpHeight = -2.0f * xfregs.viewport.ht;
 
 		if (m_pointShader.SetShader(g_nativeVertexFmt->m_components, pointSize, texOffset, vpWidth, vpHeight))
 		{

dolphin-ios/Source/Plugins/Plugin_VideoDX9/Src/Render.cpp
@@ -707,10 +707,10 @@ void Renderer::UpdateViewport()
 	int scissorYOff = bpmem.scissorOffset.y << 1;
 
 	// TODO: ceil, floor or just cast to int?
-	int X = EFBToScaledX((int)ceil(xfregs.rawViewport[3] - xfregs.rawViewport[0] - scissorXOff)) + TargetStrideX();
-	int Y = EFBToScaledY((int)ceil(xfregs.rawViewport[4] + xfregs.rawViewport[1] - scissorYOff)) + TargetStrideY();
-	int Width = EFBToScaledX((int)ceil(2.0f * xfregs.rawViewport[0]));
-	int Height = EFBToScaledY((int)ceil(-2.0f * xfregs.rawViewport[1]));
+	int X = EFBToScaledX((int)ceil(xfregs.viewport.xOrig - xfregs.viewport.wd - scissorXOff)) + TargetStrideX();
+	int Y = EFBToScaledY((int)ceil(xfregs.viewport.yOrig + xfregs.viewport.ht - scissorYOff)) + TargetStrideY();
+	int Width = EFBToScaledX((int)ceil(2.0f * xfregs.viewport.wd));
+	int Height = EFBToScaledY((int)ceil(-2.0f * xfregs.viewport.ht));
 	if (Width < 0)
 	{
 		X += Width;
@@ -794,8 +794,8 @@ void Renderer::UpdateViewport()
 	vp.Height = Height;
 	
 	// Some games set invalids values for z min and z max so fix them to the max an min alowed and let the shaders do this work
-	vp.MinZ = 0.0f; // (xfregs.rawViewport[5] - xfregs.rawViewport[2]) / 16777216.0f;
-	vp.MaxZ = 1.0f; // xfregs.rawViewport[5] / 16777216.0f;
+	vp.MinZ = 0.0f; // (xfregs.viewport.farZ - xfregs.viewport.zRange) / 16777216.0f;
+	vp.MaxZ = 1.0f; // xfregs.viewport.farZ / 16777216.0f;
 	D3D::dev->SetViewport(&vp);
 }
 
@@ -1367,7 +1367,7 @@ void Renderer::SetDitherMode()
 void Renderer::SetLineWidth()
 {
 	// We can't change line width in D3D unless we use ID3DXLine
-	float fratio = xfregs.rawViewport[0] != 0 ? Renderer::EFBToScaledXf(1.f) : 1.0f;
+	float fratio = xfregs.viewport.wd != 0 ? Renderer::EFBToScaledXf(1.f) : 1.0f;
 	float psize = bpmem.lineptwidth.linesize * fratio / 6.0f;
 	D3D::SetRenderState(D3DRS_POINTSIZE, *((DWORD*)&psize));
 }

dolphin-ios/Source/Plugins/Plugin_VideoOGL/Src/Render.cpp
@@ -830,12 +830,12 @@ void Renderer::UpdateViewport()
 	int scissorYOff = bpmem.scissorOffset.y << 1;
 
 	// TODO: ceil, floor or just cast to int?
-	int X = EFBToScaledX((int)ceil(xfregs.rawViewport[3] - xfregs.rawViewport[0] - (float)scissorXOff));
-	int Y = EFBToScaledY((int)ceil((float)EFB_HEIGHT - xfregs.rawViewport[4] + xfregs.rawViewport[1] + (float)scissorYOff));
-	int Width = EFBToScaledX((int)ceil(2.0f * xfregs.rawViewport[0]));
-	int Height = EFBToScaledY((int)ceil(-2.0f * xfregs.rawViewport[1]));
-	double GLNear = (xfregs.rawViewport[5] - xfregs.rawViewport[2]) / 16777216.0f;
-	double GLFar = xfregs.rawViewport[5] / 16777216.0f;
+	int X = EFBToScaledX((int)ceil(xfregs.viewport.xOrig - xfregs.viewport.wd - (float)scissorXOff));
+	int Y = EFBToScaledY((int)ceil((float)EFB_HEIGHT - xfregs.viewport.yOrig + xfregs.viewport.ht + (float)scissorYOff));
+	int Width = EFBToScaledX((int)ceil(2.0f * xfregs.viewport.wd));
+	int Height = EFBToScaledY((int)ceil(-2.0f * xfregs.viewport.ht));
+	double GLNear = (xfregs.viewport.farZ - xfregs.viewport.zRange) / 16777216.0f;
+	double GLFar = xfregs.viewport.farZ / 16777216.0f;
 	if (Width < 0)
 	{
 		X += Width;
@@ -1482,7 +1482,7 @@ void Renderer::SetDitherMode()
 
 void Renderer::SetLineWidth()
 {
-	float fratio = xfregs.rawViewport[0] != 0 ?
+	float fratio = xfregs.viewport.wd != 0 ?
 		((float)Renderer::GetTargetWidth() / EFB_WIDTH) : 1.0f;
 	if (bpmem.lineptwidth.linesize > 0)
 		// scale by ratio of widths