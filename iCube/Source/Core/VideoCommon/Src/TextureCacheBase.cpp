kiddreads/Avalon

commit 41c97496aa67c4b26083072bf0143c362c90dea8

Enable hires textures even when texture dumping is enabled. Remove some deprecated code.

iCube/Source/Core/VideoCommon/Src/TextureCacheBase.cpp
@@ -61,28 +61,19 @@ TextureCache::TextureCache()
 	SetHash64Function(g_ActiveConfig.bHiresTextures || g_ActiveConfig.bDumpTextures);
 }
 
-// TODO: Kill shutdown parameter...
-void TextureCache::Invalidate(bool shutdown)
+void TextureCache::Invalidate()
 {
 	TexCache::iterator
 		iter = textures.begin(),
 		tcend = textures.end();
 	for (; iter != tcend; ++iter)
-	{
-		if (shutdown)
-			iter->second->addr = 0;
 		delete iter->second;
-	}
 
 	textures.clear();
-	if(g_ActiveConfig.bHiresTextures && !g_ActiveConfig.bDumpTextures)
-		HiresTextures::Init(SConfig::GetInstance().m_LocalCoreStartupParameter.m_strUniqueID.c_str());
-	SetHash64Function(g_ActiveConfig.bHiresTextures || g_ActiveConfig.bDumpTextures);
 }
 
 TextureCache::~TextureCache()
 {
-	Invalidate(true);
 	if (temp)
 	{
 		FreeAlignedMemory(temp);
@@ -100,7 +91,14 @@ void TextureCache::OnConfigChanged(VideoConfig& config)
 		config.bTexFmtOverlayEnable != backup_config.s_texfmt_overlay ||
 		config.bTexFmtOverlayCenter != backup_config.s_texfmt_overlay_center ||
 		config.bHiresTextures != backup_config.s_hires_textures)
-		g_texture_cache->Invalidate(false);
+	{
+		g_texture_cache->Invalidate();
+
+		if(g_ActiveConfig.bHiresTextures)
+			HiresTextures::Init(SConfig::GetInstance().m_LocalCoreStartupParameter.m_strUniqueID.c_str());
+
+		SetHash64Function(g_ActiveConfig.bHiresTextures || g_ActiveConfig.bDumpTextures);
+	}
 
 	// TODO: Probably shouldn't clear all render targets here, just mark them dirty or something.
 	if (config.bEFBCopyCacheEnable != backup_config.s_copy_cache_enable || // TODO: not sure if this is needed?

iCube/Source/Core/DolphinWX/Src/Debugger/DebuggerPanel.cpp
@@ -334,7 +334,7 @@ void GFXDebuggerPanel::OnClearScreenButton(wxCommandEvent& event)
 
 void GFXDebuggerPanel::OnClearTextureCacheButton(wxCommandEvent& event)
 {
-	TextureCache::Invalidate(false);
+	TextureCache::Invalidate();
 }
 
 void GFXDebuggerPanel::OnClearVertexShaderCacheButton(wxCommandEvent& event)

iCube/Source/Core/VideoCommon/Src/MainBase.cpp
@@ -203,7 +203,7 @@ void VideoBackendHardware::DoState(PointerWrap& p)
 
 		// Clear all caches that touch RAM
 		// (? these don't appear to touch any emulation state that gets saved. moved to on load only.)
-		TextureCache::Invalidate(false);
+		TextureCache::Invalidate();
 		VertexLoaderManager::MarkAllDirty();
 	}
 }

iCube/Source/Core/VideoCommon/Src/TextureCacheBase.h
@@ -105,7 +105,7 @@ public:
 	static void OnConfigChanged(VideoConfig& config);
 	static void Cleanup();
 
-	static void Invalidate(bool shutdown);
+	static void Invalidate();
 	static void InvalidateRange(u32 start_address, u32 size);
 	static void MakeRangeDynamic(u32 start_address, u32 size);
 	static void ClearRenderTargets();	// currently only used by OGL

iCube/Source/Plugins/Plugin_VideoDX9/Src/Render.cpp
@@ -237,7 +237,7 @@ void TeardownDeviceObjects()
 	D3D::dev->SetDepthStencilSurface(D3D::GetBackBufferDepthSurface());
 	delete g_framebuffer_manager;
 	D3D::font.Shutdown();
-	TextureCache::Invalidate(false);
+	TextureCache::Invalidate();
 	VertexLoaderManager::Shutdown();
 	VertexShaderCache::Shutdown();
 	PixelShaderCache::Shutdown();

commit b6a6f5d66f152e0ec0a680e4bcf697047fac961d

Add a missing Invalidate()  call in ~TextureCache(). Fixes a regression from 41c97496aa67 causing textures to load improperly when a game is run two times in the same Dolphin instance

iCube/Source/Core/VideoCommon/Src/TextureCacheBase.cpp
@@ -74,6 +74,7 @@ void TextureCache::Invalidate()
 
 TextureCache::~TextureCache()
 {
+	Invalidate();
 	if (temp)
 	{
 		FreeAlignedMemory(temp);