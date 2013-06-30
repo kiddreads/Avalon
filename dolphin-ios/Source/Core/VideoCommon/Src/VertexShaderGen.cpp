kiddreads/Avalon

commit 829aa55357a931dd8c2ce94f72654a14f301c9e4

VertexShaderGen: Fix a potential bug where vertex shader uids don't change when pixel lighting is toggled.

dolphin-ios/Source/Core/VideoCommon/Src/VertexShaderGen.cpp
@@ -279,8 +279,8 @@ static void GenerateVertexShader(T& out, u32 components, API_TYPE api_type)
 			out.Write("o.colors_0 = float4(1.0f, 1.0f, 1.0f, 1.0f);\n");
 	}
 
-	// TODO: This probably isn't necessary if pixel lighting is enabled.
-	GenerateLightingShader<T>(out, uid_data.lighting, components, I_MATERIALS, I_LIGHTS, "color", "o.colors_");
+	if (g_ActiveConfig.bEnablePixelLighting && g_ActiveConfig.backend_info.bSupportsPixelLighting)
+		GenerateLightingShader<T>(out, uid_data.lighting, components, I_MATERIALS, I_LIGHTS, "color", "o.colors_");
 
 	if (xfregs.numChan.numColorChans < 2)
 	{

commit 3297412d4d9b0d41bbed6858cbd706f515f81ab0

VertexShaderGen: Fix a dumb regression from revision 829aa55357a9.

dolphin-ios/Source/Core/VideoCommon/Src/VertexShaderGen.cpp
@@ -279,7 +279,7 @@ static void GenerateVertexShader(T& out, u32 components, API_TYPE api_type)
 			out.Write("o.colors_0 = float4(1.0f, 1.0f, 1.0f, 1.0f);\n");
 	}
 
-	if (g_ActiveConfig.bEnablePixelLighting && g_ActiveConfig.backend_info.bSupportsPixelLighting)
+	if (!(g_ActiveConfig.bEnablePixelLighting && g_ActiveConfig.backend_info.bSupportsPixelLighting))
 		GenerateLightingShader<T>(out, uid_data.lighting, components, I_MATERIALS, I_LIGHTS, "color", "o.colors_");
 
 	if (xfregs.numChan.numColorChans < 2)