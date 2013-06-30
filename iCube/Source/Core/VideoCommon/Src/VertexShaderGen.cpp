kiddreads/Avalon

commit 7b8c49b5fffa4630318e1df21354d816d524d579

VertexShaderGen: Fix a potential bug where vertex shader uids don't change when pixel lighting is toggled.

iCube/Source/Core/VideoCommon/Src/VertexShaderGen.cpp
@@ -279,8 +279,8 @@ static void GenerateVertexShader(T& out, u32 components, API_TYPE api_type)
 			out.Write("o.colors_0 = float4(1.0f, 1.0f, 1.0f, 1.0f);\n");
 	}
 
-	// TODO: This probably isn't necessary if pixel lighting is enabled.
-	GenerateLightingShader<T>(out, uid_data.lighting, components, I_MATERIALS, I_LIGHTS, "color", "o.colors_");
+	if (g_ActiveConfig.bEnablePixelLighting && g_ActiveConfig.backend_info.bSupportsPixelLighting)
+		GenerateLightingShader<T>(out, uid_data.lighting, components, I_MATERIALS, I_LIGHTS, "color", "o.colors_");
 
 	if (xfregs.numChan.numColorChans < 2)
 	{

commit 1790a4ed441a4f98b61da330eb2391a6e5c011ee

VertexShaderGen: Fix a dumb regression from revision 7b8c49b5fffa.

iCube/Source/Core/VideoCommon/Src/VertexShaderGen.cpp
@@ -279,7 +279,7 @@ static void GenerateVertexShader(T& out, u32 components, API_TYPE api_type)
 			out.Write("o.colors_0 = float4(1.0f, 1.0f, 1.0f, 1.0f);\n");
 	}
 
-	if (g_ActiveConfig.bEnablePixelLighting && g_ActiveConfig.backend_info.bSupportsPixelLighting)
+	if (!(g_ActiveConfig.bEnablePixelLighting && g_ActiveConfig.backend_info.bSupportsPixelLighting))
 		GenerateLightingShader<T>(out, uid_data.lighting, components, I_MATERIALS, I_LIGHTS, "color", "o.colors_");
 
 	if (xfregs.numChan.numColorChans < 2)