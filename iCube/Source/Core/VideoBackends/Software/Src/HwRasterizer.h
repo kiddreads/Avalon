mirror/dolphin-emu

Put Plugins/ in Core/, rename to VideoBackends

Source/Core/VideoBackends/Backend_Software/Src/HwRasterizer.h
@@ -8,7 +8,7 @@
 #include <map>
 
 #include "BPMemLoader.h"
-#include "../../Plugin_VideoOGL/Src/GLUtil.h"
+#include "../../Backend_OGL/Src/GLUtil.h"
 
 struct OutputVertexData;
 

Source/Core/VideoBackends/Backend_Software/Src/SWRenderer.cpp
@@ -5,7 +5,7 @@
 #include "Common.h"
 #include <math.h>
 
-#include "../../Plugin_VideoOGL/Src/GLUtil.h"
+#include "../../Backend_OGL/Src/GLUtil.h"
 #include "RasterFont.h"
 #include "SWRenderer.h"
 #include "SWStatistics.h"

kiddreads/Avalon

iCube/Source/Core/VideoBackends/Software/Src/HwRasterizer.h
@@ -8,7 +8,7 @@
 #include <map>
 
 #include "BPMemLoader.h"
-#include "../../Plugin_VideoOGL/Src/GLUtil.h"
+#include "../../OGL/Src/GLUtil.h"
 
 struct OutputVertexData;
 

iCube/Source/Core/VideoBackends/Software/Src/SWRenderer.cpp
@@ -5,7 +5,7 @@
 #include "Common.h"
 #include <math.h>
 
-#include "../../Plugin_VideoOGL/Src/GLUtil.h"
+#include "../../OGL/Src/GLUtil.h"
 #include "RasterFont.h"
 #include "SWRenderer.h"
 #include "SWStatistics.h"