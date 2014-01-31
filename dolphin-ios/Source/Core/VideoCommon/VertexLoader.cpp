mirror/dolphin-emu

PortableVertexFormat: use AttributeFormat for posmtx attribute format

Source/Core/VideoCommon/VertexLoader.cpp
@@ -463,12 +463,12 @@ void VertexLoader::CompileVertexTranslator()
 	if (m_VtxDesc.PosMatIdx)
 	{
 		WriteCall(PosMtx_Write);
-		vtx_decl.posmtx_offset = nat_offset;
+		vtx_decl.posmtx.components = 4;
+		vtx_decl.posmtx.enable = true;
+		vtx_decl.posmtx.offset = nat_offset;
+		vtx_decl.posmtx.type = VAR_UNSIGNED_BYTE;
+		vtx_decl.posmtx.normalize = false;
 		nat_offset += 4;
-	}
-	else
-	{
-		vtx_decl.posmtx_offset = -1;
 	}
 
 	native_stride = nat_offset;

kiddreads/Avalon

dolphin-ios/Source/Core/VideoCommon/VertexLoader.cpp
@@ -756,12 +756,12 @@ void VertexLoader::CompileVertexTranslator()
 	if (m_VtxDesc.PosMatIdx)
 	{
 		WriteCall(PosMtx_Write);
-		vtx_decl.posmtx_offset = nat_offset;
+		vtx_decl.posmtx.components = 4;
+		vtx_decl.posmtx.enable = true;
+		vtx_decl.posmtx.offset = nat_offset;
+		vtx_decl.posmtx.type = VAR_UNSIGNED_BYTE;
+		vtx_decl.posmtx.integer = false;
 		nat_offset += 4;
-	}
-	else
-	{
-		vtx_decl.posmtx_offset = -1;
 	}
 
 	native_stride = nat_offset;