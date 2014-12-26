litzs123/ppsspp

Add basic infrastructure to time io better.
This allows the filesystems to more accurately control timing.
But they're not actually doing it yet (same timing as before.)

Core/HW/AsyncIOManager.cpp
@@ -57,6 +57,16 @@ bool AsyncIOManager::PopResult(u32 handle, AsyncIOResult &result) {
 	}
 }
 
+bool AsyncIOManager::ReadResult(u32 handle, AsyncIOResult &result) {
+	lock_guard guard(resultsLock_);
+	if (results_.find(handle) != results_.end()) {
+		result = results_[handle];
+		return true;
+	} else {
+		return false;
+	}
+}
+
 bool AsyncIOManager::WaitResult(u32 handle, AsyncIOResult &result) {
 	lock_guard guard(resultsLock_);
 	ScheduleEvent(IO_EVENT_SYNC);
@@ -71,6 +81,24 @@ bool AsyncIOManager::WaitResult(u32 handle, AsyncIOResult &result) {
 	}
 
 	return false;
+}
+
+u64 AsyncIOManager::ResultFinishTicks(u32 handle) {
+	AsyncIOResult result;
+
+	lock_guard guard(resultsLock_);
+	ScheduleEvent(IO_EVENT_SYNC);
+	while (HasEvents() && ThreadEnabled() && resultsPending_.find(handle) != resultsPending_.end()) {
+		if (ReadResult(handle, result)) {
+			return result.finishTicks;
+		}
+		resultsWait_.wait_for(resultsLock_, 16);
+	}
+	if (ReadResult(handle, result)) {
+		return result.finishTicks;
+	}
+
+	return 0;
 }
 
 void AsyncIOManager::ProcessEvent(AsyncIOEvent ev) {
@@ -89,13 +117,15 @@ void AsyncIOManager::ProcessEvent(AsyncIOEvent ev) {
 }
 
 void AsyncIOManager::Read(u32 handle, u8 *buf, size_t bytes) {
-	size_t result = pspFileSystem.ReadFile(handle, buf, bytes);
-	EventResult(handle, result);
+	int usec = 0;
+	size_t result = pspFileSystem.ReadFile(handle, buf, bytes, usec);
+	EventResult(handle, AsyncIOResult(result, usec));
 }
 
 void AsyncIOManager::Write(u32 handle, u8 *buf, size_t bytes) {
-	size_t result = pspFileSystem.WriteFile(handle, buf, bytes);
-	EventResult(handle, result);
+	int usec = 0;
+	size_t result = pspFileSystem.WriteFile(handle, buf, bytes, usec);
+	EventResult(handle, AsyncIOResult(result, usec));
 }
 
 void AsyncIOManager::EventResult(u32 handle, AsyncIOResult result) {
@@ -108,12 +138,20 @@ void AsyncIOManager::EventResult(u32 handle, AsyncIOResult result) {
 }
 
 void AsyncIOManager::DoState(PointerWrap &p) {
-	auto s = p.Section("AsyncIoManager", 1);
+	auto s = p.Section("AsyncIoManager", 1, 2);
 	if (!s)
 		return;
 
 	SyncThread();
 	lock_guard guard(resultsLock_);
 	p.Do(resultsPending_);
-	p.Do(results_);
+	if (s >= 2) {
+		p.Do(results_);
+	} else {
+		std::map<u32, size_t> oldResults;
+		p.Do(oldResults);
+		for (auto it = oldResults.begin(), end = oldResults.end(); it != end; ++it) {
+			results_[it->first] = AsyncIOResult(it->second);
+		}
+	}
 }

kiddreads/Avalon

PPSSPP/Core/HW/AsyncIOManager.cpp
@@ -64,6 +64,16 @@ bool AsyncIOManager::PopResult(u32 handle, AsyncIOResult &result) {
 	}
 }
 
+bool AsyncIOManager::ReadResult(u32 handle, AsyncIOResult &result) {
+	lock_guard guard(resultsLock_);
+	if (results_.find(handle) != results_.end()) {
+		result = results_[handle];
+		return true;
+	} else {
+		return false;
+	}
+}
+
 bool AsyncIOManager::WaitResult(u32 handle, AsyncIOResult &result) {
 	lock_guard guard(resultsLock_);
 	ScheduleEvent(IO_EVENT_SYNC);
@@ -78,6 +88,24 @@ bool AsyncIOManager::WaitResult(u32 handle, AsyncIOResult &result) {
 	}
 
 	return false;
+}
+
+u64 AsyncIOManager::ResultFinishTicks(u32 handle) {
+	AsyncIOResult result;
+
+	lock_guard guard(resultsLock_);
+	ScheduleEvent(IO_EVENT_SYNC);
+	while (HasEvents() && ThreadEnabled() && resultsPending_.find(handle) != resultsPending_.end()) {
+		if (ReadResult(handle, result)) {
+			return result.finishTicks;
+		}
+		resultsWait_.wait_for(resultsLock_, 16);
+	}
+	if (ReadResult(handle, result)) {
+		return result.finishTicks;
+	}
+
+	return 0;
 }
 
 void AsyncIOManager::ProcessEvent(AsyncIOEvent ev) {
@@ -96,14 +124,15 @@ void AsyncIOManager::ProcessEvent(AsyncIOEvent ev) {
 }
 
 void AsyncIOManager::Read(u32 handle, u8 *buf, size_t bytes) {
-	size_t result = pspFileSystem.ReadFile(handle, buf, bytes);
-	EventResult(handle, result);
+	int usec = 0;
+	s64 result = pspFileSystem.ReadFile(handle, buf, bytes, usec);
+	EventResult(handle, AsyncIOResult(result, usec));
 }
 
 void AsyncIOManager::Write(u32 handle, u8 *buf, size_t bytes) {
-	// We want to sign extend this on 32-bit.
-	AsyncIOResult result = (ssize_t)pspFileSystem.WriteFile(handle, buf, bytes);
-	EventResult(handle, result);
+	int usec = 0;
+	s64 result = pspFileSystem.WriteFile(handle, buf, bytes, usec);
+	EventResult(handle, AsyncIOResult(result, usec));
 }
 
 void AsyncIOManager::EventResult(u32 handle, AsyncIOResult result) {
@@ -116,12 +145,20 @@ void AsyncIOManager::EventResult(u32 handle, AsyncIOResult result) {
 }
 
 void AsyncIOManager::DoState(PointerWrap &p) {
-	auto s = p.Section("AsyncIoManager", 1);
+	auto s = p.Section("AsyncIoManager", 1, 2);
 	if (!s)
 		return;
 
 	SyncThread();
 	lock_guard guard(resultsLock_);
 	p.Do(resultsPending_);
-	p.Do(results_);
+	if (s >= 2) {
+		p.Do(results_);
+	} else {
+		std::map<u32, size_t> oldResults;
+		p.Do(oldResults);
+		for (auto it = oldResults.begin(), end = oldResults.end(); it != end; ++it) {
+			results_[it->first] = AsyncIOResult(it->second);
+		}
+	}
 }