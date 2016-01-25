dolphin-emu/dolphin

Fifo: Make g_bSkipCurrentFrame a TU-local variable
This is only ever queried, making it a global isn't necessary.

Source/Core/VideoBackends/Software/DebugUtil.cpp
@@ -191,7 +191,7 @@ void CopyTempBuffer(s16 x, s16 y, int bufferBase, int subBuffer, const char *nam
 
 void OnObjectBegin()
 {
-	if (!Fifo::g_bSkipCurrentFrame)
+	if (!Fifo::CanSkipCurrentFrame())
 	{
 		if (g_ActiveConfig.bDumpTextures && stats.thisFrame.numDrawnObjects >= g_ActiveConfig.drawStart && stats.thisFrame.numDrawnObjects < g_ActiveConfig.drawEnd)
 			DumpActiveTextures();
@@ -200,7 +200,7 @@ void OnObjectBegin()
 
 void OnObjectEnd()
 {
-	if (!Fifo::g_bSkipCurrentFrame)
+	if (!Fifo::CanSkipCurrentFrame())
 	{
 		if (g_ActiveConfig.bDumpObjects && stats.thisFrame.numDrawnObjects >= g_ActiveConfig.drawStart && stats.thisFrame.numDrawnObjects < g_ActiveConfig.drawEnd)
 			DumpEfb(StringFromFormat("%sobject%i.png",

Source/Core/VideoBackends/Software/EfbCopy.cpp
@@ -72,7 +72,7 @@ namespace EfbCopy
 		rc.right = rc.left + (int)bpmem.copyTexSrcWH.x + 1;
 		rc.bottom = rc.top + (int)bpmem.copyTexSrcWH.y + 1;
 
-		if (!Fifo::g_bSkipCurrentFrame)
+		if (!Fifo::CanSkipCurrentFrame())
 		{
 			if (bpmem.triggerEFBCopy.copy_to_xfb)
 			{

Source/Core/VideoBackends/Software/SWRenderer.cpp
@@ -109,7 +109,7 @@ void SWRenderer::UpdateColorTexture(EfbInterface::yuv422_packed *xfb, u32 fbWidt
 // Called on the GPU thread
 void SWRenderer::SwapImpl(u32 xfbAddr, u32 fbWidth, u32 fbStride, u32 fbHeight, const EFBRectangle& rc, float Gamma)
 {
-	if (!Fifo::g_bSkipCurrentFrame)
+	if (!Fifo::CanSkipCurrentFrame())
 	{
 
 		if (g_ActiveConfig.bUseXFB)

Source/Core/VideoCommon/BPStructs.cpp
@@ -380,7 +380,7 @@ static void BPWritten(const BPCmd& bp)
 	case BPMEM_CLEARBBOX2:
 		// Don't compute bounding box if this frame is being skipped!
 		// Wrong but valid values are better than bogus values...
-		if (!Fifo::g_bSkipCurrentFrame)
+		if (!Fifo::CanSkipCurrentFrame())
 		{
 			u8 offset = bp.address & 2;
 			BoundingBox::active = true;

Source/Core/VideoCommon/OpcodeDecoding.cpp
@@ -275,7 +275,7 @@ u8* OpcodeDecoder_Run(DataReader src, u32* cycles, bool in_display_list)
 					(cmd_byte & GX_PRIMITIVE_MASK) >> GX_PRIMITIVE_SHIFT,
 					num_vertices,
 					src,
-					Fifo::g_bSkipCurrentFrame,
+					Fifo::CanSkipCurrentFrame(),
 					is_preprocess);
 
 				if (bytes < 0)

kiddreads/Avalon

dolphin-ios/Source/Core/VideoBackends/Software/DebugUtil.cpp
@@ -191,7 +191,7 @@ void CopyTempBuffer(s16 x, s16 y, int bufferBase, int subBuffer, const char *nam
 
 void OnObjectBegin()
 {
-	if (!Fifo::g_bSkipCurrentFrame)
+	if (!Fifo::WillSkipCurrentFrame())
 	{
 		if (g_ActiveConfig.bDumpTextures && stats.thisFrame.numDrawnObjects >= g_ActiveConfig.drawStart && stats.thisFrame.numDrawnObjects < g_ActiveConfig.drawEnd)
 			DumpActiveTextures();
@@ -200,7 +200,7 @@ void OnObjectBegin()
 
 void OnObjectEnd()
 {
-	if (!Fifo::g_bSkipCurrentFrame)
+	if (!Fifo::WillSkipCurrentFrame())
 	{
 		if (g_ActiveConfig.bDumpObjects && stats.thisFrame.numDrawnObjects >= g_ActiveConfig.drawStart && stats.thisFrame.numDrawnObjects < g_ActiveConfig.drawEnd)
 			DumpEfb(StringFromFormat("%sobject%i.png",

dolphin-ios/Source/Core/VideoBackends/Software/EfbCopy.cpp
@@ -72,7 +72,7 @@ namespace EfbCopy
 		rc.right = rc.left + (int)bpmem.copyTexSrcWH.x + 1;
 		rc.bottom = rc.top + (int)bpmem.copyTexSrcWH.y + 1;
 
-		if (!Fifo::g_bSkipCurrentFrame)
+		if (!Fifo::WillSkipCurrentFrame())
 		{
 			if (bpmem.triggerEFBCopy.copy_to_xfb)
 			{

dolphin-ios/Source/Core/VideoBackends/Software/SWRenderer.cpp
@@ -109,7 +109,7 @@ void SWRenderer::UpdateColorTexture(EfbInterface::yuv422_packed *xfb, u32 fbWidt
 // Called on the GPU thread
 void SWRenderer::SwapImpl(u32 xfbAddr, u32 fbWidth, u32 fbStride, u32 fbHeight, const EFBRectangle& rc, float Gamma)
 {
-	if (!Fifo::g_bSkipCurrentFrame)
+	if (!Fifo::WillSkipCurrentFrame())
 	{
 
 		if (g_ActiveConfig.bUseXFB)

dolphin-ios/Source/Core/VideoCommon/BPStructs.cpp
@@ -380,7 +380,7 @@ static void BPWritten(const BPCmd& bp)
 	case BPMEM_CLEARBBOX2:
 		// Don't compute bounding box if this frame is being skipped!
 		// Wrong but valid values are better than bogus values...
-		if (!Fifo::g_bSkipCurrentFrame)
+		if (!Fifo::WillSkipCurrentFrame())
 		{
 			u8 offset = bp.address & 2;
 			BoundingBox::active = true;

dolphin-ios/Source/Core/VideoCommon/OpcodeDecoding.cpp
@@ -277,7 +277,7 @@ u8* Run(DataReader src, u32* cycles, bool in_display_list)
 					(cmd_byte & GX_PRIMITIVE_MASK) >> GX_PRIMITIVE_SHIFT,
 					num_vertices,
 					src,
-					Fifo::g_bSkipCurrentFrame,
+					Fifo::WillSkipCurrentFrame(),
 					is_preprocess);
 
 				if (bytes < 0)