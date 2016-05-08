dolphin-emu/dolphin

D3D12: Implement XFB encoding/decoding (support Real XFB)

Source/Core/VideoBackends/D3D12/StaticShaderCache.cpp
@@ -18,6 +18,8 @@ static ID3DBlob* s_depth_matrix_program_blob[2] = {};
 static ID3DBlob* s_depth_resolve_to_color_program_blob = {};
 static ID3DBlob* s_clear_program_blob = {};
 static ID3DBlob* s_anaglyph_program_blob = {};
+static ID3DBlob* s_xfb_encode_shader_blob = {};
+static ID3DBlob* s_xfb_decode_shader_blob = {};
 static ID3DBlob* s_rgba6_to_rgb8_program_blob[2] = {};
 static ID3DBlob* s_rgb8_to_rgba6_program_blob[2] = {};
 
@@ -411,6 +413,92 @@ static constexpr const char s_copy_geometry_shader_hlsl[] = {
 	"}\n"
 };
 
+static const char s_xfb_encode_shader_hlsl[] = R"(
+
+Texture2DArray tex0 : register(t0);
+SamplerState samp0 : register(s0);
+
+cbuffer EncodeParams : register(b0)
+{
+	float4 srcRect;
+	float4 texelSize;
+}
+
+// GameCube/Wii uses the BT.601 standard algorithm for converting to YCbCr; see
+// <http://www.equasys.de/colorconversion.html#YCbCr-RGBColorFormatConversion>
+static const float3x4 RGB_TO_YCBCR = float3x4(
+	0.257, 0.504, 0.098, 16.0/255.0,
+	-0.148, -0.291, 0.439, 128.0/255.0,
+	0.439, -0.368, -0.071, 128.0/255.0
+);
+
+void main(
+	out float4 ocol0 : SV_Target,
+	in float4 pos : SV_Position,
+	in float3 uv0 : TEXCOORD0,
+	in float gamma : TEXCOORD1)
+{
+	// Load three input pixels, emulate clamp sampler by clamping to the source rectangle.
+	// Subtract 0.5 from the x coordinate because we're doubling the width, and want the pixel center shifted back to 0.5.
+	float2 baseCoords = lerp(srcRect.xy, srcRect.zw, uv0.xy) - float2(0.5 * texelSize.x, 0);
+	float3 sampleL = tex0.Sample(samp0, float3(max(srcRect.xy, baseCoords - float2(texelSize.x, 0)), 0)).rgb;
+	float3 sampleM = tex0.Sample(samp0, float3(baseCoords, 0)).rgb;
+	float3 sampleR = tex0.Sample(samp0, float3(min(srcRect.zw, baseCoords + float2(texelSize.x, 0)), 0)).rgb;
+
+	// Gamma correction (gamma is already rcp(gamma))
+	// abs() here because the HLSL compiler throws a warning otherwise.
+	sampleL = pow(abs(sampleL), gamma);
+	sampleM = pow(abs(sampleM), gamma);
+	sampleR = pow(abs(sampleR), gamma);
+
+	// RGB -> YUV
+	float3 yuvL = mul(RGB_TO_YCBCR, float4(sampleL,1));
+	float3 yuvM = mul(RGB_TO_YCBCR, float4(sampleM,1));
+	float3 yuvR = mul(RGB_TO_YCBCR, float4(sampleR,1));
+
+	// The Y components correspond to two EFB pixels, while the U and V are
+	// made from a blend of three EFB pixels.
+	float y0 = yuvM.r;
+	float y1 = yuvR.r;
+	float u0 = 0.25*yuvL.g + 0.5*yuvM.g + 0.25*yuvR.g;
+	float v0 = 0.25*yuvL.b + 0.5*yuvM.b + 0.25*yuvR.b;
+	ocol0 = float4(y0, u0, y1, v0);
+}
+
+)";
+
+static const char s_xfb_decode_shader_hlsl[] = R"(
+
+Texture2DArray tex0 : register(t0);
+
+static const float3x3 YCBCR_TO_RGB = float3x3(
+	1.164, 0.000, 1.596,
+	1.164, -0.392, -0.813,
+	1.164, 2.017, 0.000
+);
+
+void main(
+	out float4 ocol0 : SV_Target,
+	in float4 pos : SV_Position,
+	in float3 uv0 : TEXCOORD0)
+{
+	// Divide coordinates by 2 due to half-width YUYV texure.
+	int2 ipos = int2(pos.xy);
+	int2 texpos = int2(ipos.x >> 1, ipos.y);
+	float4 yuyv = tex0.Load(int4(texpos, 0, 0));
+
+	// Select U for even pixels, V for odd pixels.
+	float y = lerp(yuyv.r, yuyv.b, float(ipos.x & 1));
+
+	// Recover RGB components
+	float3 yuv_601_sub = float3(y, yuyv.ga) - float3(16.0/255.0, 128.0/255.0, 128.0/255.0);
+	float3 rgb_601 = mul(YCBCR_TO_RGB, yuv_601_sub);
+
+	ocol0 = float4(rgb_601, 1);
+}
+
+)";
+
 D3D12_SHADER_BYTECODE StaticShaderCache::GetReinterpRGBA6ToRGB8PixelShader(bool multisampled)
 {
 	D3D12_SHADER_BYTECODE bytecode = {};
@@ -625,6 +713,28 @@ D3D12_SHADER_BYTECODE StaticShaderCache::GetCopyGeometryShader()
 	return bytecode;
 }
 
+D3D12_SHADER_BYTECODE StaticShaderCache::GetXFBEncodePixelShader()
+{
+	D3D12_SHADER_BYTECODE bytecode =
+	{
+		s_xfb_encode_shader_blob->GetBufferPointer(),
+		s_xfb_encode_shader_blob->GetBufferSize()
+	};
+
+	return bytecode;
+}
+
+D3D12_SHADER_BYTECODE StaticShaderCache::GetXFBDecodePixelShader()
+{
+	D3D12_SHADER_BYTECODE bytecode =
+	{
+		s_xfb_decode_shader_blob->GetBufferPointer(),
+		s_xfb_decode_shader_blob->GetBufferSize()
+	};
+
+	return bytecode;
+}
+
 void StaticShaderCache::Init()
 {
 	// Compile static pixel shaders
@@ -633,6 +743,8 @@ void StaticShaderCache::Init()
 	D3D::CompilePixelShader(s_color_copy_program_hlsl, &s_color_copy_program_blob[0]);
 	D3D::CompilePixelShader(s_color_matrix_program_hlsl, &s_color_matrix_program_blob[0]);
 	D3D::CompilePixelShader(s_depth_matrix_program_hlsl, &s_depth_matrix_program_blob[0]);
+	D3D::CompilePixelShader(s_xfb_encode_shader_hlsl, &s_xfb_encode_shader_blob);
+	D3D::CompilePixelShader(s_xfb_decode_shader_hlsl, &s_xfb_decode_shader_blob);
 
 	// Compile static vertex shaders
 	D3D::CompileVertexShader(s_simple_vertex_shader_hlsl, &s_simple_vertex_shader_blob);
@@ -657,7 +769,8 @@ void StaticShaderCache::InvalidateMSAAShaders()
 void StaticShaderCache::Shutdown()
 {
 	// Free pixel shader blobs
-
+	SAFE_RELEASE(s_xfb_decode_shader_blob);
+	SAFE_RELEASE(s_xfb_encode_shader_blob);
 	SAFE_RELEASE(s_clear_program_blob);
 	SAFE_RELEASE(s_anaglyph_program_blob);
 	SAFE_RELEASE(s_depth_resolve_to_color_program_blob);

Source/Core/VideoBackends/D3D12/XFBEncoder.cpp
@@ -1,4 +1,4 @@
-// Copyright 2011 Dolphin Emulator Project
+// Copyright 2016 Dolphin Emulator Project
 // Licensed under GPLv2+
 // Refer to the license.txt file included.
 
@@ -6,33 +6,175 @@
 #include "Common/MsgHandler.h"
 #include "Common/Logging/Log.h"
 #include "VideoBackends/D3D12/D3DBase.h"
+#include "VideoBackends/D3D12/D3DCommandListManager.h"
 #include "VideoBackends/D3D12/D3DShader.h"
 #include "VideoBackends/D3D12/D3DState.h"
+#include "VideoBackends/D3D12/D3DUtil.h"
 #include "VideoBackends/D3D12/FramebufferManager.h"
 #include "VideoBackends/D3D12/Render.h"
+#include "VideoBackends/D3D12/StaticShaderCache.h"
 #include "VideoBackends/D3D12/XFBEncoder.h"
-
-// D3D12TODO: Convert this file..
 
 namespace DX12
 {
 
+// YUYV data is packed into half-width RGBA, with Y values in (R,B) and UV in (G,A)
+constexpr size_t XFB_TEXTURE_WIDTH = MAX_XFB_WIDTH / 2;
+constexpr size_t XFB_TEXTURE_HEIGHT = MAX_XFB_HEIGHT;
+
+// Buffer enough space for 2 XFB buffers (our frame latency)
+constexpr size_t XFB_UPLOAD_BUFFER_SIZE = D3D::AlignValue(XFB_TEXTURE_WIDTH * sizeof(u32), D3D12_TEXTURE_DATA_PITCH_ALIGNMENT) * XFB_TEXTURE_HEIGHT * 2;
+constexpr size_t XFB_ENCODER_PARAMS_BUFFER_SIZE = 64 * 1024;
+
+std::unique_ptr<XFBEncoder> g_xfb_encoder;
+
 XFBEncoder::XFBEncoder()
-{ }
-
-void XFBEncoder::Init()
 {
-	// D3D12TODO: Convert this file..
+	ID3D12Resource* texture;
+
+	CheckHR(D3D::device12->CreateCommittedResource(
+		&CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_DEFAULT),
+		D3D12_HEAP_FLAG_NONE,
+		&CD3DX12_RESOURCE_DESC::Tex2D(DXGI_FORMAT_R8G8B8A8_UNORM, XFB_TEXTURE_WIDTH, XFB_TEXTURE_HEIGHT, 1, 1, 1, 0, D3D12_RESOURCE_FLAG_ALLOW_RENDER_TARGET),
+		D3D12_RESOURCE_STATE_RENDER_TARGET,
+		nullptr,
+		IID_PPV_ARGS(&texture)));
+
+	m_yuyv_texture = new D3DTexture2D(texture,
+		(D3D11_BIND_FLAG)(D3D11_BIND_SHADER_RESOURCE | D3D11_BIND_RENDER_TARGET),
+		DXGI_FORMAT_R8G8B8A8_UNORM, DXGI_FORMAT_UNKNOWN, DXGI_FORMAT_R8G8B8A8_UNORM);
+	SAFE_RELEASE(texture);
+
+	CheckHR(D3D::device12->CreateCommittedResource(
+		&CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_READBACK),
+		D3D12_HEAP_FLAG_NONE,
+		&CD3DX12_RESOURCE_DESC::Buffer(D3D::AlignValue(XFB_TEXTURE_WIDTH * sizeof(u32), D3D12_TEXTURE_DATA_PITCH_ALIGNMENT) * MAX_XFB_HEIGHT),
+		D3D12_RESOURCE_STATE_COPY_DEST,
+		nullptr,
+		IID_PPV_ARGS(&m_readback_buffer)));
+
+	m_upload_buffer = std::make_unique<D3DStreamBuffer>(XFB_UPLOAD_BUFFER_SIZE, XFB_UPLOAD_BUFFER_SIZE, nullptr);
+	m_encode_params_buffer = std::make_unique<D3DStreamBuffer>(XFB_ENCODER_PARAMS_BUFFER_SIZE, XFB_ENCODER_PARAMS_BUFFER_SIZE, nullptr);
 }
 
-void XFBEncoder::Shutdown()
+XFBEncoder::~XFBEncoder()
 {
-	// D3D12TODO: Convert this file..
+	SAFE_RELEASE(m_yuyv_texture);
+	SAFE_RELEASE(m_readback_buffer);
 }
 
-void XFBEncoder::Encode(u8* dst, u32 width, u32 height, const EFBRectangle& srcRect, float gamma)
+void XFBEncoder::EncodeTextureToRam(u8* dst, u32 dst_pitch, u32 dst_height,
+	                                D3DTexture2D* src_texture, const TargetRectangle& src_rect,
+	                                u32 src_width, u32 src_height, float gamma)
 {
-	// D3D12TODO: Convert this file..
+	// src_rect is in native coordinates
+	// dst_pitch is in words
+	u32 dst_width = dst_pitch / 2;
+	u32 dst_texture_width = dst_width / 2;
+	_assert_msg_(VIDEO, dst_width <= MAX_XFB_WIDTH && dst_height <= MAX_XFB_HEIGHT, "XFB destination does not exceed maximum size");
+
+	// Encode parameters constant buffer used by shader
+	struct EncodeParameters
+	{
+		float src_rect[4];
+		float texel_size[4];
+	};
+	EncodeParameters parameters =
+	{
+		{
+			static_cast<float>(src_rect.left) / static_cast<float>(src_width),
+			static_cast<float>(src_rect.top) / static_cast<float>(src_height),
+			static_cast<float>(src_rect.right) / static_cast<float>(src_width),
+			static_cast<float>(src_rect.bottom) / static_cast<float>(src_height)
+		},
+		{
+			1.0f / static_cast<float>(src_width),
+			1.0f / static_cast<float>(src_height),
+			0.0f,
+			0.0f
+		}
+	};
+	m_encode_params_buffer->AllocateSpaceInBuffer(sizeof(parameters), D3D12_CONSTANT_BUFFER_DATA_PLACEMENT_ALIGNMENT);
+	memcpy(m_encode_params_buffer->GetCPUAddressOfCurrentAllocation(), &parameters, sizeof(parameters));
+
+	// Convert RGBA texture to YUYV intermediate texture.
+	// Performs downscaling through a linear filter. Probably not ideal, but it's not going to look perfect anyway.
+	CD3DX12_RECT src_texture_rect(src_rect.left, src_rect.top, src_rect.right, src_rect.bottom);
+	D3D12_RESOURCE_STATES src_texture_state = src_texture->GetResourceUsageState();
+	m_yuyv_texture->TransitionToResourceState(D3D::current_command_list, D3D12_RESOURCE_STATE_RENDER_TARGET);
+	D3D::current_command_list->OMSetRenderTargets(1, &m_yuyv_texture->GetRTV12(), FALSE, nullptr);
+	D3D::current_command_list->SetGraphicsRootConstantBufferView(DESCRIPTOR_TABLE_PS_CBVONE, m_encode_params_buffer->GetGPUAddressOfCurrentAllocation());
+	D3D::command_list_mgr->SetCommandListDirtyState(COMMAND_LIST_STATE_PS_CBV, true);
+	D3D::SetViewportAndScissor(0, 0, dst_texture_width, dst_height);
+	D3D::SetLinearCopySampler();
+	D3D::DrawShadedTexQuad(
+		src_texture, &src_texture_rect, src_rect.GetWidth(), src_rect.GetHeight(),
+		StaticShaderCache::GetXFBEncodePixelShader(), StaticShaderCache::GetSimpleVertexShader(), StaticShaderCache::GetSimpleVertexShaderInputLayout(),
+		{}, gamma, 0, DXGI_FORMAT_R8G8B8A8_UNORM, false, false);
+
+	src_texture->TransitionToResourceState(D3D::current_command_list, src_texture_state);
+
+	// Copy from YUYV intermediate texture to readback buffer. It's likely the pitch here is going to be different to dst_pitch.
+	u32 readback_pitch = D3D::AlignValue(dst_width * sizeof(u16), D3D12_TEXTURE_DATA_PITCH_ALIGNMENT);
+	D3D12_PLACED_SUBRESOURCE_FOOTPRINT dst_footprint = { 0, { DXGI_FORMAT_R8G8B8A8_UNORM, dst_texture_width, dst_height, 1, readback_pitch } };
+	CD3DX12_TEXTURE_COPY_LOCATION dst_location(m_readback_buffer, dst_footprint);
+	CD3DX12_TEXTURE_COPY_LOCATION src_location(m_yuyv_texture->GetTex12(), 0);
+	CD3DX12_BOX src_box(0, 0, dst_texture_width, dst_height);
+	m_yuyv_texture->TransitionToResourceState(D3D::current_command_list, D3D12_RESOURCE_STATE_COPY_SOURCE);
+	D3D::current_command_list->CopyTextureRegion(&dst_location, 0, 0, 0, &src_location, &src_box);
+
+	// Wait until the GPU completes the copy. Resets back to known state automatically.
+	D3D::command_list_mgr->ExecuteQueuedWork(true);
+
+	// Copy from the readback buffer to dst.
+	// Can't be done as one memcpy due to pitch difference.
+	void* readback_texture_map;
+	CheckHR(m_readback_buffer->Map(0, nullptr, &readback_texture_map));
+
+	for (u32 row = 0; row < dst_height; row++)
+	{
+		const u8* row_src = reinterpret_cast<u8*>(readback_texture_map) + readback_pitch * row;
+		u8* row_dst = dst + dst_pitch * row;
+		memcpy(row_dst, row_src, std::min(dst_pitch, readback_pitch));
+	}
+
+	m_readback_buffer->Unmap(0, nullptr);
+}
+
+void XFBEncoder::DecodeToTexture(D3DTexture2D* dst_texture, const u8* src, u32 src_width, u32 src_height)
+{
+	_assert_msg_(VIDEO, src_width <= MAX_XFB_WIDTH && src_height <= MAX_XFB_HEIGHT, "XFB source does not exceed maximum size");
+
+	// Copy to XFB upload buffer. Each row has to be done separately due to pitch differences.
+	u32 buffer_pitch = D3D::AlignValue(src_width / 2 * sizeof(u32), D3D12_TEXTURE_DATA_PITCH_ALIGNMENT);
+	m_upload_buffer->AllocateSpaceInBuffer(buffer_pitch * src_height, D3D12_TEXTURE_DATA_PLACEMENT_ALIGNMENT);
+	for (u32 row = 0; row < src_height; row++)
+	{
+		const u8* row_src = src + (src_width * 2) * row;
+		u8* row_dst = reinterpret_cast<u8*>(m_upload_buffer->GetCPUAddressOfCurrentAllocation()) + buffer_pitch * row;
+		memcpy(row_dst, row_src, src_width * 2);
+	}
+
+	// Copy from upload buffer to intermediate YUYV texture.
+	D3D12_PLACED_SUBRESOURCE_FOOTPRINT src_footprint = { m_upload_buffer->GetOffsetOfCurrentAllocation(), { DXGI_FORMAT_R8G8B8A8_UNORM, src_width / 2, src_height, 1, buffer_pitch } };
+	CD3DX12_TEXTURE_COPY_LOCATION src_location(m_upload_buffer->GetBuffer(), src_footprint);
+	CD3DX12_TEXTURE_COPY_LOCATION dst_location(m_yuyv_texture->GetTex12(), 0);
+	CD3DX12_BOX src_box(0, 0, src_width / 2, src_height);
+	m_yuyv_texture->TransitionToResourceState(D3D::current_command_list, D3D12_RESOURCE_STATE_COPY_DEST);
+	D3D::current_command_list->CopyTextureRegion(&dst_location, 0, 0, 0, &src_location, &src_box);
+
+	// Convert YUYV texture to RGBA texture with pixel shader.
+	CD3DX12_RECT src_texture_rect(0, 0, src_width / 2, src_height);
+	dst_texture->TransitionToResourceState(D3D::current_command_list, D3D12_RESOURCE_STATE_RENDER_TARGET);
+	D3D::current_command_list->OMSetRenderTargets(1, &dst_texture->GetRTV12(), FALSE, nullptr);
+	D3D::SetViewportAndScissor(0, 0, src_width, src_height);
+	D3D::DrawShadedTexQuad(
+		m_yuyv_texture, &src_texture_rect, XFB_TEXTURE_WIDTH, XFB_TEXTURE_HEIGHT,
+		StaticShaderCache::GetXFBDecodePixelShader(), StaticShaderCache::GetSimpleVertexShader(), StaticShaderCache::GetSimpleVertexShaderInputLayout(),
+		{}, 1.0f, 0, DXGI_FORMAT_R8G8B8A8_UNORM, false, false);
+
+	// XFB source textures are expected to be in shader resource state.
+	dst_texture->TransitionToResourceState(D3D::current_command_list, D3D12_RESOURCE_STATE_PIXEL_SHADER_RESOURCE);
 }
 
 }

kiddreads/Avalon

dolphin-ios/Source/Core/VideoBackends/D3D12/StaticShaderCache.cpp
@@ -18,6 +18,8 @@ static ID3DBlob* s_depth_matrix_program_blob[2] = {};
 static ID3DBlob* s_depth_resolve_to_color_program_blob = {};
 static ID3DBlob* s_clear_program_blob = {};
 static ID3DBlob* s_anaglyph_program_blob = {};
+static ID3DBlob* s_xfb_encode_shader_blob = {};
+static ID3DBlob* s_xfb_decode_shader_blob = {};
 static ID3DBlob* s_rgba6_to_rgb8_program_blob[2] = {};
 static ID3DBlob* s_rgb8_to_rgba6_program_blob[2] = {};
 
@@ -411,6 +413,93 @@ static constexpr const char s_copy_geometry_shader_hlsl[] = {
 	"}\n"
 };
 
+static const char s_xfb_encode_shader_hlsl[] = R"(
+
+Texture2DArray tex0 : register(t0);
+SamplerState samp0 : register(s0);
+
+cbuffer EncodeParams : register(b0)
+{
+	float4 srcRect;
+	float2 texelSize;
+}
+
+// GameCube/Wii uses the BT.601 standard algorithm for converting to YCbCr; see
+// <http://www.equasys.de/colorconversion.html#YCbCr-RGBColorFormatConversion>
+static const float3x4 RGB_TO_YCBCR = float3x4(
+	0.257, 0.504, 0.098, 16.0/255.0,
+	-0.148, -0.291, 0.439, 128.0/255.0,
+	0.439, -0.368, -0.071, 128.0/255.0
+);
+
+void main(
+	out float4 ocol0 : SV_Target,
+	in float4 pos : SV_Position,
+	in float3 uv0 : TEXCOORD0,
+	in float gamma : TEXCOORD1)
+{
+	// Load three input pixels, emulate clamp sampler by clamping to the source rectangle.
+	// Subtract 0.5 from the x coordinate because we're doubling the width, and want the pixel center shifted back to 0.5.
+	// The native resolution is used as a reference here so bilinear filtering works as expected.
+	float2 baseCoords = lerp(srcRect.xy, srcRect.zw, float2(uv0.x - 0.5 * texelSize.x, uv0.y));
+	float3 sampleL = tex0.Sample(samp0, float3(max(srcRect.xy, baseCoords - float2(texelSize.x, 0)), 0)).rgb;
+	float3 sampleM = tex0.Sample(samp0, float3(baseCoords, 0)).rgb;
+	float3 sampleR = tex0.Sample(samp0, float3(min(srcRect.zw, baseCoords + float2(texelSize.x, 0)), 0)).rgb;
+
+	// Gamma correction (gamma is already rcp(gamma))
+	// abs() here because the HLSL compiler throws a warning otherwise.
+	sampleL = pow(abs(sampleL), gamma);
+	sampleM = pow(abs(sampleM), gamma);
+	sampleR = pow(abs(sampleR), gamma);
+
+	// RGB -> YUV
+	float3 yuvL = mul(RGB_TO_YCBCR, float4(sampleL,1));
+	float3 yuvM = mul(RGB_TO_YCBCR, float4(sampleM,1));
+	float3 yuvR = mul(RGB_TO_YCBCR, float4(sampleR,1));
+
+	// The Y components correspond to two EFB pixels, while the U and V are
+	// made from a blend of three EFB pixels.
+	float y0 = yuvM.r;
+	float y1 = yuvR.r;
+	float u0 = 0.25*yuvL.g + 0.5*yuvM.g + 0.25*yuvR.g;
+	float v0 = 0.25*yuvL.b + 0.5*yuvM.b + 0.25*yuvR.b;
+	ocol0 = float4(y0, u0, y1, v0);
+}
+
+)";
+
+static const char s_xfb_decode_shader_hlsl[] = R"(
+
+Texture2DArray tex0 : register(t0);
+
+static const float3x3 YCBCR_TO_RGB = float3x3(
+	1.164, 0.000, 1.596,
+	1.164, -0.392, -0.813,
+	1.164, 2.017, 0.000
+);
+
+void main(
+	out float4 ocol0 : SV_Target,
+	in float4 pos : SV_Position,
+	in float3 uv0 : TEXCOORD0)
+{
+	// Divide coordinates by 2 due to half-width YUYV texure.
+	int2 ipos = int2(pos.xy);
+	int2 texpos = int2(ipos.x >> 1, ipos.y);
+	float4 yuyv = tex0.Load(int4(texpos, 0, 0));
+
+	// Select U for even pixels, V for odd pixels.
+	float y = lerp(yuyv.r, yuyv.b, float(ipos.x & 1));
+
+	// Recover RGB components
+	float3 yuv_601_sub = float3(y, yuyv.ga) - float3(16.0/255.0, 128.0/255.0, 128.0/255.0);
+	float3 rgb_601 = mul(YCBCR_TO_RGB, yuv_601_sub);
+
+	ocol0 = float4(rgb_601, 1);
+}
+
+)";
+
 D3D12_SHADER_BYTECODE StaticShaderCache::GetReinterpRGBA6ToRGB8PixelShader(bool multisampled)
 {
 	D3D12_SHADER_BYTECODE bytecode = {};
@@ -625,6 +714,28 @@ D3D12_SHADER_BYTECODE StaticShaderCache::GetCopyGeometryShader()
 	return bytecode;
 }
 
+D3D12_SHADER_BYTECODE StaticShaderCache::GetXFBEncodePixelShader()
+{
+	D3D12_SHADER_BYTECODE bytecode =
+	{
+		s_xfb_encode_shader_blob->GetBufferPointer(),
+		s_xfb_encode_shader_blob->GetBufferSize()
+	};
+
+	return bytecode;
+}
+
+D3D12_SHADER_BYTECODE StaticShaderCache::GetXFBDecodePixelShader()
+{
+	D3D12_SHADER_BYTECODE bytecode =
+	{
+		s_xfb_decode_shader_blob->GetBufferPointer(),
+		s_xfb_decode_shader_blob->GetBufferSize()
+	};
+
+	return bytecode;
+}
+
 void StaticShaderCache::Init()
 {
 	// Compile static pixel shaders
@@ -633,6 +744,8 @@ void StaticShaderCache::Init()
 	D3D::CompilePixelShader(s_color_copy_program_hlsl, &s_color_copy_program_blob[0]);
 	D3D::CompilePixelShader(s_color_matrix_program_hlsl, &s_color_matrix_program_blob[0]);
 	D3D::CompilePixelShader(s_depth_matrix_program_hlsl, &s_depth_matrix_program_blob[0]);
+	D3D::CompilePixelShader(s_xfb_encode_shader_hlsl, &s_xfb_encode_shader_blob);
+	D3D::CompilePixelShader(s_xfb_decode_shader_hlsl, &s_xfb_decode_shader_blob);
 
 	// Compile static vertex shaders
 	D3D::CompileVertexShader(s_simple_vertex_shader_hlsl, &s_simple_vertex_shader_blob);
@@ -657,7 +770,8 @@ void StaticShaderCache::InvalidateMSAAShaders()
 void StaticShaderCache::Shutdown()
 {
 	// Free pixel shader blobs
-
+	SAFE_RELEASE(s_xfb_decode_shader_blob);
+	SAFE_RELEASE(s_xfb_encode_shader_blob);
 	SAFE_RELEASE(s_clear_program_blob);
 	SAFE_RELEASE(s_anaglyph_program_blob);
 	SAFE_RELEASE(s_depth_resolve_to_color_program_blob);

dolphin-ios/Source/Core/VideoBackends/D3D12/XFBEncoder.cpp
@@ -1,4 +1,4 @@
-// Copyright 2011 Dolphin Emulator Project
+// Copyright 2016 Dolphin Emulator Project
 // Licensed under GPLv2+
 // Refer to the license.txt file included.
 
@@ -6,33 +6,178 @@
 #include "Common/MsgHandler.h"
 #include "Common/Logging/Log.h"
 #include "VideoBackends/D3D12/D3DBase.h"
+#include "VideoBackends/D3D12/D3DCommandListManager.h"
 #include "VideoBackends/D3D12/D3DShader.h"
 #include "VideoBackends/D3D12/D3DState.h"
+#include "VideoBackends/D3D12/D3DUtil.h"
 #include "VideoBackends/D3D12/FramebufferManager.h"
 #include "VideoBackends/D3D12/Render.h"
+#include "VideoBackends/D3D12/StaticShaderCache.h"
 #include "VideoBackends/D3D12/XFBEncoder.h"
-
-// D3D12TODO: Convert this file..
 
 namespace DX12
 {
 
+// YUYV data is packed into half-width RGBA, with Y values in (R,B) and UV in (G,A)
+constexpr size_t XFB_TEXTURE_WIDTH = MAX_XFB_WIDTH / 2;
+constexpr size_t XFB_TEXTURE_HEIGHT = MAX_XFB_HEIGHT;
+
+// Buffer enough space for 2 XFB buffers (our frame latency)
+constexpr size_t XFB_UPLOAD_BUFFER_SIZE = D3D::AlignValue(XFB_TEXTURE_WIDTH * sizeof(u32), D3D12_TEXTURE_DATA_PITCH_ALIGNMENT) * XFB_TEXTURE_HEIGHT * 2;
+constexpr size_t XFB_ENCODER_PARAMS_BUFFER_SIZE = 64 * 1024;
+
+std::unique_ptr<XFBEncoder> g_xfb_encoder;
+
 XFBEncoder::XFBEncoder()
-{ }
-
-void XFBEncoder::Init()
 {
-	// D3D12TODO: Convert this file..
+	ID3D12Resource* texture;
+
+	CheckHR(D3D::device12->CreateCommittedResource(
+		&CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_DEFAULT),
+		D3D12_HEAP_FLAG_NONE,
+		&CD3DX12_RESOURCE_DESC::Tex2D(DXGI_FORMAT_R8G8B8A8_UNORM, XFB_TEXTURE_WIDTH, XFB_TEXTURE_HEIGHT, 1, 1, 1, 0, D3D12_RESOURCE_FLAG_ALLOW_RENDER_TARGET),
+		D3D12_RESOURCE_STATE_RENDER_TARGET,
+		nullptr,
+		IID_PPV_ARGS(&texture)));
+
+	m_yuyv_texture = new D3DTexture2D(texture,
+		(D3D11_BIND_FLAG)(D3D11_BIND_SHADER_RESOURCE | D3D11_BIND_RENDER_TARGET),
+		DXGI_FORMAT_R8G8B8A8_UNORM, DXGI_FORMAT_UNKNOWN, DXGI_FORMAT_R8G8B8A8_UNORM);
+	SAFE_RELEASE(texture);
+
+	CheckHR(D3D::device12->CreateCommittedResource(
+		&CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_READBACK),
+		D3D12_HEAP_FLAG_NONE,
+		&CD3DX12_RESOURCE_DESC::Buffer(D3D::AlignValue(XFB_TEXTURE_WIDTH * sizeof(u32), D3D12_TEXTURE_DATA_PITCH_ALIGNMENT) * MAX_XFB_HEIGHT),
+		D3D12_RESOURCE_STATE_COPY_DEST,
+		nullptr,
+		IID_PPV_ARGS(&m_readback_buffer)));
+
+	m_upload_buffer = std::make_unique<D3DStreamBuffer>(XFB_UPLOAD_BUFFER_SIZE, XFB_UPLOAD_BUFFER_SIZE, nullptr);
+	m_encode_params_buffer = std::make_unique<D3DStreamBuffer>(XFB_ENCODER_PARAMS_BUFFER_SIZE, XFB_ENCODER_PARAMS_BUFFER_SIZE, nullptr);
 }
 
-void XFBEncoder::Shutdown()
+XFBEncoder::~XFBEncoder()
 {
-	// D3D12TODO: Convert this file..
+	SAFE_RELEASE(m_yuyv_texture);
+	SAFE_RELEASE(m_readback_buffer);
 }
 
-void XFBEncoder::Encode(u8* dst, u32 width, u32 height, const EFBRectangle& srcRect, float gamma)
+void XFBEncoder::EncodeTextureToRam(u8* dst, u32 dst_pitch, u32 dst_height,
+	                                D3DTexture2D* src_texture, const TargetRectangle& src_rect,
+	                                u32 src_width, u32 src_height, float gamma)
 {
-	// D3D12TODO: Convert this file..
+	// src_rect is in native coordinates
+	// dst_pitch is in words
+	u32 dst_width = dst_pitch / 2;
+	u32 dst_texture_width = dst_width / 2;
+	_assert_msg_(VIDEO, dst_width <= MAX_XFB_WIDTH && dst_height <= MAX_XFB_HEIGHT, "XFB destination does not exceed maximum size");
+
+	// Encode parameters constant buffer used by shader
+	struct EncodeParameters
+	{
+		float srcRect[4];
+		float texelSize[2];
+		float pad[2];
+	};
+	EncodeParameters parameters =
+	{
+		{
+			static_cast<float>(src_rect.left) / static_cast<float>(src_width),
+			static_cast<float>(src_rect.top) / static_cast<float>(src_height),
+			static_cast<float>(src_rect.right) / static_cast<float>(src_width),
+			static_cast<float>(src_rect.bottom) / static_cast<float>(src_height)
+		},
+		{
+			1.0f / EFB_WIDTH,
+			1.0f / EFB_HEIGHT
+		},
+		{
+			0.0f,
+			0.0f
+		}
+	};
+	m_encode_params_buffer->AllocateSpaceInBuffer(sizeof(parameters), D3D12_CONSTANT_BUFFER_DATA_PLACEMENT_ALIGNMENT);
+	memcpy(m_encode_params_buffer->GetCPUAddressOfCurrentAllocation(), &parameters, sizeof(parameters));
+
+	// Convert RGBA texture to YUYV intermediate texture.
+	// Performs downscaling through a linear filter. Probably not ideal, but it's not going to look perfect anyway.
+	CD3DX12_RECT src_texture_rect(src_rect.left, src_rect.top, src_rect.right, src_rect.bottom);
+	D3D12_RESOURCE_STATES src_texture_state = src_texture->GetResourceUsageState();
+	m_yuyv_texture->TransitionToResourceState(D3D::current_command_list, D3D12_RESOURCE_STATE_RENDER_TARGET);
+	D3D::current_command_list->OMSetRenderTargets(1, &m_yuyv_texture->GetRTV12(), FALSE, nullptr);
+	D3D::current_command_list->SetGraphicsRootConstantBufferView(DESCRIPTOR_TABLE_PS_CBVONE, m_encode_params_buffer->GetGPUAddressOfCurrentAllocation());
+	D3D::command_list_mgr->SetCommandListDirtyState(COMMAND_LIST_STATE_PS_CBV, true);
+	D3D::SetViewportAndScissor(0, 0, dst_texture_width, dst_height);
+	D3D::SetLinearCopySampler();
+	D3D::DrawShadedTexQuad(
+		src_texture, &src_texture_rect, src_rect.GetWidth(), src_rect.GetHeight(),
+		StaticShaderCache::GetXFBEncodePixelShader(), StaticShaderCache::GetSimpleVertexShader(), StaticShaderCache::GetSimpleVertexShaderInputLayout(),
+		{}, gamma, 0, DXGI_FORMAT_R8G8B8A8_UNORM, false, false);
+
+	src_texture->TransitionToResourceState(D3D::current_command_list, src_texture_state);
+
+	// Copy from YUYV intermediate texture to readback buffer. It's likely the pitch here is going to be different to dst_pitch.
+	u32 readback_pitch = D3D::AlignValue(dst_width * sizeof(u16), D3D12_TEXTURE_DATA_PITCH_ALIGNMENT);
+	D3D12_PLACED_SUBRESOURCE_FOOTPRINT dst_footprint = { 0, { DXGI_FORMAT_R8G8B8A8_UNORM, dst_texture_width, dst_height, 1, readback_pitch } };
+	CD3DX12_TEXTURE_COPY_LOCATION dst_location(m_readback_buffer, dst_footprint);
+	CD3DX12_TEXTURE_COPY_LOCATION src_location(m_yuyv_texture->GetTex12(), 0);
+	CD3DX12_BOX src_box(0, 0, dst_texture_width, dst_height);
+	m_yuyv_texture->TransitionToResourceState(D3D::current_command_list, D3D12_RESOURCE_STATE_COPY_SOURCE);
+	D3D::current_command_list->CopyTextureRegion(&dst_location, 0, 0, 0, &src_location, &src_box);
+
+	// Wait until the GPU completes the copy. Resets back to known state automatically.
+	D3D::command_list_mgr->ExecuteQueuedWork(true);
+
+	// Copy from the readback buffer to dst.
+	// Can't be done as one memcpy due to pitch difference.
+	void* readback_texture_map;
+	CheckHR(m_readback_buffer->Map(0, nullptr, &readback_texture_map));
+
+	for (u32 row = 0; row < dst_height; row++)
+	{
+		const u8* row_src = reinterpret_cast<u8*>(readback_texture_map) + readback_pitch * row;
+		u8* row_dst = dst + dst_pitch * row;
+		memcpy(row_dst, row_src, std::min(dst_pitch, readback_pitch));
+	}
+
+	m_readback_buffer->Unmap(0, nullptr);
+}
+
+void XFBEncoder::DecodeToTexture(D3DTexture2D* dst_texture, const u8* src, u32 src_width, u32 src_height)
+{
+	_assert_msg_(VIDEO, src_width <= MAX_XFB_WIDTH && src_height <= MAX_XFB_HEIGHT, "XFB source does not exceed maximum size");
+
+	// Copy to XFB upload buffer. Each row has to be done separately due to pitch differences.
+	u32 buffer_pitch = D3D::AlignValue(src_width / 2 * sizeof(u32), D3D12_TEXTURE_DATA_PITCH_ALIGNMENT);
+	m_upload_buffer->AllocateSpaceInBuffer(buffer_pitch * src_height, D3D12_TEXTURE_DATA_PLACEMENT_ALIGNMENT);
+	for (u32 row = 0; row < src_height; row++)
+	{
+		const u8* row_src = src + (src_width * 2) * row;
+		u8* row_dst = reinterpret_cast<u8*>(m_upload_buffer->GetCPUAddressOfCurrentAllocation()) + buffer_pitch * row;
+		memcpy(row_dst, row_src, src_width * 2);
+	}
+
+	// Copy from upload buffer to intermediate YUYV texture.
+	D3D12_PLACED_SUBRESOURCE_FOOTPRINT src_footprint = { m_upload_buffer->GetOffsetOfCurrentAllocation(), { DXGI_FORMAT_R8G8B8A8_UNORM, src_width / 2, src_height, 1, buffer_pitch } };
+	CD3DX12_TEXTURE_COPY_LOCATION src_location(m_upload_buffer->GetBuffer(), src_footprint);
+	CD3DX12_TEXTURE_COPY_LOCATION dst_location(m_yuyv_texture->GetTex12(), 0);
+	CD3DX12_BOX src_box(0, 0, src_width / 2, src_height);
+	m_yuyv_texture->TransitionToResourceState(D3D::current_command_list, D3D12_RESOURCE_STATE_COPY_DEST);
+	D3D::current_command_list->CopyTextureRegion(&dst_location, 0, 0, 0, &src_location, &src_box);
+
+	// Convert YUYV texture to RGBA texture with pixel shader.
+	CD3DX12_RECT src_texture_rect(0, 0, src_width / 2, src_height);
+	dst_texture->TransitionToResourceState(D3D::current_command_list, D3D12_RESOURCE_STATE_RENDER_TARGET);
+	D3D::current_command_list->OMSetRenderTargets(1, &dst_texture->GetRTV12(), FALSE, nullptr);
+	D3D::SetViewportAndScissor(0, 0, src_width, src_height);
+	D3D::DrawShadedTexQuad(
+		m_yuyv_texture, &src_texture_rect, XFB_TEXTURE_WIDTH, XFB_TEXTURE_HEIGHT,
+		StaticShaderCache::GetXFBDecodePixelShader(), StaticShaderCache::GetSimpleVertexShader(), StaticShaderCache::GetSimpleVertexShaderInputLayout(),
+		{}, 1.0f, 0, DXGI_FORMAT_R8G8B8A8_UNORM, false, false);
+
+	// XFB source textures are expected to be in shader resource state.
+	dst_texture->TransitionToResourceState(D3D::current_command_list, D3D12_RESOURCE_STATE_PIXEL_SHADER_RESOURCE);
 }
 
 }