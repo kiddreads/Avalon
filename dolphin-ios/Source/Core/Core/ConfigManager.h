dolphin-emu/dolphin

Move DiscIO enums to a new file
At first there weren't many enums in Volume.h, but the number has been
growing, and I'm planning to add one more for regions. To not make
Volume.h too large, and to avoid needing to include Volume.h in code
that doesn't use volume objects, I'm moving the enums to a new file.
I'm also turning them into enum classes while I'm at it.

Source/Core/Core/ConfigManager.h
@@ -12,7 +12,11 @@
 #include "Common/SysConf.h"
 #include "Core/HW/EXI_Device.h"
 #include "Core/HW/SI_Device.h"
-#include "DiscIO/Volume.h"
+
+namespace DiscIO
+{
+enum class ELanguage;
+}
 
 // DSP Backend Types
 #define BACKEND_NULLSOUND _trans("No audio output")
@@ -177,7 +181,7 @@ struct SConfig : NonCopyable
   bool AutoSetup(EBootBS2 _BootBS2);
   const std::string& GetUniqueID() const { return m_strUniqueID; }
   void CheckMemcardPath(std::string& memcardPath, const std::string& gameRegion, bool isSlotA);
-  DiscIO::IVolume::ELanguage GetCurrentLanguage(bool wii) const;
+  DiscIO::ELanguage GetCurrentLanguage(bool wii) const;
 
   IniFile LoadDefaultGameIni() const;
   IniFile LoadLocalGameIni() const;

Source/Core/DiscIO/FileMonitor.cpp
@@ -18,6 +18,7 @@
 #include "Core/ConfigManager.h"
 #include "Core/Core.h"
 
+#include "DiscIO/Enums.h"
 #include "DiscIO/FileMonitor.h"
 #include "DiscIO/Filesystem.h"
 #include "DiscIO/Volume.h"
@@ -67,7 +68,7 @@ void ReadFileSystem(const std::string& filename)
   if (!s_open_iso)
     return;
 
-  if (s_open_iso->GetVolumeType() != DiscIO::IVolume::WII_WAD)
+  if (s_open_iso->GetVolumeType() != DiscIO::EPlatform::WII_WAD)
   {
     s_filesystem = DiscIO::CreateFileSystem(s_open_iso.get());
 

Source/Core/DiscIO/VolumeDirectory.cpp
@@ -17,6 +17,7 @@
 #include "Common/Logging/Log.h"
 #include "Common/MathUtil.h"
 #include "DiscIO/Blob.h"
+#include "DiscIO/Enums.h"
 #include "DiscIO/FileMonitor.h"
 #include "DiscIO/Volume.h"
 #include "DiscIO/VolumeDirectory.h"
@@ -163,7 +164,7 @@ void CVolumeDirectory::SetUniqueID(const std::string& id)
   memcpy(m_diskHeader.data(), id.c_str(), std::min(id.length(), MAX_ID_LENGTH));
 }
 
-IVolume::ECountry CVolumeDirectory::GetCountry() const
+ECountry CVolumeDirectory::GetCountry() const
 {
   return CountrySwitch(m_diskHeader[3]);
 }
@@ -183,12 +184,12 @@ std::string CVolumeDirectory::GetInternalName() const
     return "";
 }
 
-std::map<IVolume::ELanguage, std::string> CVolumeDirectory::GetLongNames() const
+std::map<ELanguage, std::string> CVolumeDirectory::GetLongNames() const
 {
   std::string name = GetInternalName();
   if (name.empty())
     return {{}};
-  return {{IVolume::LANGUAGE_UNKNOWN, name}};
+  return {{ELanguage::LANGUAGE_UNKNOWN, name}};
 }
 
 std::vector<u32> CVolumeDirectory::GetBanner(int* width, int* height) const
@@ -218,9 +219,9 @@ std::string CVolumeDirectory::GetApploaderDate() const
   return "VOID";
 }
 
-IVolume::EPlatform CVolumeDirectory::GetVolumeType() const
+EPlatform CVolumeDirectory::GetVolumeType() const
 {
-  return m_is_wii ? WII_DISC : GAMECUBE_DISC;
+  return m_is_wii ? EPlatform::WII_DISC : EPlatform::GAMECUBE_DISC;
 }
 
 BlobType CVolumeDirectory::GetBlobType() const

Source/Core/DolphinQt2/Settings.h
@@ -6,7 +6,10 @@
 
 #include <QSettings>
 
-#include "DiscIO/Volume.h"
+namespace DiscIO
+{
+enum class ELanguage;
+}
 
 // UI settings to be stored in the config directory.
 class Settings final : public QSettings
@@ -33,8 +36,8 @@ public:
   void SetApploader(const QString& path);
   QString GetWiiNAND() const;
   void SetWiiNAND(const QString& path);
-  DiscIO::IVolume::ELanguage GetWiiSystemLanguage() const;
-  DiscIO::IVolume::ELanguage GetGCSystemLanguage() const;
+  DiscIO::ELanguage GetWiiSystemLanguage() const;
+  DiscIO::ELanguage GetGCSystemLanguage() const;
   bool GetPreferredView() const;
   void SetPreferredView(bool table);
 

Source/Core/DolphinWX/ISOFile.h
@@ -10,15 +10,22 @@
 #include <vector>
 
 #include "Common/Common.h"
-#include "DiscIO/Blob.h"
-#include "DiscIO/Volume.h"
 
 #if defined(HAVE_WX) && HAVE_WX
 #include <wx/bitmap.h>
 #include <wx/image.h>
 #endif
 
+namespace DiscIO
+{
+enum class BlobType;
+enum class ECountry;
+enum class ELanguage;
+enum class EPlatform;
+}
+
 class PointerWrap;
+
 class GameListItem
 {
 public:
@@ -31,25 +38,21 @@ public:
 
   bool IsValid() const { return m_Valid; }
   const std::string& GetFileName() const { return m_FileName; }
-  std::string GetName(DiscIO::IVolume::ELanguage language) const;
+  std::string GetName(DiscIO::ELanguage language) const;
   std::string GetName() const;
-  std::string GetDescription(DiscIO::IVolume::ELanguage language) const;
+  std::string GetDescription(DiscIO::ELanguage language) const;
   std::string GetDescription() const;
-  std::vector<DiscIO::IVolume::ELanguage> GetLanguages() const;
+  std::vector<DiscIO::ELanguage> GetLanguages() const;
   std::string GetCompany() const { return m_company; }
   u16 GetRevision() const { return m_Revision; }
   const std::string& GetUniqueID() const { return m_UniqueID; }
   const std::string GetWiiFSPath() const;
-  DiscIO::IVolume::ECountry GetCountry() const { return m_Country; }
-  DiscIO::IVolume::EPlatform GetPlatform() const { return m_Platform; }
+  DiscIO::ECountry GetCountry() const { return m_Country; }
+  DiscIO::EPlatform GetPlatform() const { return m_Platform; }
   DiscIO::BlobType GetBlobType() const { return m_blob_type; }
   const std::string& GetIssues() const { return m_issues; }
   int GetEmuState() const { return m_emu_state; }
-  bool IsCompressed() const
-  {
-    return m_blob_type == DiscIO::BlobType::GCZ || m_blob_type == DiscIO::BlobType::CISO ||
-           m_blob_type == DiscIO::BlobType::WBFS;
-  }
+  bool IsCompressed() const;
   u64 GetFileSize() const { return m_FileSize; }
   u64 GetVolumeSize() const { return m_VolumeSize; }
   // 0 is the first disc, 1 is the second disc
@@ -63,8 +66,8 @@ public:
 private:
   std::string m_FileName;
 
-  std::map<DiscIO::IVolume::ELanguage, std::string> m_names;
-  std::map<DiscIO::IVolume::ELanguage, std::string> m_descriptions;
+  std::map<DiscIO::ELanguage, std::string> m_names;
+  std::map<DiscIO::ELanguage, std::string> m_descriptions;
   std::string m_company;
 
   std::string m_UniqueID;
@@ -76,8 +79,8 @@ private:
   u64 m_FileSize;
   u64 m_VolumeSize;
 
-  DiscIO::IVolume::ECountry m_Country;
-  DiscIO::IVolume::EPlatform m_Platform;
+  DiscIO::ECountry m_Country;
+  DiscIO::EPlatform m_Platform;
   DiscIO::BlobType m_blob_type;
   u16 m_Revision;
 

Source/Core/DolphinWX/NetPlay/NetWindow.cpp
@@ -34,6 +34,8 @@
 #include "Core/NetPlayProto.h"
 #include "Core/NetPlayServer.h"
 
+#include "DiscIO/Enums.h"
+
 #include "DolphinWX/Frame.h"
 #include "DolphinWX/GameListCtrl.h"
 #include "DolphinWX/ISOFile.h"
@@ -69,7 +71,7 @@ static wxString FailureReasonStringForHostLabel(int reason)
 static std::string BuildGameName(const GameListItem& game)
 {
   // Lang needs to be consistent
-  DiscIO::IVolume::ELanguage const lang = DiscIO::IVolume::LANGUAGE_ENGLISH;
+  const DiscIO::ELanguage lang = DiscIO::ELanguage::LANGUAGE_ENGLISH;
   std::vector<std::string> info;
   if (!game.GetUniqueID().empty())
     info.push_back(game.GetUniqueID());

kiddreads/Avalon

dolphin-ios/Source/Core/Core/ConfigManager.h
@@ -12,7 +12,11 @@
 #include "Common/SysConf.h"
 #include "Core/HW/EXI_Device.h"
 #include "Core/HW/SI_Device.h"
-#include "DiscIO/Volume.h"
+
+namespace DiscIO
+{
+enum class Language;
+}
 
 // DSP Backend Types
 #define BACKEND_NULLSOUND _trans("No audio output")
@@ -177,7 +181,7 @@ struct SConfig : NonCopyable
   bool AutoSetup(EBootBS2 _BootBS2);
   const std::string& GetUniqueID() const { return m_strUniqueID; }
   void CheckMemcardPath(std::string& memcardPath, const std::string& gameRegion, bool isSlotA);
-  DiscIO::IVolume::ELanguage GetCurrentLanguage(bool wii) const;
+  DiscIO::Language GetCurrentLanguage(bool wii) const;
 
   IniFile LoadDefaultGameIni() const;
   IniFile LoadLocalGameIni() const;

dolphin-ios/Source/Core/DiscIO/FileMonitor.cpp
@@ -18,6 +18,7 @@
 #include "Core/ConfigManager.h"
 #include "Core/Core.h"
 
+#include "DiscIO/Enums.h"
 #include "DiscIO/FileMonitor.h"
 #include "DiscIO/Filesystem.h"
 #include "DiscIO/Volume.h"
@@ -67,7 +68,7 @@ void ReadFileSystem(const std::string& filename)
   if (!s_open_iso)
     return;
 
-  if (s_open_iso->GetVolumeType() != DiscIO::IVolume::WII_WAD)
+  if (s_open_iso->GetVolumeType() != DiscIO::Platform::WII_WAD)
   {
     s_filesystem = DiscIO::CreateFileSystem(s_open_iso.get());
 

dolphin-ios/Source/Core/DiscIO/VolumeDirectory.cpp
@@ -17,6 +17,7 @@
 #include "Common/Logging/Log.h"
 #include "Common/MathUtil.h"
 #include "DiscIO/Blob.h"
+#include "DiscIO/Enums.h"
 #include "DiscIO/FileMonitor.h"
 #include "DiscIO/Volume.h"
 #include "DiscIO/VolumeDirectory.h"
@@ -163,7 +164,7 @@ void CVolumeDirectory::SetUniqueID(const std::string& id)
   memcpy(m_diskHeader.data(), id.c_str(), std::min(id.length(), MAX_ID_LENGTH));
 }
 
-IVolume::ECountry CVolumeDirectory::GetCountry() const
+Country CVolumeDirectory::GetCountry() const
 {
   return CountrySwitch(m_diskHeader[3]);
 }
@@ -183,12 +184,12 @@ std::string CVolumeDirectory::GetInternalName() const
     return "";
 }
 
-std::map<IVolume::ELanguage, std::string> CVolumeDirectory::GetLongNames() const
+std::map<Language, std::string> CVolumeDirectory::GetLongNames() const
 {
   std::string name = GetInternalName();
   if (name.empty())
     return {{}};
-  return {{IVolume::LANGUAGE_UNKNOWN, name}};
+  return {{Language::LANGUAGE_UNKNOWN, name}};
 }
 
 std::vector<u32> CVolumeDirectory::GetBanner(int* width, int* height) const
@@ -218,9 +219,9 @@ std::string CVolumeDirectory::GetApploaderDate() const
   return "VOID";
 }
 
-IVolume::EPlatform CVolumeDirectory::GetVolumeType() const
+Platform CVolumeDirectory::GetVolumeType() const
 {
-  return m_is_wii ? WII_DISC : GAMECUBE_DISC;
+  return m_is_wii ? Platform::WII_DISC : Platform::GAMECUBE_DISC;
 }
 
 BlobType CVolumeDirectory::GetBlobType() const

dolphin-ios/Source/Core/DolphinQt2/Settings.h
@@ -6,7 +6,10 @@
 
 #include <QSettings>
 
-#include "DiscIO/Volume.h"
+namespace DiscIO
+{
+enum class Language;
+}
 
 // UI settings to be stored in the config directory.
 class Settings final : public QSettings
@@ -34,8 +37,8 @@ public:
   void SetApploader(const QString& path);
   QString GetWiiNAND() const;
   void SetWiiNAND(const QString& path);
-  DiscIO::IVolume::ELanguage GetWiiSystemLanguage() const;
-  DiscIO::IVolume::ELanguage GetGCSystemLanguage() const;
+  DiscIO::Language GetWiiSystemLanguage() const;
+  DiscIO::Language GetGCSystemLanguage() const;
   bool GetPreferredView() const;
   void SetPreferredView(bool table);
 

dolphin-ios/Source/Core/DolphinWX/ISOFile.h
@@ -10,15 +10,22 @@
 #include <vector>
 
 #include "Common/Common.h"
-#include "DiscIO/Blob.h"
-#include "DiscIO/Volume.h"
 
 #if defined(HAVE_WX) && HAVE_WX
 #include <wx/bitmap.h>
 #include <wx/image.h>
 #endif
 
+namespace DiscIO
+{
+enum class BlobType;
+enum class Country;
+enum class Language;
+enum class Platform;
+}
+
 class PointerWrap;
+
 class GameListItem
 {
 public:
@@ -31,25 +38,21 @@ public:
 
   bool IsValid() const { return m_Valid; }
   const std::string& GetFileName() const { return m_FileName; }
-  std::string GetName(DiscIO::IVolume::ELanguage language) const;
+  std::string GetName(DiscIO::Language language) const;
   std::string GetName() const;
-  std::string GetDescription(DiscIO::IVolume::ELanguage language) const;
+  std::string GetDescription(DiscIO::Language language) const;
   std::string GetDescription() const;
-  std::vector<DiscIO::IVolume::ELanguage> GetLanguages() const;
+  std::vector<DiscIO::Language> GetLanguages() const;
   std::string GetCompany() const { return m_company; }
   u16 GetRevision() const { return m_Revision; }
   const std::string& GetUniqueID() const { return m_UniqueID; }
   const std::string GetWiiFSPath() const;
-  DiscIO::IVolume::ECountry GetCountry() const { return m_Country; }
-  DiscIO::IVolume::EPlatform GetPlatform() const { return m_Platform; }
+  DiscIO::Country GetCountry() const { return m_Country; }
+  DiscIO::Platform GetPlatform() const { return m_Platform; }
   DiscIO::BlobType GetBlobType() const { return m_blob_type; }
   const std::string& GetIssues() const { return m_issues; }
   int GetEmuState() const { return m_emu_state; }
-  bool IsCompressed() const
-  {
-    return m_blob_type == DiscIO::BlobType::GCZ || m_blob_type == DiscIO::BlobType::CISO ||
-           m_blob_type == DiscIO::BlobType::WBFS;
-  }
+  bool IsCompressed() const;
   u64 GetFileSize() const { return m_FileSize; }
   u64 GetVolumeSize() const { return m_VolumeSize; }
   // 0 is the first disc, 1 is the second disc
@@ -63,8 +66,8 @@ public:
 private:
   std::string m_FileName;
 
-  std::map<DiscIO::IVolume::ELanguage, std::string> m_names;
-  std::map<DiscIO::IVolume::ELanguage, std::string> m_descriptions;
+  std::map<DiscIO::Language, std::string> m_names;
+  std::map<DiscIO::Language, std::string> m_descriptions;
   std::string m_company;
 
   std::string m_UniqueID;
@@ -76,8 +79,8 @@ private:
   u64 m_FileSize;
   u64 m_VolumeSize;
 
-  DiscIO::IVolume::ECountry m_Country;
-  DiscIO::IVolume::EPlatform m_Platform;
+  DiscIO::Country m_Country;
+  DiscIO::Platform m_Platform;
   DiscIO::BlobType m_blob_type;
   u16 m_Revision;
 

dolphin-ios/Source/Core/DolphinWX/NetPlay/NetWindow.cpp
@@ -34,6 +34,8 @@
 #include "Core/NetPlayProto.h"
 #include "Core/NetPlayServer.h"
 
+#include "DiscIO/Enums.h"
+
 #include "DolphinWX/Frame.h"
 #include "DolphinWX/GameListCtrl.h"
 #include "DolphinWX/ISOFile.h"
@@ -69,7 +71,7 @@ static wxString FailureReasonStringForHostLabel(int reason)
 static std::string BuildGameName(const GameListItem& game)
 {
   // Lang needs to be consistent
-  DiscIO::IVolume::ELanguage const lang = DiscIO::IVolume::LANGUAGE_ENGLISH;
+  const DiscIO::Language lang = DiscIO::Language::LANGUAGE_ENGLISH;
   std::vector<std::string> info;
   if (!game.GetUniqueID().empty())
     info.push_back(game.GetUniqueID());