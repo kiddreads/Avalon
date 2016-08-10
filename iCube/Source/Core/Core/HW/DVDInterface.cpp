dolphin-emu/dolphin

Replace some unnamed tick constants with GetTicksPerSecond()
These operations should always take the same amount of time,
not the same amount of ticks. The number of ticks per second
is different for GameCube and Wii.

Source/Core/Core/HW/DVDInterface.cpp
@@ -489,7 +489,7 @@ void ChangeDisc(const std::string& newFileName)
 	bool was_unpaused = is_cpu ? false : Core::PauseAndLock(true);
 	std::string* _FileName = new std::string(newFileName);
 	CoreTiming::ScheduleEvent(0, ejectDisc);
-	CoreTiming::ScheduleEvent(500000000, insertDisc, (u64)_FileName);
+	CoreTiming::ScheduleEvent(SystemTimers::GetTicksPerSecond(), insertDisc, (u64)_FileName);
 	if (Movie::IsRecordingInput())
 	{
 		Movie::g_bDiscChange = true;

kiddreads/Avalon

iCube/Source/Core/Core/HW/DVDInterface.cpp
@@ -489,7 +489,7 @@ void ChangeDiscAsCPU(const std::string& newFileName)
 {
   std::string* _FileName = new std::string(newFileName);
   CoreTiming::ScheduleEvent(0, s_eject_disc);
-  CoreTiming::ScheduleEvent(500000000, s_insert_disc, (u64)_FileName);
+  CoreTiming::ScheduleEvent(SystemTimers::GetTicksPerSecond(), s_insert_disc, (u64)_FileName);
   if (Movie::IsRecordingInput())
   {
     std::string fileName = newFileName;