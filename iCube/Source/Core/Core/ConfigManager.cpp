kiddreads/Avalon

commit 64b597eac36cf035a92997873f32154cf761317c

SConfig: Replace bNTSC with m_region
This lets us get rid of VideoInterface::SetRegionReg,
a huge hack in CEXIMemoryCard, and some minor things.

iCube/Source/Core/Core/ConfigManager.cpp
@@ -794,7 +794,7 @@ void SConfig::LoadDefaults()
   m_revision = 0;
 }
 
-static const char* GetDirectoryForRegion(DiscIO::Region region)
+const char* SConfig::GetDirectoryForRegion(DiscIO::Region region)
 {
   switch (region)
   {
@@ -867,19 +867,18 @@ bool SConfig::AutoSetup(EBootBS2 _BootBS2)
       // Check if we have a Wii disc
       bWii = pVolume->GetVolumeType() == DiscIO::Platform::WII_DISC;
 
-      DiscIO::Region region = pVolume->GetRegion();
-      const char* retrieved_region_dir = GetDirectoryForRegion(region);
+      m_region = pVolume->GetRegion();
+      const char* retrieved_region_dir = GetDirectoryForRegion(m_region);
       if (!retrieved_region_dir)
       {
         if (!PanicYesNoT("Your GCM/ISO file seems to be invalid (invalid country)."
                          "\nContinue with PAL region?"))
           return false;
-        region = DiscIO::Region::PAL;
+        m_region = DiscIO::Region::PAL;
         retrieved_region_dir = EUR_DIR;
       }
 
       set_region_dir = retrieved_region_dir;
-      bNTSC = region != DiscIO::Region::PAL;
     }
     else if (!strcasecmp(Extension.c_str(), ".elf"))
     {
@@ -889,8 +888,8 @@ bool SConfig::AutoSetup(EBootBS2 _BootBS2)
       // all GC homebrew to 50Hz.
       // In the future, it probably makes sense to add a Region setting for homebrew somewhere in
       // the emulator config.
-      bNTSC = bWii ? false : true;
-      set_region_dir = bNTSC ? USA_DIR : EUR_DIR;
+      m_region = bWii ? DiscIO::Region::PAL : DiscIO::Region::NTSC_U;
+      set_region_dir = bWii ? EUR_DIR : USA_DIR;
       m_BootType = BOOT_ELF;
     }
     else if (!strcasecmp(Extension.c_str(), ".dol"))
@@ -898,15 +897,15 @@ bool SConfig::AutoSetup(EBootBS2 _BootBS2)
       CDolLoader dolfile(m_strFilename);
       bWii = dolfile.IsWii();
       // TODO: See the ELF code above.
-      bNTSC = bWii ? false : true;
-      set_region_dir = bNTSC ? USA_DIR : EUR_DIR;
+      m_region = bWii ? DiscIO::Region::PAL : DiscIO::Region::NTSC_U;
+      set_region_dir = bWii ? EUR_DIR : USA_DIR;
       m_BootType = BOOT_DOL;
     }
     else if (!strcasecmp(Extension.c_str(), ".dff"))
     {
       bWii = true;
+      m_region = DiscIO::Region::NTSC_U;
       set_region_dir = USA_DIR;
-      bNTSC = true;
       m_BootType = BOOT_DFF;
 
       std::unique_ptr<FifoDataFile> ddfFile(FifoDataFile::Load(m_strFilename, true));
@@ -931,9 +930,9 @@ bool SConfig::AutoSetup(EBootBS2 _BootBS2)
         return false;  // do not boot
       }
 
-      const char* retrieved_region_dir = GetDirectoryForRegion(ContentLoader.GetRegion());
+      m_region = ContentLoader.GetRegion();
+      const char* retrieved_region_dir = GetDirectoryForRegion(m_region);
       set_region_dir = retrieved_region_dir ? retrieved_region_dir : EUR_DIR;
-      bNTSC = set_region_dir == USA_DIR || set_region_dir == JAP_DIR;
 
       bWii = true;
       m_BootType = BOOT_WII_NAND;
@@ -974,21 +973,21 @@ bool SConfig::AutoSetup(EBootBS2 _BootBS2)
   break;
 
   case BOOT_BS2_USA:
+    m_region = DiscIO::Region::NTSC_U;
     set_region_dir = USA_DIR;
     m_strFilename.clear();
-    bNTSC = true;
     break;
 
   case BOOT_BS2_JAP:
+    m_region = DiscIO::Region::NTSC_J;
     set_region_dir = JAP_DIR;
     m_strFilename.clear();
-    bNTSC = true;
     break;
 
   case BOOT_BS2_EUR:
+    m_region = DiscIO::Region::PAL;
     set_region_dir = EUR_DIR;
     m_strFilename.clear();
-    bNTSC = false;
     break;
   }
 

iCube/Source/Core/Core/Boot/Boot.cpp
@@ -188,32 +188,34 @@ bool CBoot::Load_BS2(const std::string& _rBootROMFilename)
   // Use zlibs crc32 implementation to compute the hash
   u32 ipl_hash = crc32(0L, Z_NULL, 0);
   ipl_hash = crc32(ipl_hash, (const Bytef*)data.data(), (u32)data.size());
-  std::string ipl_region;
+  DiscIO::Region ipl_region;
   switch (ipl_hash)
   {
   case USA_v1_0:
   case USA_v1_1:
   case USA_v1_2:
   case BRA_v1_0:
-    ipl_region = USA_DIR;
+    ipl_region = DiscIO::Region::NTSC_U;
     break;
   case JAP_v1_0:
   case JAP_v1_1:
-    ipl_region = JAP_DIR;
+    ipl_region = DiscIO::Region::NTSC_J;
     break;
   case PAL_v1_0:
   case PAL_v1_2:
-    ipl_region = EUR_DIR;
+    ipl_region = DiscIO::Region::PAL;
     break;
   default:
     PanicAlertT("IPL with unknown hash %x", ipl_hash);
+    ipl_region = DiscIO::Region::UNKNOWN_REGION;
     break;
   }
 
-  std::string BootRegion = _rBootROMFilename.substr(_rBootROMFilename.find_last_of(DIR_SEP) - 3, 3);
-  if (BootRegion != ipl_region)
+  const DiscIO::Region boot_region = SConfig::GetInstance().m_region;
+  if (ipl_region != DiscIO::Region::UNKNOWN_REGION && boot_region != ipl_region)
     PanicAlertT("%s IPL found in %s directory. The disc might not be recognized",
-                ipl_region.c_str(), BootRegion.c_str());
+                SConfig::GetDirectoryForRegion(ipl_region),
+                SConfig::GetDirectoryForRegion(boot_region));
 
   // Run the descrambler over the encrypted section containing BS1/BS2
   CEXIIPL::Descrambler((u8*)data.data() + 0x100, 0x1AFE00);
@@ -255,7 +257,8 @@ bool CBoot::BootUp()
   g_symbolDB.Clear();
 
   // PAL Wii uses NTSC framerate and linecount in 60Hz modes
-  VideoInterface::Preset(_StartupPara.bNTSC || (_StartupPara.bWii && _StartupPara.bPAL60));
+  VideoInterface::Preset(DiscIO::IsNTSC(_StartupPara.m_region) ||
+                         (_StartupPara.bWii && _StartupPara.bPAL60));
 
   switch (_StartupPara.m_BootType)
   {
@@ -275,8 +278,6 @@ bool CBoot::BootUp()
     }
 
     std::string game_id = DVDInterface::GetVolume().GetGameID();
-    if (game_id.size() >= 4)
-      VideoInterface::SetRegionReg(game_id.at(3));
 
     std::vector<u8> tmd_buffer = pVolume.GetTMD();
     if (!tmd_buffer.empty())
@@ -419,7 +420,7 @@ bool CBoot::BootUp()
 
     // Poor man's bootup
     if (_StartupPara.bWii)
-      SetupWiiMemory(DiscIO::Region::UNKNOWN_REGION);
+      SetupWiiMemory();
     else
       EmulatedBS2_GC(true);
 

iCube/Source/Core/Core/Boot/Boot.h
@@ -7,11 +7,6 @@
 #include <cstdlib>
 #include <string>
 
-namespace DiscIO
-{
-enum class Region;
-}
-
 struct RegionSetting
 {
   const std::string area;
@@ -57,5 +52,5 @@ private:
   static bool Load_BS2(const std::string& _rBootROMFilename);
   static void Load_FST(bool _bIsWii);
 
-  static bool SetupWiiMemory(DiscIO::Region region);
+  static bool SetupWiiMemory();
 };

iCube/Source/Core/Core/Boot/Boot_BS2Emu.cpp
@@ -74,8 +74,8 @@ bool CBoot::EmulatedBS2_GC(bool skipAppLoader)
       0x10000006,
       0x8000002C);  // Console type - DevKit  (retail ID == 0x00000003) see YAGCD 4.2.1.1.2
 
-  PowerPC::HostWrite_U32(SConfig::GetInstance().bNTSC ? 0 : 1,
-                         0x800000CC);  // Fake the VI Init of the IPL (YAGCD 4.2.1.4)
+  const bool ntsc = DiscIO::IsNTSC(SConfig::GetInstance().m_region);
+  PowerPC::HostWrite_U32(ntsc ? 0 : 1, 0x800000CC);  // Fake the VI Init of the IPL (YAGCD 4.2.1.4)
 
   PowerPC::HostWrite_U32(0x01000000, 0x800000d0);  // ARAM Size. 16MB main + 4/16/32MB external
                                                    // (retail consoles have no external ARAM)
@@ -113,13 +113,14 @@ bool CBoot::EmulatedBS2_GC(bool skipAppLoader)
   DVDRead(apploader_offset + 0x20, 0x01200000, apploader_size + apploader_trailer, false);
 
   // Setup pointers like real BS2 does
-  if (SConfig::GetInstance().bNTSC)
+  if (ntsc)
   {
-    PowerPC::ppcState.gpr[1] = 0x81566550;  // StackPointer, used to be set to 0x816ffff0
-    PowerPC::ppcState.gpr[2] = 0x81465cc0;  // Global pointer to Small Data Area 2 Base (haven't
-                                            // seen anything use it...meh)
-    PowerPC::ppcState.gpr[13] =
-        0x81465320;  // Global pointer to Small Data Area Base (Luigi's Mansion's apploader uses it)
+    // StackPointer, used to be set to 0x816ffff0
+    PowerPC::ppcState.gpr[1] = 0x81566550;
+    // Global pointer to Small Data Area 2 Base (haven't seen anything use it...meh)
+    PowerPC::ppcState.gpr[2] = 0x81465cc0;
+    // Global pointer to Small Data Area Base (Luigi's Mansion's apploader uses it)
+    PowerPC::ppcState.gpr[13] = 0x81465320;
   }
   else
   {
@@ -181,22 +182,15 @@ bool CBoot::EmulatedBS2_GC(bool skipAppLoader)
   return true;
 }
 
-bool CBoot::SetupWiiMemory(DiscIO::Region region)
+bool CBoot::SetupWiiMemory()
 {
-  static const RegionSetting SETTING_NTSC_J = {"JPN", "NTSC", "JP", "LJ"};
-  static const RegionSetting SETTING_NTSC_U = {"USA", "NTSC", "US", "LU"};
-  static const RegionSetting SETTING_PAL = {"EUR", "PAL", "EU", "LE"};
-  static const RegionSetting SETTING_NTSC_K = {"KOR", "NTSC", "KR", "LKH"};
   static const std::map<DiscIO::Region, const RegionSetting> region_settings = {
-      {DiscIO::Region::NTSC_J, SETTING_NTSC_J},
-      {DiscIO::Region::NTSC_U, SETTING_NTSC_U},
-      {DiscIO::Region::PAL, SETTING_PAL},
-      {DiscIO::Region::NTSC_K, SETTING_NTSC_K}};
-  auto entryPos = region_settings.find(region);
-  const RegionSetting& region_setting =
-      (entryPos != region_settings.end()) ?
-          entryPos->second :
-          (SConfig::GetInstance().bNTSC ? SETTING_NTSC_U : SETTING_PAL);
+      {DiscIO::Region::NTSC_J, {"JPN", "NTSC", "JP", "LJ"}},
+      {DiscIO::Region::NTSC_U, {"USA", "NTSC", "US", "LU"}},
+      {DiscIO::Region::PAL, {"EUR", "PAL", "EU", "LE"}},
+      {DiscIO::Region::NTSC_K, {"KOR", "NTSC", "KR", "LKH"}}};
+  auto entryPos = region_settings.find(SConfig::GetInstance().m_region);
+  const RegionSetting& region_setting = entryPos->second;
 
   SettingsHandler gen;
   std::string serno;
@@ -311,7 +305,7 @@ bool CBoot::SetupWiiMemory(DiscIO::Region region)
   Memory::Write_U32(0x80000000, 0x00003184);  // GameID Address
 
   // Fake the VI Init of the IPL
-  Memory::Write_U32(SConfig::GetInstance().bNTSC ? 0 : 1, 0x000000CC);
+  Memory::Write_U32(DiscIO::IsNTSC(SConfig::GetInstance().m_region) ? 0 : 1, 0x000000CC);
 
   // Clear exception handler. Why? Don't we begin with only zeros?
   for (int i = 0x3000; i <= 0x3038; i += 4)
@@ -330,10 +324,7 @@ bool CBoot::EmulatedBS2_Wii()
   INFO_LOG(BOOT, "Faking Wii BS2...");
 
   // Setup Wii memory
-  DiscIO::Region region_code = DiscIO::Region::UNKNOWN_REGION;
-  if (DVDInterface::VolumeIsValid())
-    region_code = DVDInterface::GetVolume().GetRegion();
-  if (SetupWiiMemory(region_code) == false)
+  if (!SetupWiiMemory())
     return false;
 
   // Execute the apploader

iCube/Source/Core/Core/Boot/Boot_WiiWAD.cpp
@@ -89,13 +89,8 @@ bool CBoot::Boot_WiiWAD(const std::string& _pFilename)
   if (titleID == TITLEID_SYSMENU)
     HLE_IPC_CreateVirtualFATFilesystem();
   // setup Wii memory
-  if (!SetupWiiMemory(ContentLoader.GetRegion()))
+  if (!SetupWiiMemory())
     return false;
-  // this sets a bit that is used to detect NTSC-J
-  if (ContentLoader.GetRegion() == DiscIO::Region::NTSC_J)
-  {
-    VideoInterface::SetRegionReg('J');
-  }
   // DOL
   const DiscIO::SNANDContent* pContent =
       ContentLoader.GetContentByIndex(ContentLoader.GetBootIndex());

iCube/Source/Core/Core/BootManager.cpp
@@ -28,6 +28,7 @@
 #include "Common/IniFile.h"
 #include "Common/Logging/Log.h"
 #include "Common/MsgHandler.h"
+
 #include "Core/BootManager.h"
 #include "Core/ConfigManager.h"
 #include "Core/Core.h"
@@ -38,6 +39,9 @@
 #include "Core/Host.h"
 #include "Core/Movie.h"
 #include "Core/NetPlayProto.h"
+
+#include "DiscIO/Enums.h"
+
 #include "VideoCommon/VideoBackendBase.h"
 
 namespace BootManager
@@ -349,17 +353,19 @@ bool BootCore(const std::string& _rFilename)
     g_SRAM_netplay_initialized = false;
   }
 
+  const bool ntsc = DiscIO::IsNTSC(StartUp.m_region);
+
   // Apply overrides
-  // Some NTSC GameCube games such as Baten Kaitos react strangely to language settings that would
-  // be invalid on an NTSC system
-  if (!StartUp.bOverrideGCLanguage && StartUp.bNTSC)
+  // Some NTSC GameCube games such as Baten Kaitos react strangely to
+  // language settings that would be invalid on an NTSC system
+  if (!StartUp.bOverrideGCLanguage && ntsc)
   {
     StartUp.SelectedLanguage = 0;
   }
 
-  // Some NTSC Wii games such as Doc Louis's Punch-Out!! and 1942 (Virtual Console) crash if the
-  // PAL60 option is enabled
-  if (StartUp.bWii && StartUp.bNTSC)
+  // Some NTSC Wii games such as Doc Louis's Punch-Out!! and
+  // 1942 (Virtual Console) crash if the PAL60 option is enabled
+  if (StartUp.bWii && ntsc)
   {
     StartUp.bPAL60 = false;
   }

iCube/Source/Core/Core/ConfigManager.h
@@ -16,6 +16,7 @@
 namespace DiscIO
 {
 enum class Language;
+enum class Region;
 }
 
 // DSP Backend Types
@@ -92,7 +93,6 @@ struct SConfig : NonCopyable
   bool bDSPThread = false;
   bool bDSPHLE = true;
   bool bSyncGPUOnSkipIdleHack = true;
-  bool bNTSC = false;
   bool bForceNTSCJ = false;
   bool bHLE_BS2 = true;
   bool bEnableCheats = false;
@@ -183,7 +183,9 @@ struct SConfig : NonCopyable
     BOOT_BS2,
     BOOT_DFF
   };
+
   EBootType m_BootType;
+  DiscIO::Region m_region;
 
   std::string m_strVideoBackend;
   std::string m_strGPUDeterminismMode;
@@ -206,6 +208,7 @@ struct SConfig : NonCopyable
   std::string m_perfDir;
 
   void LoadDefaults();
+  static const char* GetDirectoryForRegion(DiscIO::Region region);
   bool AutoSetup(EBootBS2 _BootBS2);
   const std::string& GetGameID() const { return m_strGameID; }
   void CheckMemcardPath(std::string& memcardPath, const std::string& gameRegion, bool isSlotA);

iCube/Source/Core/Core/HW/EXI_DeviceIPL.cpp
@@ -13,6 +13,7 @@
 #include "Common/MemoryUtil.h"
 #include "Common/StringUtil.h"
 #include "Common/Timer.h"
+
 #include "Core/ConfigManager.h"
 #include "Core/Core.h"
 #include "Core/CoreTiming.h"
@@ -21,6 +22,8 @@
 #include "Core/HW/SystemTimers.h"
 #include "Core/Movie.h"
 #include "Core/NetPlayProto.h"
+
+#include "DiscIO/Enums.h"
 
 // We should provide an option to choose from the above, or figure out the checksum (the algo in
 // yagcd seems wrong)
@@ -89,7 +92,7 @@ void CEXIIPL::Descrambler(u8* data, u32 size)
 CEXIIPL::CEXIIPL() : m_uPosition(0), m_uAddress(0), m_uRWOffset(0), m_FontsLoaded(false)
 {
   // Determine region
-  m_bNTSC = SConfig::GetInstance().bNTSC;
+  m_bNTSC = DiscIO::IsNTSC(SConfig::GetInstance().m_region);
 
   // Create the IPL
   m_pIPL = static_cast<u8*>(Common::AllocateMemoryPages(ROM_SIZE));

commit 0e579aa99e68382fb12b207a2bea2d9bfe0d43e4

ConfigManager: Always set a valid m_region
Fixes a regression from 64b597e that made WADs
with no detectable region not boot correctly.
The non-WAD code was already handling this correctly.

iCube/Source/Core/Core/ConfigManager.cpp
@@ -900,7 +900,12 @@ bool SConfig::AutoSetup(EBootBS2 _BootBS2)
 
       m_region = ContentLoader.GetRegion();
       const char* retrieved_region_dir = GetDirectoryForRegion(m_region);
-      set_region_dir = retrieved_region_dir ? retrieved_region_dir : EUR_DIR;
+      if (!retrieved_region_dir)
+      {
+        m_region = DiscIO::Region::PAL;
+        retrieved_region_dir = EUR_DIR;
+      }
+      set_region_dir = retrieved_region_dir;
 
       bWii = true;
       m_BootType = BOOT_WII_NAND;