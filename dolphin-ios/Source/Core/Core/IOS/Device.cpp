dolphin-emu/dolphin

IOS: Implement OH0 (/dev/usb/oh0)

Source/Core/Core/IOS/Device.cpp
@@ -76,6 +76,15 @@ bool IOCtlVRequest::HasInputVectorWithAddress(const u32 vector_address) const
                      [&](const auto& in_vector) { return in_vector.address == vector_address; });
 }
 
+bool IOCtlVRequest::HasNumberOfVectors(const size_t in_count, const size_t io_count) const
+{
+  if (in_vectors.size() != in_count || io_vectors.size() != io_count)
+    return false;
+  auto IsValidVector = [](const auto& vector) { return vector.size == 0 || vector.address != 0; };
+  return std::all_of(in_vectors.begin(), in_vectors.end(), IsValidVector) &&
+         std::all_of(io_vectors.begin(), io_vectors.end(), IsValidVector);
+}
+
 void IOCtlRequest::Log(const std::string& device_name, LogTypes::LOG_TYPE type,
                        LogTypes::LOG_LEVELS verbosity) const
 {

Source/Core/Core/IOS/Device.h
@@ -42,6 +42,7 @@ enum ReturnCode : s32
   FS_EDIRDEPTH = -116,       // Max directory depth exceeded
   FS_EBUSY = -118,           // Resource busy
   IPC_EESEXHAUSTED = -1016,  // Max of 2 ES handles exceeded
+  USB_ECANCELED = -7022,     // USB OH0 insertion hook cancelled
 };
 
 struct Request
@@ -122,6 +123,7 @@ struct IOCtlVRequest final : Request
   std::vector<IOVector> io_vectors;
   explicit IOCtlVRequest(u32 address);
   bool HasInputVectorWithAddress(u32 vector_address) const;
+  bool HasNumberOfVectors(size_t in_count, size_t io_count) const;
   void Dump(const std::string& description, LogTypes::LOG_TYPE type = LogTypes::IOS,
             LogTypes::LOG_LEVELS level = LogTypes::LINFO) const;
   void DumpUnknown(const std::string& description, LogTypes::LOG_TYPE type = LogTypes::IOS,
@@ -137,6 +139,7 @@ public:
   {
     Static,  // Devices which appear in s_device_map.
     FileIO,  // FileIO devices which are created dynamically.
+    OH0,     // OH0 child devices which are created dynamically.
   };
 
   Device(u32 device_id, const std::string& device_name, DeviceType type = DeviceType::Static);

kiddreads/Avalon

dolphin-ios/Source/Core/Core/IOS/Device.cpp
@@ -76,6 +76,16 @@ bool IOCtlVRequest::HasInputVectorWithAddress(const u32 vector_address) const
                      [&](const auto& in_vector) { return in_vector.address == vector_address; });
 }
 
+bool IOCtlVRequest::HasNumberOfValidVectors(const size_t in_count, const size_t io_count) const
+{
+  if (in_vectors.size() != in_count || io_vectors.size() != io_count)
+    return false;
+
+  auto IsValidVector = [](const auto& vector) { return vector.size == 0 || vector.address != 0; };
+  return std::all_of(in_vectors.begin(), in_vectors.end(), IsValidVector) &&
+         std::all_of(io_vectors.begin(), io_vectors.end(), IsValidVector);
+}
+
 void IOCtlRequest::Log(const std::string& device_name, LogTypes::LOG_TYPE type,
                        LogTypes::LOG_LEVELS verbosity) const
 {

dolphin-ios/Source/Core/Core/IOS/Device.h
@@ -42,6 +42,7 @@ enum ReturnCode : s32
   FS_EDIRDEPTH = -116,       // Max directory depth exceeded
   FS_EBUSY = -118,           // Resource busy
   IPC_EESEXHAUSTED = -1016,  // Max of 2 ES handles exceeded
+  USB_ECANCELED = -7022,     // USB OH0 insertion hook cancelled
 };
 
 struct Request
@@ -122,6 +123,7 @@ struct IOCtlVRequest final : Request
   std::vector<IOVector> io_vectors;
   explicit IOCtlVRequest(u32 address);
   bool HasInputVectorWithAddress(u32 vector_address) const;
+  bool HasNumberOfValidVectors(size_t in_count, size_t io_count) const;
   void Dump(const std::string& description, LogTypes::LOG_TYPE type = LogTypes::IOS,
             LogTypes::LOG_LEVELS level = LogTypes::LINFO) const;
   void DumpUnknown(const std::string& description, LogTypes::LOG_TYPE type = LogTypes::IOS,
@@ -137,6 +139,7 @@ public:
   {
     Static,  // Devices which appear in s_device_map.
     FileIO,  // FileIO devices which are created dynamically.
+    OH0,     // OH0 child devices which are created dynamically.
   };
 
   Device(u32 device_id, const std::string& device_name, DeviceType type = DeviceType::Static);