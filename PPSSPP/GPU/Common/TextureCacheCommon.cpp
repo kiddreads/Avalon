kiddreads/Avalon

commit 5f2f40b4e442169d95fc7f601c95a2066d90c56c

Plug a memory leak in the GLES texture cache. Fixes #9089

PPSSPP/GPU/Common/TextureCacheCommon.cpp
@@ -601,11 +601,10 @@ void TextureCacheCommon::AttachFramebufferInvalid(TexCacheEntry *entry, VirtualF
 }
 
 void TextureCacheCommon::DetachFramebuffer(TexCacheEntry *entry, u32 address, VirtualFramebuffer *framebuffer) {
-	const u64 cachekey = entry->CacheKey();
-
 	if (entry->framebuffer == framebuffer) {
+		const u64 cachekey = entry->CacheKey();
 		cacheSizeEstimate_ += EstimateTexMemoryUsage(entry);
-		entry->framebuffer = 0;
+		entry->framebuffer = nullptr;
 		fbTexInfo_.erase(cachekey);
 		host->GPUNotifyTextureAttachment(entry->addr);
 	}

PPSSPP/GPU/GLES/TextureCacheGLES.cpp
@@ -71,8 +71,11 @@ void TextureCacheGLES::SetFramebufferManager(FramebufferManagerGLES *fbManager) 
 
 void TextureCacheGLES::ReleaseTexture(TexCacheEntry *entry, bool delete_them) {
 	DEBUG_LOG(G3D, "Deleting texture %i", entry->textureName);
-	if (delete_them)
-		glDeleteTextures(1, &entry->textureName);
+	if (delete_them) {
+		if (entry->textureName != 0) {
+			glDeleteTextures(1, &entry->textureName);
+		}
+	}
 	entry->textureName = 0;
 }
 
@@ -539,14 +542,16 @@ void TextureCacheGLES::BuildTexture(TexCacheEntry *const entry, bool replaceImag
 	// For the estimate, we assume cluts always point to 8888 for simplicity.
 	cacheSizeEstimate_ += EstimateTexMemoryUsage(entry);
 
-	// Always generate a texture name, we might need it if the texture is replaced later.
-	if (!replaceImages) {
-		entry->textureName = AllocTextureName();
-	}
-
 	if (entry->framebuffer) {
 		// Nothing else to do here.
 		return;
+	}
+
+	// Always generate a texture name unless it's a framebuffer, we might need it if the texture is replaced later.
+	if (!replaceImages) {
+		if (!entry->textureName) {
+			entry->textureName = AllocTextureName();
+		}
 	}
 
 	if ((entry->bufw == 0 || (gstate.texbufwidth[0] & 0xf800) != 0) && entry->addr >= PSP_GetKernelMemoryEnd()) {

commit 503cb09f1682dcc090adf7dc828fd70bbc52fcc7

Texcache: Fix "reuse changed textures".
Broken by 5f2f40b, which stopped generating new texture names/ptrs.

PPSSPP/GPU/Common/TextureCacheCommon.cpp
@@ -1486,6 +1486,9 @@ bool TextureCacheCommon::CheckFullHash(TexCacheEntry *entry, bool &doDelete) {
 				secondCacheSizeEstimate_ += EstimateTexMemoryUsage(entry);
 				// Is this wise? We simply copy the entry.
 				secondCache_[secondKey].reset(new TexCacheEntry(*entry));
+
+				// Make sure we don't delete the texture we just copied.
+				entry->texturePtr = nullptr;
 				doDelete = false;
 			}
 		}