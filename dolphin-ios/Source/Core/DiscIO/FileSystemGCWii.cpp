dolphin-emu/dolphin

Filesystem: Make FindFileInfo public

Source/Core/DiscIO/FileSystemGCWii.cpp
@@ -36,12 +36,34 @@ CFileSystemGCWii::~CFileSystemGCWii()
   m_FileInfoVector.clear();
 }
 
+const std::vector<CFileInfoGCWii>& CFileSystemGCWii::GetFileList()
+{
+	if (!m_Initialized)
+		InitFileSystem();
+
+	return m_FileInfoVector;
+}
+
+const IFileInfo* CFileSystemGCWii::FindFileInfo(const std::string& _rFullPath)
+{
+	if (!m_Initialized)
+		InitFileSystem();
+
+	for (size_t i = 0; i < m_FileInfoVector.size(); ++i)
+	{
+		if (!strcasecmp(GetPathFromFSTOffset(i).c_str(), _rFullPath.c_str()))
+			return &m_FileInfoVector[i];
+	}
+
+	return nullptr;
+}
+
 u64 CFileSystemGCWii::GetFileSize(const std::string& _rFullPath)
 {
   if (!m_Initialized)
     InitFileSystem();
 
-  const CFileInfoGCWii* pFileInfo = FindFileInfo(_rFullPath);
+  const IFileInfo* pFileInfo = FindFileInfo(_rFullPath);
 
   if (pFileInfo != nullptr && !pFileInfo->IsDirectory())
     return pFileInfo->GetSize();
@@ -113,7 +135,7 @@ u64 CFileSystemGCWii::ReadFile(const std::string& _rFullPath, u8* _pBuffer, u64 
   if (!m_Initialized)
     InitFileSystem();
 
-  const CFileInfoGCWii* pFileInfo = FindFileInfo(_rFullPath);
+  const IFileInfo* pFileInfo = FindFileInfo(_rFullPath);
   if (pFileInfo == nullptr)
     return 0;
 
@@ -137,7 +159,7 @@ bool CFileSystemGCWii::ExportFile(const std::string& _rFullPath,
   if (!m_Initialized)
     InitFileSystem();
 
-  const CFileInfoGCWii* pFileInfo = FindFileInfo(_rFullPath);
+  const IFileInfo* pFileInfo = FindFileInfo(_rFullPath);
 
   if (!pFileInfo)
     return false;
@@ -271,28 +293,6 @@ std::string CFileSystemGCWii::GetStringFromOffset(u64 _Offset) const
   // TODO: Should we really always use SHIFT-JIS?
   // It makes some filenames in Pikmin (NTSC-U) sane, but is it correct?
   return SHIFTJISToUTF8(data);
-}
-
-const std::vector<CFileInfoGCWii>& CFileSystemGCWii::GetFileList()
-{
-  if (!m_Initialized)
-    InitFileSystem();
-
-  return m_FileInfoVector;
-}
-
-const CFileInfoGCWii* CFileSystemGCWii::FindFileInfo(const std::string& _rFullPath)
-{
-  if (!m_Initialized)
-    InitFileSystem();
-
-  for (size_t i = 0; i < m_FileInfoVector.size(); ++i)
-  {
-    if (!strcasecmp(GetPathFromFSTOffset(i).c_str(), _rFullPath.c_str()))
-      return &m_FileInfoVector[i];
-  }
-
-  return nullptr;
 }
 
 bool CFileSystemGCWii::DetectFileSystem()

Source/Core/DiscIO/FileSystemGCWii.h
@@ -39,8 +39,9 @@ public:
   CFileSystemGCWii(const IVolume* _rVolume);
   ~CFileSystemGCWii() override;
   bool IsValid() const override { return m_Valid; }
-  u64 GetFileSize(const std::string& _rFullPath) override;
   const std::vector<CFileInfoGCWii>& GetFileList() override;
+  const IFileInfo* FindFileInfo(const std::string& path) override;
+  u64 GetFileSize(const std::string& _rFullPath) override;
   std::string GetPath(u64 _Address) override;
   std::string GetPathFromFSTOffset(size_t file_info_offset) override;
   u64 ReadFile(const std::string& _rFullPath, u8* _pBuffer, u64 _MaxBufferSize,
@@ -58,7 +59,6 @@ private:
   std::vector<CFileInfoGCWii> m_FileInfoVector;
 
   std::string GetStringFromOffset(u64 _Offset) const;
-  const CFileInfoGCWii* FindFileInfo(const std::string& _rFullPath);
   bool DetectFileSystem();
   void InitFileSystem();
   u32 GetOffsetShift() const;

Source/Core/DiscIO/Filesystem.h
@@ -39,6 +39,7 @@ public:
   // TODO: Should only return IFileInfo, not CFileInfoGCWii
   virtual const std::vector<CFileInfoGCWii>& GetFileList() = 0;
   virtual u64 GetFileSize(const std::string& _rFullPath) = 0;
+  virtual const IFileInfo* FindFileInfo(const std::string& path) = 0;
   virtual u64 ReadFile(const std::string& _rFullPath, u8* _pBuffer, u64 _MaxBufferSize,
                        u64 _OffsetInFile = 0) = 0;
   virtual bool ExportFile(const std::string& _rFullPath, const std::string& _rExportFilename) = 0;

kiddreads/Avalon

dolphin-ios/Source/Core/DiscIO/FileSystemGCWii.cpp
@@ -42,12 +42,34 @@ FileSystemGCWii::~FileSystemGCWii()
   m_FileInfoVector.clear();
 }
 
+const std::vector<FileInfoGCWii>& FileSystemGCWii::GetFileList()
+{
+  if (!m_Initialized)
+    InitFileSystem();
+
+  return m_FileInfoVector;
+}
+
+const FileInfo* FileSystemGCWii::FindFileInfo(const std::string& _rFullPath)
+{
+  if (!m_Initialized)
+    InitFileSystem();
+
+  for (size_t i = 0; i < m_FileInfoVector.size(); ++i)
+  {
+    if (!strcasecmp(GetPathFromFSTOffset(i).c_str(), _rFullPath.c_str()))
+      return &m_FileInfoVector[i];
+  }
+
+  return nullptr;
+}
+
 u64 FileSystemGCWii::GetFileSize(const std::string& _rFullPath)
 {
   if (!m_Initialized)
     InitFileSystem();
 
-  const FileInfoGCWii* pFileInfo = FindFileInfo(_rFullPath);
+  const FileInfo* pFileInfo = FindFileInfo(_rFullPath);
 
   if (pFileInfo != nullptr && !pFileInfo->IsDirectory())
     return pFileInfo->GetSize();
@@ -119,7 +141,7 @@ u64 FileSystemGCWii::ReadFile(const std::string& _rFullPath, u8* _pBuffer, u64 _
   if (!m_Initialized)
     InitFileSystem();
 
-  const FileInfoGCWii* pFileInfo = FindFileInfo(_rFullPath);
+  const FileInfo* pFileInfo = FindFileInfo(_rFullPath);
   if (pFileInfo == nullptr)
     return 0;
 
@@ -142,7 +164,7 @@ bool FileSystemGCWii::ExportFile(const std::string& _rFullPath, const std::strin
   if (!m_Initialized)
     InitFileSystem();
 
-  const FileInfoGCWii* pFileInfo = FindFileInfo(_rFullPath);
+  const FileInfo* pFileInfo = FindFileInfo(_rFullPath);
 
   if (!pFileInfo)
     return false;
@@ -274,28 +296,6 @@ std::string FileSystemGCWii::GetStringFromOffset(u64 _Offset) const
   // TODO: Should we really always use SHIFT-JIS?
   // It makes some filenames in Pikmin (NTSC-U) sane, but is it correct?
   return SHIFTJISToUTF8(data);
-}
-
-const std::vector<FileInfoGCWii>& FileSystemGCWii::GetFileList()
-{
-  if (!m_Initialized)
-    InitFileSystem();
-
-  return m_FileInfoVector;
-}
-
-const FileInfoGCWii* FileSystemGCWii::FindFileInfo(const std::string& _rFullPath)
-{
-  if (!m_Initialized)
-    InitFileSystem();
-
-  for (size_t i = 0; i < m_FileInfoVector.size(); ++i)
-  {
-    if (!strcasecmp(GetPathFromFSTOffset(i).c_str(), _rFullPath.c_str()))
-      return &m_FileInfoVector[i];
-  }
-
-  return nullptr;
 }
 
 bool FileSystemGCWii::DetectFileSystem()

dolphin-ios/Source/Core/DiscIO/FileSystemGCWii.h
@@ -42,8 +42,9 @@ public:
   FileSystemGCWii(const Volume* _rVolume, const Partition& partition);
   ~FileSystemGCWii() override;
   bool IsValid() const override { return m_Valid; }
-  u64 GetFileSize(const std::string& _rFullPath) override;
   const std::vector<FileInfoGCWii>& GetFileList() override;
+  const FileInfo* FindFileInfo(const std::string& path) override;
+  u64 GetFileSize(const std::string& _rFullPath) override;
   std::string GetPath(u64 _Address) override;
   std::string GetPathFromFSTOffset(size_t file_info_offset) override;
   u64 ReadFile(const std::string& _rFullPath, u8* _pBuffer, u64 _MaxBufferSize,
@@ -61,7 +62,6 @@ private:
   std::vector<FileInfoGCWii> m_FileInfoVector;
 
   std::string GetStringFromOffset(u64 _Offset) const;
-  const FileInfoGCWii* FindFileInfo(const std::string& _rFullPath);
   bool DetectFileSystem();
   void InitFileSystem();
 };

dolphin-ios/Source/Core/DiscIO/Filesystem.h
@@ -41,6 +41,7 @@ public:
   // TODO: Should only return FileInfo, not FileInfoGCWii
   virtual const std::vector<FileInfoGCWii>& GetFileList() = 0;
   virtual u64 GetFileSize(const std::string& _rFullPath) = 0;
+  virtual const FileInfo* FindFileInfo(const std::string& path) = 0;
   virtual u64 ReadFile(const std::string& _rFullPath, u8* _pBuffer, u64 _MaxBufferSize,
                        u64 _OffsetInFile = 0) = 0;
   virtual bool ExportFile(const std::string& _rFullPath, const std::string& _rExportFilename) = 0;