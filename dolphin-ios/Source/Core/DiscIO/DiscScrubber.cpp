dolphin-emu/dolphin

Filesystem: Replace GetFileList()
Instead of expecting callers to know how the size of directory file infos
relates to which files are in which directories, filesystems now offer a
GetRoot() method, and file infos offer a way to get their children. As
a bonus, m_FileInfoVector no longer has to be created and kept around
in RAM. Only the file info objects that actually are used are created.

Source/Core/DiscIO/DiscScrubber.cpp
@@ -15,8 +15,6 @@
 #include "Common/Logging/Log.h"
 #include "DiscIO/DiscScrubber.h"
 #include "DiscIO/Filesystem.h"
-// TODO: eww
-#include "DiscIO/FileSystemGCWii.h"
 #include "DiscIO/Volume.h"
 #include "DiscIO/VolumeCreator.h"
 
@@ -77,6 +75,7 @@ bool ReadFromVolume(u64 _Offset, u32& _Buffer, bool _Decrypt);
 bool ReadFromVolume(u64 _Offset, u64& _Buffer, bool _Decrypt);
 bool ParseDisc();
 bool ParsePartitionData(SPartition& _rPartition);
+void ParseFileSystemData(u64 offset, const IFileInfo& directory);
 
 bool SetupScrub(const std::string& filename, int block_size)
 {
@@ -313,22 +312,25 @@ bool ParsePartitionData(SPartition& partition)
                 partition.Header.FSTSize);
 
     // Go through the filesystem and mark entries as used
-    auto& file_list = filesystem->GetFileList();
-    for (size_t i = 0; i < file_list.size(); ++i)
-    {
-      const std::string path = filesystem->GetPathFromFSTOffset(i);
-      DEBUG_LOG(DISCIO, "%s", path.empty() ? "/" : path.c_str());
-      auto& file = file_list[i];
-      if (!file.IsDirectory())
-        MarkAsUsedE(partition.Offset + partition.Header.DataOffset, file.GetOffset(),
-                    file.GetSize());
-    }
+    ParseFileSystemData(partition.Offset + partition.Header.DataOffset, filesystem->GetRoot());
   }
 
   // Swap back
   s_disc.swap(old_volume);
 
   return parsed_ok;
+}
+
+void ParseFileSystemData(u64 offset, const IFileInfo& directory)
+{
+  for (const DiscIO::IFileInfo& file_info : directory)
+  {
+    DEBUG_LOG(DISCIO, "Scrubbing %s", file_info.GetPath().c_str());
+    if (file_info.IsDirectory())
+      ParseFileSystemData(offset, file_info);
+    else
+      MarkAsUsedE(offset, file_info.GetOffset(), file_info.GetSize());
+  }
 }
 
 }  // namespace DiscScrubber

Source/Core/DiscIO/FileSystemGCWii.h
@@ -18,23 +18,49 @@ class IVolume;
 class CFileInfoGCWii : public IFileInfo
 {
 public:
-  // Does not take ownership of pointers
-  CFileInfoGCWii(const u8* fst, bool wii, u32 index, u32 total_file_infos);
+  // None of the constructors take ownership of FST pointers
 
+  // Set everything manually
+  CFileInfoGCWii(const u8* fst, bool wii, u32 index, u32 total_file_infos);
+  // For the root object only
+  CFileInfoGCWii(const u8* fst, bool wii);
+  // Copy another object
+  CFileInfoGCWii(const CFileInfoGCWii& file_info) = default;
+  // Copy data that is common to the whole file system
+  CFileInfoGCWii(const CFileInfoGCWii& file_info, u32 index);
+
+  ~CFileInfoGCWii() override;
+
+  IFileInfo& operator++() override;
+  std::unique_ptr<IFileInfo> clone() const override;
+  const_iterator begin() const override;
+  const_iterator end() const override;
+
+  // For directories, GetRawOffset should be used instead. This one does a special correction for
+  // Wii files
   u64 GetOffset() const override;
+  // For directories, returns the index of the next entry that is not in this directory
   u32 GetSize() const override;
   bool IsDirectory() const override;
+  u32 GetTotalChildren() const override;
   std::string GetName() const override;
+  std::string GetPath() const override;
+
+protected:
+  const void* GetAddress() const override;
 
 private:
+  u32 GetNextIndex() const;
+
   u32 Get(int n) const;
   u32 GetRawNameOffset() const;
+  // For directories, returns the index of the parent directory (or 0 if root)
   u32 GetRawOffset() const;
 
-  const u8* const m_fst;
-  const bool m_wii;
-  const u32 m_index;
-  const u32 m_total_file_infos;
+  const u8* m_fst;
+  bool m_wii;
+  u32 m_index;
+  u32 m_total_file_infos;
 };
 
 class CFileSystemGCWii : public IFileSystem
@@ -42,12 +68,12 @@ class CFileSystemGCWii : public IFileSystem
 public:
   CFileSystemGCWii(const IVolume* _rVolume);
   ~CFileSystemGCWii() override;
+
   bool IsValid() const override { return m_Valid; }
-  const std::vector<CFileInfoGCWii>& GetFileList() const override;
-  const IFileInfo* FindFileInfo(const std::string& path) const override;
-  const IFileInfo* FindFileInfo(u64 disc_offset) const override;
-  std::string GetPath(u64 _Address) const override;
-  std::string GetPathFromFSTOffset(size_t file_info_offset) const override;
+  const IFileInfo& GetRoot() const override;
+  std::unique_ptr<IFileInfo> FindFileInfo(const std::string& path) const override;
+  std::unique_ptr<IFileInfo> FindFileInfo(u64 disc_offset) const override;
+
   u64 ReadFile(const IFileInfo* file_info, u8* _pBuffer, u64 _MaxBufferSize,
                u64 _OffsetInFile) const override;
   bool ExportFile(const IFileInfo* file_info, const std::string& _rExportFilename) const override;
@@ -59,10 +85,12 @@ public:
 private:
   bool m_Valid;
   bool m_Wii;
-  std::vector<CFileInfoGCWii> m_FileInfoVector;
   std::vector<u8> m_file_system_table;
+  CFileInfoGCWii m_root;
 
-  const IFileInfo* FindFileInfo(const std::string& path, size_t search_start_offset) const;
+  std::unique_ptr<IFileInfo> FindFileInfo(const std::string& path,
+                                          const IFileInfo& file_info) const;
+  std::unique_ptr<IFileInfo> FindFileInfo(u64 disc_offset, const IFileInfo& file_info) const;
   u32 GetOffsetShift() const;
 };
 

Source/Core/DiscIO/Filesystem.h
@@ -4,6 +4,7 @@
 
 #pragma once
 
+#include <iterator>
 #include <memory>
 #include <string>
 #include <vector>
@@ -14,40 +15,96 @@ namespace DiscIO
 {
 class IVolume;
 
-// TODO: eww
-class CFileInfoGCWii;
-
 // file info of an FST entry
 class IFileInfo
 {
+friend class const_iterator;
+
 public:
-  // Not guaranteed to return a meaningful value for directories
+  class const_iterator final : public std::iterator<std::input_iterator_tag, IFileInfo>
+  {
+  public:
+    const_iterator(std::unique_ptr<IFileInfo> file_info) : m_file_info(std::move(file_info)) {}
+    const_iterator(const const_iterator& it) : m_file_info(it.m_file_info->clone()) {}
+    const_iterator(const_iterator&& it) : m_file_info(std::move(it.m_file_info)) {}
+    ~const_iterator() {}
+    const_iterator& operator=(const const_iterator& it)
+    {
+      m_file_info = it.m_file_info->clone();
+      return *this;
+    }
+    const_iterator& operator=(const_iterator&& it)
+    {
+      m_file_info = std::move(it.m_file_info);
+      return *this;
+    }
+    const_iterator& operator++()
+    {
+      ++*m_file_info;
+      return *this;
+    }
+    bool operator==(const const_iterator& it) const { return *m_file_info == *it.m_file_info; }
+    bool operator!=(const const_iterator& it) const { return !operator==(it); }
+    // Incrementing or destroying an iterator will invalidate its returned references and
+    // pointers, but will not invalidate copies of the iterator or file info object.
+    const IFileInfo& operator*() const { return *m_file_info.get(); }
+    const IFileInfo* operator->() const { return m_file_info.get(); }
+  private:
+    std::unique_ptr<IFileInfo> m_file_info;
+  };
+
+  virtual ~IFileInfo() {}
+  bool operator==(const IFileInfo& other) const { return GetAddress() == other.GetAddress(); }
+  bool operator!=(const IFileInfo& other) const { return !operator==(other); }
+  virtual IFileInfo& operator++() = 0;
+  virtual std::unique_ptr<IFileInfo> clone() const = 0;
+  virtual const_iterator begin() const = 0;
+  virtual const_iterator end() const = 0;
+
+  // The offset of a file on the disc (inside the partition, if there is one).
+  // Not guaranteed to return a meaningful value for directories.
   virtual u64 GetOffset() const = 0;
-  // Not guaranteed to return a meaningful value for directories
+  // The size of a file.
+  // Not guaranteed to return a meaningful value for directories.
   virtual u32 GetSize() const = 0;
   virtual bool IsDirectory() const = 0;
+  // The number of files and directories in a directory, including those in subdirectories.
+  // Not guaranteed to return a meaningful value for files.
+  virtual u32 GetTotalChildren() const = 0;
   virtual std::string GetName() const = 0;
+  // GetPath will find the parents of the current object and call GetName on them,
+  // so it's slower than other functions. If you're traversing through folders
+  // to get a file and its path, building the path while traversing is faster.
+  virtual std::string GetPath() const = 0;
+
+protected:
+  // Used for comparisons with other file info objects
+  virtual const void* GetAddress() const = 0;
 };
 
 class IFileSystem
 {
 public:
   IFileSystem(const IVolume* _rVolume);
-
   virtual ~IFileSystem();
+
+  // If IsValid is false, GetRoot must not be called. CreateFileSystem
+  // takes care of this automatically, so other code is recommended to use it.
   virtual bool IsValid() const = 0;
-  // TODO: Should only return IFileInfo, not CFileInfoGCWii
-  virtual const std::vector<CFileInfoGCWii>& GetFileList() const = 0;
-  virtual const IFileInfo* FindFileInfo(const std::string& path) const = 0;
-  virtual const IFileInfo* FindFileInfo(u64 disc_offset) const = 0;
+  // The object returned by GetRoot and all objects created from it
+  // are only valid for as long as the file system object is valid.
+  virtual const IFileInfo& GetRoot() const = 0;
+  // Returns nullptr if not found
+  virtual std::unique_ptr<IFileInfo> FindFileInfo(const std::string& path) const = 0;
+  // Returns nullptr if not found
+  virtual std::unique_ptr<IFileInfo> FindFileInfo(u64 disc_offset) const = 0;
+
   virtual u64 ReadFile(const IFileInfo* file_info, u8* _pBuffer, u64 _MaxBufferSize,
                        u64 _OffsetInFile = 0) const = 0;
   virtual bool ExportFile(const IFileInfo* file_info,
                           const std::string& _rExportFilename) const = 0;
   virtual bool ExportApploader(const std::string& _rExportFolder) const = 0;
   virtual bool ExportDOL(const std::string& _rExportFolder) const = 0;
-  virtual std::string GetPath(u64 _Address) const = 0;
-  virtual std::string GetPathFromFSTOffset(size_t file_info_offset) const = 0;
   virtual u64 GetBootDOLOffset() const = 0;
   virtual u32 GetBootDOLSize(u64 dol_offset) const = 0;
 
@@ -55,6 +112,7 @@ protected:
   const IVolume* m_rVolume;
 };
 
+// Returns nullptr if a valid file system could not be created
 std::unique_ptr<IFileSystem> CreateFileSystem(const IVolume* volume);
 
 }  // namespace

Source/Core/DiscIO/VolumeGC.cpp
@@ -210,7 +210,7 @@ void CVolumeGC::LoadBannerFile() const
   if (!file_system)
     return;
 
-  const IFileInfo* file_info = file_system->FindFileInfo("opening.bnr");
+  std::unique_ptr<IFileInfo> file_info = file_system->FindFileInfo("opening.bnr");
   if (!file_info)
     return;
 
@@ -223,7 +223,8 @@ void CVolumeGC::LoadBannerFile() const
     return;
   }
 
-  if (file_size != file_system->ReadFile(file_info, reinterpret_cast<u8*>(&banner_file), file_size))
+  if (file_size !=
+      file_system->ReadFile(file_info.get(), reinterpret_cast<u8*>(&banner_file), file_size))
   {
     WARN_LOG(DISCIO, "Could not read opening.bnr.");
     return;

kiddreads/Avalon

dolphin-ios/Source/Core/DiscIO/DiscScrubber.cpp
@@ -16,8 +16,6 @@
 #include "Common/Logging/Log.h"
 #include "DiscIO/DiscScrubber.h"
 #include "DiscIO/Filesystem.h"
-// TODO: eww
-#include "DiscIO/FileSystemGCWii.h"
 #include "DiscIO/Volume.h"
 
 namespace DiscIO
@@ -221,17 +219,21 @@ bool DiscScrubber::ParsePartitionData(const Partition& partition, PartitionHeade
   MarkAsUsedE(partition_data_offset, header->fst_offset, header->fst_size);
 
   // Go through the filesystem and mark entries as used
-  auto& file_list = filesystem->GetFileList();
-  for (size_t i = 0; i < file_list.size(); ++i)
-  {
-    const std::string path = filesystem->GetPathFromFSTOffset(i);
-    DEBUG_LOG(DISCIO, "%s", path.empty() ? "/" : path.c_str());
-    auto& file = file_list[i];
-    if (!file.IsDirectory())
-      MarkAsUsedE(partition_data_offset, file.GetOffset(), file.GetSize());
-  }
+  ParseFileSystemData(partition_data_offset, filesystem->GetRoot());
 
   return true;
+}
+
+void DiscScrubber::ParseFileSystemData(u64 partition_data_offset, const FileInfo& directory)
+{
+  for (const DiscIO::FileInfo& file_info : directory)
+  {
+    DEBUG_LOG(DISCIO, "Scrubbing %s", file_info.GetPath().c_str());
+    if (file_info.IsDirectory())
+      ParseFileSystemData(partition_data_offset, file_info);
+    else
+      MarkAsUsedE(partition_data_offset, file_info.GetOffset(), file_info.GetSize());
+  }
 }
 
 }  // namespace DiscIO

dolphin-ios/Source/Core/DiscIO/FileSystemGCWii.h
@@ -5,6 +5,7 @@
 #pragma once
 
 #include <cstddef>
+#include <memory>
 #include <optional>
 #include <string>
 #include <vector>
@@ -20,30 +21,60 @@ struct Partition;
 class FileInfoGCWii : public FileInfo
 {
 public:
-  // Does not take ownership of pointers
+  // None of the constructors take ownership of FST pointers
+
+  // Set everything manually
   FileInfoGCWii(const u8* fst, u8 offset_shift, u32 index, u32 total_file_infos);
+  // For the root object only
+  FileInfoGCWii(const u8* fst, u8 offset_shift);
+  // Copy another object
+  FileInfoGCWii(const FileInfoGCWii& file_info) = default;
+  // Copy data that is common to the whole file system
+  FileInfoGCWii(const FileInfoGCWii& file_info, u32 index);
 
   ~FileInfoGCWii() override;
+
+  std::unique_ptr<FileInfo> clone() const override;
+  const_iterator begin() const override;
+  const_iterator end() const override;
 
   u64 GetOffset() const override;
   u32 GetSize() const override;
   bool IsDirectory() const override;
+  u32 GetTotalChildren() const override;
   std::string GetName() const override;
+  std::string GetPath() const override;
+
+protected:
+  uintptr_t GetAddress() const override;
+  FileInfo& operator++() override;
 
 private:
   enum class EntryProperty
   {
+    // NAME_OFFSET's lower 3 bytes are the name's offset within the name table.
+    // NAME_OFFSET's upper 1 byte is 1 for directories and 0 for files.
     NAME_OFFSET = 0,
+    // For files, FILE_OFFSET is the file offset in the partition,
+    // and for directories, it's the FST index of the parent directory.
+    // The root directory has its parent directory index set to 0.
     FILE_OFFSET = 1,
+    // For files, FILE_SIZE is the file size, and for directories,
+    // it's the FST index of the next entry that isn't in the directory.
     FILE_SIZE = 2
   };
 
+  // For files, returns the index of the next item. For directories,
+  // returns the index of the next item that isn't inside it.
+  u32 GetNextIndex() const;
+  // Returns one of the three properties of this FST entry.
+  // Read the comments in EntryProperty for details.
   u32 Get(EntryProperty entry_property) const;
 
-  const u8* const m_fst;
-  const u8 m_offset_shift;
-  const u32 m_index;
-  const u32 m_total_file_infos;
+  const u8* m_fst;
+  u8 m_offset_shift;
+  u32 m_index;
+  u32 m_total_file_infos;
 };
 
 class FileSystemGCWii : public FileSystem
@@ -51,12 +82,12 @@ class FileSystemGCWii : public FileSystem
 public:
   FileSystemGCWii(const Volume* _rVolume, const Partition& partition);
   ~FileSystemGCWii() override;
+
   bool IsValid() const override { return m_Valid; }
-  const std::vector<FileInfoGCWii>& GetFileList() const override;
-  const FileInfo* FindFileInfo(const std::string& path) const override;
-  const FileInfo* FindFileInfo(u64 disc_offset) const override;
-  std::string GetPath(u64 _Address) const override;
-  std::string GetPathFromFSTOffset(size_t file_info_offset) const override;
+  const FileInfo& GetRoot() const override;
+  std::unique_ptr<FileInfo> FindFileInfo(const std::string& path) const override;
+  std::unique_ptr<FileInfo> FindFileInfo(u64 disc_offset) const override;
+
   u64 ReadFile(const FileInfo* file_info, u8* _pBuffer, u64 _MaxBufferSize,
                u64 _OffsetInFile) const override;
   bool ExportFile(const FileInfo* file_info, const std::string& _rExportFilename) const override;
@@ -68,10 +99,11 @@ public:
 private:
   bool m_Valid;
   u32 m_offset_shift;
-  std::vector<FileInfoGCWii> m_FileInfoVector;
   std::vector<u8> m_file_system_table;
+  FileInfoGCWii m_root;
 
-  const FileInfo* FindFileInfo(const std::string& path, size_t search_start_offset) const;
+  std::unique_ptr<FileInfo> FindFileInfo(const std::string& path, const FileInfo& file_info) const;
+  std::unique_ptr<FileInfo> FindFileInfo(u64 disc_offset, const FileInfo& file_info) const;
 };
 
 }  // namespace

dolphin-ios/Source/Core/DiscIO/Filesystem.h
@@ -4,6 +4,7 @@
 
 #pragma once
 
+#include <iterator>
 #include <memory>
 #include <optional>
 #include <string>
@@ -14,42 +15,116 @@
 
 namespace DiscIO
 {
-// TODO: eww
-class FileInfoGCWii;
-
 // file info of an FST entry
 class FileInfo
 {
+  friend class const_iterator;
+
 public:
+  class const_iterator final
+  {
+  public:
+    using iterator_category = std::forward_iterator_tag;
+    using value_type = const FileInfo;
+    using difference_type = std::ptrdiff_t;
+    using pointer = value_type*;
+    using reference = value_type&;
+
+    const_iterator() : m_file_info(nullptr) {}
+    const_iterator(std::unique_ptr<FileInfo> file_info) : m_file_info(std::move(file_info)) {}
+    const_iterator(const const_iterator& it) : m_file_info(it.m_file_info->clone()) {}
+    const_iterator(const_iterator&& it) : m_file_info(std::move(it.m_file_info)) {}
+    ~const_iterator() {}
+    const_iterator& operator=(const const_iterator& it)
+    {
+      m_file_info = it.m_file_info ? it.m_file_info->clone() : nullptr;
+      return *this;
+    }
+    const_iterator& operator=(const_iterator&& it)
+    {
+      m_file_info = std::move(it.m_file_info);
+      return *this;
+    }
+    const_iterator& operator++()
+    {
+      ++*m_file_info;
+      return *this;
+    }
+    const_iterator operator++(int)
+    {
+      const_iterator old = *this;
+      ++*m_file_info;
+      return old;
+    }
+    bool operator==(const const_iterator& it) const
+    {
+      return m_file_info ? (it.m_file_info && *m_file_info == *it.m_file_info) : (!it.m_file_info);
+    }
+    bool operator!=(const const_iterator& it) const { return !operator==(it); }
+    // Incrementing or destroying an iterator will invalidate its returned references and
+    // pointers, but will not invalidate copies of the iterator or file info object.
+    const FileInfo& operator*() const { return *m_file_info.get(); }
+    const FileInfo* operator->() const { return m_file_info.get(); }
+  private:
+    std::unique_ptr<FileInfo> m_file_info;
+  };
+
   virtual ~FileInfo();
 
-  // Not guaranteed to return a meaningful value for directories
+  bool operator==(const FileInfo& other) const { return GetAddress() == other.GetAddress(); }
+  bool operator!=(const FileInfo& other) const { return !operator==(other); }
+  virtual std::unique_ptr<FileInfo> clone() const = 0;
+  virtual const_iterator cbegin() const { return begin(); }
+  virtual const_iterator cend() const { return end(); }
+  virtual const_iterator begin() const = 0;
+  virtual const_iterator end() const = 0;
+
+  // The offset of a file on the disc (inside the partition, if there is one).
+  // Not guaranteed to return a meaningful value for directories.
   virtual u64 GetOffset() const = 0;
-  // Not guaranteed to return a meaningful value for directories
+  // The size of a file.
+  // Not guaranteed to return a meaningful value for directories.
   virtual u32 GetSize() const = 0;
   virtual bool IsDirectory() const = 0;
+  // The number of files and directories in a directory, including those in subdirectories.
+  // Not guaranteed to return a meaningful value for files.
+  virtual u32 GetTotalChildren() const = 0;
   virtual std::string GetName() const = 0;
+  // GetPath will find the parents of the current object and call GetName on them,
+  // so it's slower than other functions. If you're traversing through folders
+  // to get a file and its path, building the path while traversing is faster.
+  virtual std::string GetPath() const = 0;
+
+protected:
+  // Only used for comparisons with other file info objects
+  virtual uintptr_t GetAddress() const = 0;
+
+  // Called by iterators
+  virtual FileInfo& operator++() = 0;
 };
 
 class FileSystem
 {
 public:
   FileSystem(const Volume* _rVolume, const Partition& partition);
-
   virtual ~FileSystem();
+
+  // If IsValid is false, GetRoot must not be called. CreateFileSystem
+  // takes care of this automatically, so other code is recommended to use it.
   virtual bool IsValid() const = 0;
-  // TODO: Should only return FileInfo, not FileInfoGCWii
-  virtual const std::vector<FileInfoGCWii>& GetFileList() const = 0;
-  virtual const FileInfo* FindFileInfo(const std::string& path) const = 0;
-  virtual const FileInfo* FindFileInfo(u64 disc_offset) const = 0;
+  // The object returned by GetRoot and all objects created from it
+  // are only valid for as long as the file system object is valid.
+  virtual const FileInfo& GetRoot() const = 0;
+  // Returns nullptr if not found
+  virtual std::unique_ptr<FileInfo> FindFileInfo(const std::string& path) const = 0;
+  // Returns nullptr if not found
+  virtual std::unique_ptr<FileInfo> FindFileInfo(u64 disc_offset) const = 0;
+
   virtual u64 ReadFile(const FileInfo* file_info, u8* _pBuffer, u64 _MaxBufferSize,
                        u64 _OffsetInFile = 0) const = 0;
-  virtual bool ExportFile(const FileInfo* file_info,
-                          const std::string& _rExportFilename) const = 0;
+  virtual bool ExportFile(const FileInfo* file_info, const std::string& _rExportFilename) const = 0;
   virtual bool ExportApploader(const std::string& _rExportFolder) const = 0;
   virtual bool ExportDOL(const std::string& _rExportFolder) const = 0;
-  virtual std::string GetPath(u64 _Address) const = 0;
-  virtual std::string GetPathFromFSTOffset(size_t file_info_offset) const = 0;
   virtual std::optional<u64> GetBootDOLOffset() const = 0;
   virtual std::optional<u32> GetBootDOLSize(u64 dol_offset) const = 0;
 
@@ -59,6 +134,7 @@ protected:
   const Partition m_partition;
 };
 
+// Returns nullptr if a valid file system could not be created
 std::unique_ptr<FileSystem> CreateFileSystem(const Volume* volume, const Partition& partition);
 
 }  // namespace

dolphin-ios/Source/Core/DiscIO/VolumeGC.cpp
@@ -177,7 +177,7 @@ void VolumeGC::LoadBannerFile() const
   if (!file_system)
     return;
 
-  const FileInfo* file_info = file_system->FindFileInfo("opening.bnr");
+  std::unique_ptr<FileInfo> file_info = file_system->FindFileInfo("opening.bnr");
   if (!file_info)
     return;
 
@@ -190,7 +190,8 @@ void VolumeGC::LoadBannerFile() const
     return;
   }
 
-  if (file_size != file_system->ReadFile(file_info, reinterpret_cast<u8*>(&banner_file), file_size))
+  if (file_size !=
+      file_system->ReadFile(file_info.get(), reinterpret_cast<u8*>(&banner_file), file_size))
   {
     WARN_LOG(DISCIO, "Could not read opening.bnr.");
     return;