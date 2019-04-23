davyson441/dolphin

WiimoteEmu: MotionPlus is now working.

Source/Core/Core/HW/WiimoteEmu/Dynamics.cpp
@@ -164,11 +164,16 @@ void EmulateSwing(MotionState* state, ControllerEmu::Force* swing_group, float t
   // TODO: If we wanted to be less hacky we could use ApproachAngleWithAccel.
   const auto angle = state->position / swing_group->GetMaxDistance() * swing_group->GetTwistAngle();
 
-  const auto old_angle = state->angle;
-  state->angle = {-angle.z, 0, angle.x};
+  // const auto old_angle = state->angle;
+  // state->angle = {-angle.z, 0, angle.x};
 
-  // Update velocity based on change in angle.
-  state->angular_velocity = state->angle - old_angle;
+  // // Update velocity based on change in angle.
+  // state->angular_velocity = state->angle - old_angle;
+
+  // TODO: expose this setting in UI or calculate something sensible from jerk.
+  constexpr auto MAX_ACCEL = float(MathUtil::TAU * 50);
+
+  ApproachAngleWithAccel(state, {-angle.z, 0, angle.x}, MAX_ACCEL, time_elapsed);
 }
 
 WiimoteCommon::DataReportBuilder::AccelData ConvertAccelData(const Common::Vec3& accel, u16 zero_g,
@@ -184,7 +189,7 @@ WiimoteCommon::DataReportBuilder::AccelData ConvertAccelData(const Common::Vec3&
           u16(MathUtil::Clamp(std::lround(scaled_accel.z + zero_g), 0l, MAX_VALUE))};
 }
 
-Common::Matrix44 EmulateCursorMovement(ControllerEmu::Cursor* ir_group)
+void EmulateCursor(MotionState* state, ControllerEmu::Cursor* ir_group, float time_elapsed)
 {
   const auto cursor = ir_group->GetState(true);
 
@@ -193,18 +198,27 @@ Common::Matrix44 EmulateCursorMovement(ControllerEmu::Cursor* ir_group)
 
   // Values are optimized for default settings in "Super Mario Galaxy 2"
   // This seems to be acceptable for a good number of games.
-  constexpr float YAW_ANGLE = 0.1472f;
-  constexpr float PITCH_ANGLE = 0.121f;
+  // constexpr float YAW_ANGLE = 0.1472f;
+  // constexpr float PITCH_ANGLE = 0.121f;
+  constexpr float YAW_ANGLE = 0.3f / 5 * 1.435f * 3;
+  constexpr float PITCH_ANGLE = 0.3f / 5 * 1.34f * 3;
 
   // Nintendo recommends a distance of 1-3 meters.
-  constexpr float NEUTRAL_DISTANCE = 2.f;
-
+  constexpr float NEUTRAL_DISTANCE = 3.f;
   constexpr float MOVE_DISTANCE = 1.f;
 
-  return Matrix44::Translate({0, MOVE_DISTANCE * float(cursor.z), 0}) *
-         Matrix44::FromMatrix33(Matrix33::RotateX(PITCH_ANGLE * cursor.y) *
-                                Matrix33::RotateZ(YAW_ANGLE * cursor.x)) *
-         Matrix44::Translate({0, -NEUTRAL_DISTANCE, 0});
+  // TODO: Move state out of ControllerEmu::Cursor
+  // TODO: Use ApproachPositionWithJerk
+  // TODO: Move forward/backward after rotation.
+  const auto new_position = Common::Vec3{0, NEUTRAL_DISTANCE - MOVE_DISTANCE * float(cursor.z), 0};
+  state->acceleration = new_position - state->position;
+  state->position = new_position;
+
+  // TODO: expose this setting in UI:
+  constexpr auto MAX_ACCEL = float(MathUtil::TAU * 100);
+
+  ApproachAngleWithAccel(state, Common::Vec3(PITCH_ANGLE * -cursor.y, 0, YAW_ANGLE * -cursor.x),
+                         MAX_ACCEL, time_elapsed);
 }
 
 void ApproachAngleWithAccel(RotationalState* state, const Common::Vec3& angle_target,

kiddreads/Avalon

dolphin-ios/Source/Core/Core/HW/WiimoteEmu/Dynamics.cpp
@@ -129,7 +129,7 @@ WiimoteCommon::DataReportBuilder::AccelData ConvertAccelData(const Common::Vec3&
           u16(MathUtil::Clamp(std::lround(scaled_accel.z + zero_g), 0l, MAX_VALUE))};
 }
 
-Common::Matrix44 EmulateCursorMovement(ControllerEmu::Cursor* ir_group)
+void EmulateCursor(MotionState* state, ControllerEmu::Cursor* ir_group, float time_elapsed)
 {
   using Common::Matrix33;
   using Common::Matrix44;
@@ -149,10 +149,19 @@ Common::Matrix44 EmulateCursorMovement(ControllerEmu::Cursor* ir_group)
 
   const auto cursor = ir_group->GetState(true);
 
-  return Matrix44::Translate({0, MOVE_DISTANCE * float(cursor.z), 0}) *
-         Matrix44::FromMatrix33(Matrix33::RotateX(pitch_scale * cursor.y) *
-                                Matrix33::RotateZ(yaw_scale * cursor.x)) *
-         Matrix44::Translate({0, -NEUTRAL_DISTANCE, height});
+  // TODO: Move state out of ControllerEmu::Cursor
+  // TODO: Use ApproachPositionWithJerk
+  // TODO: Move forward/backward after rotation.
+  const auto new_position =
+      Common::Vec3{0, NEUTRAL_DISTANCE - MOVE_DISTANCE * float(cursor.z), height};
+  state->acceleration = new_position - state->position;
+  state->position = new_position;
+
+  // TODO: expose this setting in UI:
+  constexpr auto MAX_ACCEL = float(MathUtil::TAU * 100);
+
+  ApproachAngleWithAccel(state, Common::Vec3(pitch_scale * -cursor.y, 0, yaw_scale * -cursor.x),
+                         MAX_ACCEL, time_elapsed);
 }
 
 void ApproachAngleWithAccel(RotationalState* state, const Common::Vec3& angle_target,