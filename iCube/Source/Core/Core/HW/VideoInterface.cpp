adit-chandra/dolphin

Step MemoryWatcher at end of each video frame.

Source/Core/Core/HW/VideoInterface.cpp
@@ -722,6 +722,7 @@ static void BeginField(FieldType field, u64 ticks)
 static void EndField()
 {
   Core::VideoThrottle();
+  Core::FrameAdvance();
 }
 
 // Purpose: Send VI interrupt when triggered

kiddreads/Avalon

iCube/Source/Core/Core/HW/VideoInterface.cpp
@@ -726,6 +726,7 @@ static void BeginField(FieldType field, u64 ticks)
 static void EndField()
 {
   Core::VideoThrottle();
+  Core::OnFrameEnd();
 }
 
 // Purpose: Send VI interrupt when triggered