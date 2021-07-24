kiddreads/Avalon

commit e2f7044da130a981b4de7266a1f12d7fd35f3159

DiscIO: Add workaround for MSVC ARM64 ICE

iCube/Source/Core/DiscIO/WIABlob.h
@@ -70,7 +70,7 @@ private:
   using SHA1 = std::array<u8, 20>;
   using WiiKey = std::array<u8, 16>;
 
-  // See docs/WIA.md for details about the format
+  // See docs/WiaAndRvz.md for details about the format
 
 #pragma pack(push, 1)
   struct WIAHeader1
@@ -241,24 +241,22 @@ private:
 
   struct ReuseID
   {
-    bool operator==(const ReuseID& other) const
-    {
-      return std::tie(partition_key, data_size, encrypted, value) ==
-             std::tie(other.partition_key, other.data_size, other.encrypted, other.value);
-    }
-    bool operator<(const ReuseID& other) const
-    {
-      return std::tie(partition_key, data_size, encrypted, value) <
-             std::tie(other.partition_key, other.data_size, other.encrypted, other.value);
-    }
-    bool operator>(const ReuseID& other) const
-    {
-      return std::tie(partition_key, data_size, encrypted, value) >
-             std::tie(other.partition_key, other.data_size, other.encrypted, other.value);
-    }
+    // This is a workaround for an ICE in Visual Studio 16.10.0 when making an ARM64 Release build.
+    // Once the ICE has been fixed upstream, we can move partition_key inside the tie.
+#define COMPARE_TIED(op)                                                                           \
+  (partition_key op other.partition_key) &&                                                        \
+      std::tie(data_size, encrypted, value)                                                        \
+          op std::tie(other.data_size, other.encrypted, other.value)
+
+    bool operator==(const ReuseID& other) const { return COMPARE_TIED(==); }
+    bool operator<(const ReuseID& other) const { return COMPARE_TIED(<); }
+    bool operator>(const ReuseID& other) const { return COMPARE_TIED(>); }
+
     bool operator!=(const ReuseID& other) const { return !operator==(other); }
     bool operator>=(const ReuseID& other) const { return !operator<(other); }
     bool operator<=(const ReuseID& other) const { return !operator>(other); }
+
+#undef COMPARE_TIED
 
     const WiiKey* partition_key;
     u64 data_size;

commit fcb3ed75892fe91b0ef098969d805747c9e337d7

DiscIO: Fix broken workaround for MSVC ARM64 ICE
The workaround added in e2f7044 caused a regression where Dolphin
incorrectly replaced runs of one byte with runs of another byte
when writing WIA and RVZ files. ReuseID::operator< was always
returning false unless the ReuseIDs being compared had different
partition keys, which caused std::map<ReuseID, GroupEntry>
to treat all ReuseIDs with the same partition key as equal.

iCube/Source/Core/DiscIO/WIABlob.h
@@ -240,22 +240,26 @@ private:
 
   struct ReuseID
   {
-    // This is a workaround for an ICE in Visual Studio 16.10.0 when making an ARM64 Release build.
-    // Once the ICE has been fixed upstream, we can move partition_key inside the tie.
-#define COMPARE_TIED(op)                                                                           \
-  (partition_key op other.partition_key) &&                                                        \
-      std::tie(data_size, encrypted, value)                                                        \
-          op std::tie(other.data_size, other.encrypted, other.value)
+    // This code is a workaround for an ICE in Visual Studio 16.10.0 when making an ARM64 Release
+    // build. Once a fixed version of Visual Studio is released, we can use std::tie instead.
+#define COMPARE_REUSE_ID_INNER(op, f, next) ((f != other.f) ? (f op other.f) : (next))
+#define COMPARE_REUSE_ID(op, equal_result)                                                         \
+  COMPARE_REUSE_ID_INNER(                                                                          \
+      op, partition_key,                                                                           \
+      COMPARE_REUSE_ID_INNER(                                                                      \
+          op, data_size,                                                                           \
+          COMPARE_REUSE_ID_INNER(op, encrypted, COMPARE_REUSE_ID_INNER(op, value, equal_result))))
 
-    bool operator==(const ReuseID& other) const { return COMPARE_TIED(==); }
-    bool operator<(const ReuseID& other) const { return COMPARE_TIED(<); }
-    bool operator>(const ReuseID& other) const { return COMPARE_TIED(>); }
+    bool operator==(const ReuseID& other) const { return COMPARE_REUSE_ID(==, true); }
+    bool operator<(const ReuseID& other) const { return COMPARE_REUSE_ID(<, false); }
+    bool operator>(const ReuseID& other) const { return COMPARE_REUSE_ID(>, false); }
 
     bool operator!=(const ReuseID& other) const { return !operator==(other); }
     bool operator>=(const ReuseID& other) const { return !operator<(other); }
     bool operator<=(const ReuseID& other) const { return !operator>(other); }
 
-#undef COMPARE_TIED
+#undef COMPARE_REUSE_ID_INNER
+#undef COMPARE_REUSE_ID
 
     WiiKey partition_key;
     u64 data_size;