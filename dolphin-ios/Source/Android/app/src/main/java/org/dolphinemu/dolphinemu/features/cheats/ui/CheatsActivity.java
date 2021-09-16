JosJuice/dolphin

Android: Implement basic read-only cheats list

Source/Android/app/src/main/java/org/dolphinemu/dolphinemu/features/cheats/ui/CheatsActivity.java
@@ -4,8 +4,14 @@ package org.dolphinemu.dolphinemu.features.cheats.ui;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
+
+import org.dolphinemu.dolphinemu.R;
+import org.dolphinemu.dolphinemu.features.cheats.model.CheatsViewModel;
+import org.dolphinemu.dolphinemu.ui.main.MainPresenter;
 
 public class CheatsActivity extends AppCompatActivity
 {
@@ -18,5 +24,24 @@ public class CheatsActivity extends AppCompatActivity
     intent.putExtra(ARG_GAME_ID, gameId);
     intent.putExtra(ARG_REVISION, revision);
     context.startActivity(intent);
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState)
+  {
+    super.onCreate(savedInstanceState);
+
+    MainPresenter.skipRescanningLibrary();
+
+    Intent intent = getIntent();
+    String gameID = intent.getStringExtra(ARG_GAME_ID);
+    int revision = intent.getIntExtra(ARG_REVISION, 0);
+
+    setTitle(getString(R.string.preferences_cheats_with_game_id, gameID));
+
+    CheatsViewModel viewModel = new ViewModelProvider(this).get(CheatsViewModel.class);
+    viewModel.load(gameID, revision);
+
+    setContentView(R.layout.activity_cheats);
   }
 }

kiddreads/Avalon

dolphin-ios/Source/Android/app/src/main/java/org/dolphinemu/dolphinemu/features/cheats/ui/CheatsActivity.java
@@ -4,8 +4,14 @@ package org.dolphinemu.dolphinemu.features.cheats.ui;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
+
+import org.dolphinemu.dolphinemu.R;
+import org.dolphinemu.dolphinemu.features.cheats.model.CheatsViewModel;
+import org.dolphinemu.dolphinemu.ui.main.MainPresenter;
 
 public class CheatsActivity extends AppCompatActivity
 {
@@ -18,5 +24,24 @@ public class CheatsActivity extends AppCompatActivity
     intent.putExtra(ARG_GAME_ID, gameId);
     intent.putExtra(ARG_REVISION, revision);
     context.startActivity(intent);
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState)
+  {
+    super.onCreate(savedInstanceState);
+
+    MainPresenter.skipRescanningLibrary();
+
+    Intent intent = getIntent();
+    String gameId = intent.getStringExtra(ARG_GAME_ID);
+    int revision = intent.getIntExtra(ARG_REVISION, 0);
+
+    setTitle(getString(R.string.cheats_with_game_id, gameId));
+
+    CheatsViewModel viewModel = new ViewModelProvider(this).get(CheatsViewModel.class);
+    viewModel.load(gameId, revision);
+
+    setContentView(R.layout.activity_cheats);
   }
 }