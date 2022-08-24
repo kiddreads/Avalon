dolphin-emu/dolphin

BBA/BuiltIn: Add SSDP multicast support

Source/Core/Core/HW/EXI/BBA/BuiltIn.cpp
@@ -3,7 +3,9 @@
 
 #include "Core/HW/EXI/BBA/BuiltIn.h"
 
-#ifndef _WIN32
+#ifdef _WIN32
+#include <ws2ipdef.h>
+#else
 #include <sys/socket.h>
 #include <sys/types.h>
 #endif
@@ -11,6 +13,7 @@
 #include "Common/BitUtils.h"
 #include "Common/Logging/Log.h"
 #include "Common/MsgHandler.h"
+#include "Common/ScopeGuard.h"
 #include "Core/HW/EXI/EXI_Device.h"
 #include "Core/HW/EXI/EXI_DeviceEthernet.h"
 
@@ -468,7 +471,7 @@ void CEXIETHERNET::BuiltInBBAInterface::HandleUDPFrame(const Common::UDPPacket& 
         ERROR_LOG_FMT(SP1, "Couldn't open UDP socket");
         return;
       }
-      if (ntohs(udp_header.destination_port) == 1900)
+      if (ntohs(udp_header.destination_port) == Common::SSDP_PORT)
       {
         InitUDPPort(26512);  // MK DD and 1080
         InitUDPPort(26502);  // Air Ride
@@ -479,10 +482,7 @@ void CEXIETHERNET::BuiltInBBAInterface::HandleUDPFrame(const Common::UDPPacket& 
           reply.eth_header.destination = hwdata.source;
           reply.eth_header.source = hwdata.destination;
           reply.ip_header.destination_addr = ip_header.source_addr;
-          if (ip_header.destination_addr == Common::IP_ADDR_SSDP)
-            reply.ip_header.source_addr = Common::IP_ADDR_BROADCAST;
-          else
-            reply.ip_header.source_addr = Common::BitCast<Common::IPAddress>(destination_addr);
+          reply.ip_header.source_addr = Common::BitCast<Common::IPAddress>(destination_addr);
           WriteToQueue(reply.Build());
         }
       }
@@ -490,8 +490,6 @@ void CEXIETHERNET::BuiltInBBAInterface::HandleUDPFrame(const Common::UDPPacket& 
   }
   if (ntohs(udp_header.destination_port) == 53)
     target = sf::IpAddress(m_dns_ip.c_str());  // dns server ip
-  else if (ip_header.destination_addr == Common::IP_ADDR_SSDP)
-    target = sf::IpAddress(0xFFFFFFFF);  // force real broadcast
   else
     target = sf::IpAddress(ntohl(Common::BitCast<u32>(ip_header.destination_addr)));
   ref->udp_socket.send(data.data(), data.size(), target, ntohs(udp_header.destination_port));
@@ -710,5 +708,68 @@ BbaUdpSocket::BbaUdpSocket() = default;
 
 sf::Socket::Status BbaUdpSocket::Bind(u16 port)
 {
-  return this->bind(port, sf::IpAddress(ntohl(Common::GetDefaultIp())));
+  const u32 inet_ip = Common::GetDefaultIp();
+  if (port != Common::SSDP_PORT)
+    return this->bind(port, sf::IpAddress(ntohl(inet_ip)));
+
+  // Handle SSDP multicast
+  create();
+  const int on = 1;
+  if (setsockopt(getHandle(), SOL_SOCKET, SO_REUSEADDR, reinterpret_cast<const char*>(&on),
+                 sizeof(on)) != 0)
+  {
+    ERROR_LOG_FMT(SP1, "setsockopt failed to reuse SSDP address");
+  }
+#ifdef SO_REUSEPORT
+  if (setsockopt(getHandle(), SOL_SOCKET, SO_REUSEPORT, reinterpret_cast<const char*>(&on),
+                 sizeof(on)) != 0)
+  {
+    ERROR_LOG_FMT(SP1, "setsockopt failed to reuse SSDP port");
+  }
+#endif
+  if (const char loop = 1;
+      setsockopt(getHandle(), IPPROTO_IP, IP_MULTICAST_LOOP, &loop, sizeof(loop)) != 0)
+  {
+    ERROR_LOG_FMT(SP1, "setsockopt failed to set SSDP loopback");
+  }
+
+  // sf::UdpSocket::bind will close the socket and get rid of its options
+  sockaddr_in addr;
+  addr.sin_addr.s_addr = INADDR_ANY;
+  addr.sin_family = AF_INET;
+  addr.sin_port = htons(Common::SSDP_PORT);
+  Common::ScopeGuard error_guard([this] { close(); });
+  if (::bind(getHandle(), reinterpret_cast<const sockaddr*>(&addr), sizeof(addr)) != 0)
+  {
+    addr.sin_addr.s_addr = inet_ip;
+    if (::bind(getHandle(), reinterpret_cast<const sockaddr*>(&addr), sizeof(addr)) != 0)
+      return sf::Socket::Status::Error;
+  }
+  else
+  {
+    addr.sin_addr.s_addr = inet_ip;  // Set this here for IP_MULTICAST_IF
+  }
+
+  // Bind to the right interface
+  if (setsockopt(getHandle(), IPPROTO_IP, IP_MULTICAST_IF,
+                 reinterpret_cast<const char*>(&addr.sin_addr), sizeof(addr.sin_addr)) != 0)
+  {
+    ERROR_LOG_FMT(SP1, "setsockopt failed to bind to the network interface");
+    return sf::Socket::Status::Error;
+  }
+
+  // Subscribe to the SSDP multicast group
+  // NB: Other groups aren't supported because of HLE
+  struct ip_mreq mreq;
+  mreq.imr_multiaddr.s_addr = Common::BitCast<u32>(Common::IP_ADDR_SSDP);
+  mreq.imr_interface.s_addr = inet_ip;
+  if (setsockopt(getHandle(), IPPROTO_IP, IP_ADD_MEMBERSHIP, reinterpret_cast<const char*>(&mreq),
+                 sizeof(mreq)) != 0)
+  {
+    ERROR_LOG_FMT(SP1, "setsockopt failed to subscribe to SSDP multicast group");
+    return sf::Socket::Status::Error;
+  }
+
+  error_guard.Dismiss();
+  return sf::Socket::Status::Done;
 }

kiddreads/Avalon

iCube/Source/Core/Core/HW/EXI/BBA/BuiltIn.cpp
@@ -1,12 +1,19 @@
 // Copyright 2022 Dolphin Emulator Project
 // SPDX-License-Identifier: GPL-2.0-or-later
 
-#include <SFML/Network.hpp>
+#include "Core/HW/EXI/BBA/BuiltIn.h"
+
+#ifdef _WIN32
+#include <ws2ipdef.h>
+#else
+#include <sys/socket.h>
+#include <sys/types.h>
+#endif
 
 #include "Common/BitUtils.h"
 #include "Common/Logging/Log.h"
 #include "Common/MsgHandler.h"
-#include "Core/HW/EXI/BBA/BuiltIn.h"
+#include "Common/ScopeGuard.h"
 #include "Core/HW/EXI/EXI_Device.h"
 #include "Core/HW/EXI/EXI_DeviceEthernet.h"
 
@@ -462,25 +469,20 @@ void CEXIETHERNET::BuiltInBBAInterface::HandleUDPFrame(const Common::UDPPacket& 
         ERROR_LOG_FMT(SP1, "Couldn't open UDP socket");
         return;
       }
-      if (ntohs(udp_header.destination_port) == 1900 && ntohs(udp_header.length) > 150)
+      if (ntohs(udp_header.destination_port) == Common::SSDP_PORT && ntohs(udp_header.length) > 150)
       {
         // Quick hack to unlock the connection, throw it back at him
         Common::UDPPacket reply = packet;
         reply.eth_header.destination = hwdata.source;
         reply.eth_header.source = hwdata.destination;
         reply.ip_header.destination_addr = ip_header.source_addr;
-        if (ip_header.destination_addr == Common::IP_ADDR_SSDP)
-          reply.ip_header.source_addr = Common::IP_ADDR_BROADCAST;
-        else
-          reply.ip_header.source_addr = Common::BitCast<Common::IPAddress>(destination_addr);
+        reply.ip_header.source_addr = Common::BitCast<Common::IPAddress>(destination_addr);
         WriteToQueue(reply.Build());
       }
     }
   }
   if (ntohs(udp_header.destination_port) == 53)
     target = sf::IpAddress(m_dns_ip.c_str());  // dns server ip
-  else if (ip_header.destination_addr == Common::IP_ADDR_SSDP)
-    target = sf::IpAddress(0xFFFFFFFF);  // force real broadcast
   else
     target = sf::IpAddress(ntohl(Common::BitCast<u32>(ip_header.destination_addr)));
   ref->udp_socket.send(data.data(), data.size(), target, ntohs(udp_header.destination_port));
@@ -729,5 +731,75 @@ BbaUdpSocket::BbaUdpSocket() = default;
 
 sf::Socket::Status BbaUdpSocket::Bind(u16 port, u32 net_ip)
 {
-  return this->bind(port, sf::IpAddress(ntohl(net_ip)));
+  if (port != Common::SSDP_PORT)
+    return this->bind(port, sf::IpAddress(ntohl(net_ip)));
+
+  // Handle SSDP multicast
+  create();
+  const int on = 1;
+  if (setsockopt(getHandle(), SOL_SOCKET, SO_REUSEADDR, reinterpret_cast<const char*>(&on),
+                 sizeof(on)) != 0)
+  {
+    ERROR_LOG_FMT(SP1, "setsockopt failed to reuse SSDP address: {}", Common::StrNetworkError());
+  }
+#ifdef SO_REUSEPORT
+  if (setsockopt(getHandle(), SOL_SOCKET, SO_REUSEPORT, reinterpret_cast<const char*>(&on),
+                 sizeof(on)) != 0)
+  {
+    ERROR_LOG_FMT(SP1, "setsockopt failed to reuse SSDP port: {}", Common::StrNetworkError());
+  }
+#endif
+  if (const char loop = 1;
+      setsockopt(getHandle(), IPPROTO_IP, IP_MULTICAST_LOOP, &loop, sizeof(loop)) != 0)
+  {
+    ERROR_LOG_FMT(SP1, "setsockopt failed to set SSDP loopback: {}", Common::StrNetworkError());
+  }
+
+  // sf::UdpSocket::bind will close the socket and get rid of its options
+  sockaddr_in addr;
+  addr.sin_addr.s_addr = INADDR_ANY;
+  addr.sin_family = AF_INET;
+  addr.sin_port = htons(Common::SSDP_PORT);
+  Common::ScopeGuard error_guard([this] { close(); });
+  if (::bind(getHandle(), reinterpret_cast<const sockaddr*>(&addr), sizeof(addr)) != 0)
+  {
+    WARN_LOG_FMT(SP1, "bind with SSDP port and INADDR_ANY failed: {}", Common::StrNetworkError());
+    addr.sin_addr.s_addr = net_ip;
+    if (::bind(getHandle(), reinterpret_cast<const sockaddr*>(&addr), sizeof(addr)) != 0)
+    {
+      ERROR_LOG_FMT(SP1, "bind with SSDP port failed: {}", Common::StrNetworkError());
+      return sf::Socket::Status::Error;
+    }
+  }
+  else
+  {
+    addr.sin_addr.s_addr = net_ip;  // Set this here for IP_MULTICAST_IF
+  }
+  INFO_LOG_FMT(SP1, "SSDP bind successful");
+
+  // Bind to the right interface
+  if (setsockopt(getHandle(), IPPROTO_IP, IP_MULTICAST_IF,
+                 reinterpret_cast<const char*>(&addr.sin_addr), sizeof(addr.sin_addr)) != 0)
+  {
+    ERROR_LOG_FMT(SP1, "setsockopt failed to bind to the network interface: {}",
+                  Common::StrNetworkError());
+    return sf::Socket::Status::Error;
+  }
+
+  // Subscribe to the SSDP multicast group
+  // NB: Other groups aren't supported because of HLE
+  struct ip_mreq mreq;
+  mreq.imr_multiaddr.s_addr = Common::BitCast<u32>(Common::IP_ADDR_SSDP);
+  mreq.imr_interface.s_addr = net_ip;
+  if (setsockopt(getHandle(), IPPROTO_IP, IP_ADD_MEMBERSHIP, reinterpret_cast<const char*>(&mreq),
+                 sizeof(mreq)) != 0)
+  {
+    ERROR_LOG_FMT(SP1, "setsockopt failed to subscribe to SSDP multicast group: {}",
+                  Common::StrNetworkError());
+    return sf::Socket::Status::Error;
+  }
+
+  error_guard.Dismiss();
+  INFO_LOG_FMT(SP1, "SSDP multicast membership successful");
+  return sf::Socket::Status::Done;
 }