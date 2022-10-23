K0bin/dolphin

VideoBackends:Vulkan: Use VMA for staging buffers

Source/Core/VideoBackends/Vulkan/StagingBuffer.h
@@ -13,8 +13,8 @@ namespace Vulkan
 class StagingBuffer
 {
 public:
-  StagingBuffer(STAGING_BUFFER_TYPE type, VkBuffer buffer, VkDeviceMemory memory, VkDeviceSize size,
-                bool coherent);
+  StagingBuffer(STAGING_BUFFER_TYPE type, VkBuffer buffer, VmaAllocation allocation,
+                VkDeviceSize size, char* map_ptr);
   virtual ~StagingBuffer();
 
   STAGING_BUFFER_TYPE GetType() const { return m_type; }
@@ -23,9 +23,7 @@ public:
   bool IsMapped() const { return m_map_pointer != nullptr; }
   const char* GetMapPointer() const { return m_map_pointer; }
   char* GetMapPointer() { return m_map_pointer; }
-  VkDeviceSize GetMapOffset() const { return m_map_offset; }
-  VkDeviceSize GetMapSize() const { return m_map_size; }
-  bool Map(VkDeviceSize offset = 0, VkDeviceSize size = VK_WHOLE_SIZE);
+  bool Map();
   void Unmap();
 
   // Upload part 1: Prepare from device read from the CPU side
@@ -60,7 +58,7 @@ public:
 
   // Allocates the resources needed to create a staging buffer.
   static bool AllocateBuffer(STAGING_BUFFER_TYPE type, VkDeviceSize size, VkBufferUsageFlags usage,
-                             VkBuffer* out_buffer, VkDeviceMemory* out_memory, bool* out_coherent);
+                             VkBuffer* out_buffer, VmaAllocation* alloc, char** out_map_ptr);
 
   // Wrapper for creating an barrier on a buffer
   static void BufferMemoryBarrier(VkCommandBuffer command_buffer, VkBuffer buffer,
@@ -72,12 +70,9 @@ public:
 protected:
   STAGING_BUFFER_TYPE m_type;
   VkBuffer m_buffer;
-  VkDeviceMemory m_memory;
+  VmaAllocation m_alloc;
   VkDeviceSize m_size;
-  bool m_coherent;
 
   char* m_map_pointer = nullptr;
-  VkDeviceSize m_map_offset = 0;
-  VkDeviceSize m_map_size = 0;
 };
 }  // namespace Vulkan

kiddreads/Avalon

dolphin-ios/Source/Core/VideoBackends/Vulkan/StagingBuffer.h
@@ -13,8 +13,8 @@ namespace Vulkan
 class StagingBuffer
 {
 public:
-  StagingBuffer(STAGING_BUFFER_TYPE type, VkBuffer buffer, VkDeviceMemory memory, VkDeviceSize size,
-                bool coherent);
+  StagingBuffer(STAGING_BUFFER_TYPE type, VkBuffer buffer, VmaAllocation allocation,
+                VkDeviceSize size, char* map_ptr);
   virtual ~StagingBuffer();
 
   STAGING_BUFFER_TYPE GetType() const { return m_type; }
@@ -23,9 +23,7 @@ public:
   bool IsMapped() const { return m_map_pointer != nullptr; }
   const char* GetMapPointer() const { return m_map_pointer; }
   char* GetMapPointer() { return m_map_pointer; }
-  VkDeviceSize GetMapOffset() const { return m_map_offset; }
-  VkDeviceSize GetMapSize() const { return m_map_size; }
-  bool Map(VkDeviceSize offset = 0, VkDeviceSize size = VK_WHOLE_SIZE);
+  bool Map();
   void Unmap();
 
   // Upload part 1: Prepare from device read from the CPU side
@@ -60,7 +58,7 @@ public:
 
   // Allocates the resources needed to create a staging buffer.
   static bool AllocateBuffer(STAGING_BUFFER_TYPE type, VkDeviceSize size, VkBufferUsageFlags usage,
-                             VkBuffer* out_buffer, VkDeviceMemory* out_memory, bool* out_coherent);
+                             VkBuffer* out_buffer, VmaAllocation* out_alloc, char** out_map_ptr);
 
   // Wrapper for creating an barrier on a buffer
   static void BufferMemoryBarrier(VkCommandBuffer command_buffer, VkBuffer buffer,
@@ -72,12 +70,9 @@ public:
 protected:
   STAGING_BUFFER_TYPE m_type;
   VkBuffer m_buffer;
-  VkDeviceMemory m_memory;
+  VmaAllocation m_alloc;
   VkDeviceSize m_size;
-  bool m_coherent;
 
   char* m_map_pointer = nullptr;
-  VkDeviceSize m_map_offset = 0;
-  VkDeviceSize m_map_size = 0;
 };
 }  // namespace Vulkan