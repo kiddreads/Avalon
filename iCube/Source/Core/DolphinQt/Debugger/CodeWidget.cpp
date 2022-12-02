kiddreads/Avalon

commit 0ac47b67be935f8da02cc866c470c4b68f1d098d

CodeViewWidget: Add WithDetailedUpdate to update CodeWidget
This used to also update the function calls and callers.

iCube/Source/Core/DolphinQt/Debugger/CodeWidget.cpp
@@ -170,6 +170,7 @@ void CodeWidget::ConnectWidgets()
   connect(m_code_view, &CodeViewWidget::SymbolsChanged, this, &CodeWidget::UpdateSymbols);
   connect(m_code_view, &CodeViewWidget::BreakpointsChanged, this,
           [this] { emit BreakpointsChanged(); });
+  connect(m_code_view, &CodeViewWidget::UpdateCodeWidget, this, &CodeWidget::Update);
 
   connect(m_code_view, &CodeViewWidget::RequestPPCComparison, this,
           &CodeWidget::RequestPPCComparison);

iCube/Source/Core/DolphinQt/Debugger/CodeViewWidget.cpp
@@ -442,8 +442,19 @@ void CodeViewWidget::SetAddress(u32 address, SetAddressUpdate update)
     return;
 
   m_address = address;
-  if (update == SetAddressUpdate::WithUpdate)
+  switch (update)
+  {
+  case SetAddressUpdate::WithoutUpdate:
+    return;
+  case SetAddressUpdate::WithUpdate:
+    // Update the CodeViewWidget
     Update();
+    break;
+  case SetAddressUpdate::WithDetailedUpdate:
+    // Update the CodeWidget's views (code view, function calls/callers, ...)
+    emit UpdateCodeWidget();
+    break;
+  }
 }
 
 void CodeViewWidget::ReplaceAddress(u32 address, ReplaceWith replace)
@@ -611,7 +622,7 @@ void CodeViewWidget::OnFollowBranch()
   if (!branch_addr)
     return;
 
-  SetAddress(branch_addr, SetAddressUpdate::WithUpdate);
+  SetAddress(branch_addr, SetAddressUpdate::WithDetailedUpdate);
 }
 
 void CodeViewWidget::OnRenameSymbol()
@@ -785,7 +796,7 @@ void CodeViewWidget::mousePressEvent(QMouseEvent* event)
     if (column(item) == CODE_VIEW_COLUMN_BREAKPOINT)
       ToggleBreakpoint();
     else
-      SetAddress(addr, SetAddressUpdate::WithUpdate);
+      SetAddress(addr, SetAddressUpdate::WithDetailedUpdate);
 
     Update();
     break;

iCube/Source/Core/DolphinQt/Debugger/CodeViewWidget.h
@@ -25,7 +25,8 @@ public:
   enum class SetAddressUpdate
   {
     WithUpdate,
-    WithoutUpdate
+    WithoutUpdate,
+    WithDetailedUpdate
   };
 
   explicit CodeViewWidget();
@@ -49,6 +50,7 @@ signals:
   void ShowMemory(u32 address);
   void SymbolsChanged();
   void BreakpointsChanged();
+  void UpdateCodeWidget();
 
 private:
   enum class ReplaceWith

iCube/Source/Core/DolphinQt/MainWindow.cpp
@@ -419,7 +419,7 @@ void MainWindow::CreateComponents()
   };
   const auto request_view_in_memory = [this](u32 addr) { m_memory_widget->SetAddress(addr); };
   const auto request_view_in_code = [this](u32 addr) {
-    m_code_widget->SetAddress(addr, CodeViewWidget::SetAddressUpdate::WithUpdate);
+    m_code_widget->SetAddress(addr, CodeViewWidget::SetAddressUpdate::WithDetailedUpdate);
   };
 
   connect(m_watch_widget, &WatchWidget::RequestMemoryBreakpoint, request_memory_breakpoint);
@@ -439,7 +439,7 @@ void MainWindow::CreateComponents()
   connect(m_memory_widget, &MemoryWidget::BreakpointsChanged, m_breakpoint_widget,
           &BreakpointWidget::Update);
   connect(m_memory_widget, &MemoryWidget::ShowCode, m_code_widget, [this](u32 address) {
-    m_code_widget->SetAddress(address, CodeViewWidget::SetAddressUpdate::WithUpdate);
+    m_code_widget->SetAddress(address, CodeViewWidget::SetAddressUpdate::WithDetailedUpdate);
   });
 
   connect(m_breakpoint_widget, &BreakpointWidget::BreakpointsChanged, m_code_widget,
@@ -448,7 +448,7 @@ void MainWindow::CreateComponents()
           &MemoryWidget::Update);
   connect(m_breakpoint_widget, &BreakpointWidget::SelectedBreakpoint, [this](u32 address) {
     if (Core::GetState() == Core::State::Paused)
-      m_code_widget->SetAddress(address, CodeViewWidget::SetAddressUpdate::WithUpdate);
+      m_code_widget->SetAddress(address, CodeViewWidget::SetAddressUpdate::WithDetailedUpdate);
   });
 }
 

commit 433a5ed1840acce95efc6fed483c42291c80a16a

Fix code widget not becoming visible when selecting 'view code' or similar
This affected the memory and registers widgets (and possibly others). I'm pretty sure it regressed in 0ac47b67be935f8da02cc866c470c4b68f1d098d.

The SetCodeVisible line is a new fix, but the equivalent already existed in the memory widget.

iCube/Source/Core/DolphinQt/Debugger/CodeWidget.cpp
@@ -291,8 +291,10 @@ void CodeWidget::SetAddress(u32 address, CodeViewWidget::SetAddressUpdate update
 {
   m_code_view->SetAddress(address, update);
 
-  if (update == CodeViewWidget::SetAddressUpdate::WithUpdate)
+  if (update == CodeViewWidget::SetAddressUpdate::WithUpdate ||
+      update == CodeViewWidget::SetAddressUpdate::WithDetailedUpdate)
   {
+    Settings::Instance().SetCodeVisible(true);
     raise();
     m_code_view->setFocus();
   }