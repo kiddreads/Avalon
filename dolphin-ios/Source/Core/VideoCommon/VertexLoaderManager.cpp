TellowKrinkle/dolphin

VideoCommon: Cull vertices on the CPU

Source/Core/VideoCommon/VertexLoaderManager.cpp
@@ -29,6 +29,7 @@
 #include "VideoCommon/VertexLoaderBase.h"
 #include "VideoCommon/VertexManagerBase.h"
 #include "VideoCommon/VertexShaderManager.h"
+#include "VideoCommon/VideoConfig.h"
 #include "VideoCommon/XFMemory.h"
 
 namespace VertexLoaderManager
@@ -355,16 +356,32 @@ int RunVertices(int vtx_attr_group, OpcodeDecoder::Primitive primitive, int coun
     VertexShaderManager::SetVertexFormat(loader->m_native_components,
                                          loader->m_native_vertex_format->GetVertexDeclaration());
 
-    // if cull mode is CULL_ALL, tell VertexManager to skip triangles and quads.
-    // They still need to go through vertex loading, because we need to calculate a zfreeze refrence
-    // slope.
-    bool cullall = (bpmem.genMode.cullmode == CullMode::All &&
-                    primitive < OpcodeDecoder::Primitive::GX_DRAW_LINES);
+    // CPUCull's performance increase comes from encoding less GPU commands, not sending less data
+    // Therefore it's only useful to check if culling could remove a flush
+    const bool can_cpu_cull = g_ActiveConfig.bCPUCull &&
+                              primitive < OpcodeDecoder::Primitive::GX_DRAW_LINES &&
+                              !g_vertex_manager->HasSendableVertices();
 
-    DataReader dst = g_vertex_manager->PrepareForAdditionalData(
-        primitive, count, loader->m_native_vtx_decl.stride, cullall);
+    // if cull mode is CULL_ALL, tell VertexManager to skip triangles and quads.
+    // They still need to go through vertex loading, because we need to calculate a zfreeze
+    // reference slope.
+    const bool cullall = (bpmem.genMode.cullmode == CullMode::All &&
+                          primitive < OpcodeDecoder::Primitive::GX_DRAW_LINES);
+
+    const int stride = loader->m_native_vtx_decl.stride;
+    DataReader dst = g_vertex_manager->PrepareForAdditionalData(primitive, count, stride,
+                                                                cullall || can_cpu_cull);
 
     count = loader->RunVertices(src, dst, count);
+
+    if (can_cpu_cull && !cullall)
+    {
+      if (!g_vertex_manager->CPUCullVertices(loader, primitive, dst.GetPointer(), count))
+      {
+        DataReader new_dst = g_vertex_manager->DisableCullAll(stride);
+        memmove(new_dst.GetPointer(), dst.GetPointer(), count * stride);
+      }
+    }
 
     g_vertex_manager->AddIndices(primitive, count);
     g_vertex_manager->FlushData(count, loader->m_native_vtx_decl.stride);

Source/Core/VideoCommon/VertexManagerBase.cpp
@@ -103,6 +103,7 @@ VertexManagerBase::~VertexManagerBase() = default;
 bool VertexManagerBase::Initialize()
 {
   m_index_generator.Init();
+  m_cpu_cull.Init();
   return true;
 }
 
@@ -114,6 +115,13 @@ u32 VertexManagerBase::GetRemainingSize() const
 void VertexManagerBase::AddIndices(OpcodeDecoder::Primitive primitive, u32 num_vertices)
 {
   m_index_generator.AddIndices(primitive, num_vertices);
+}
+
+bool VertexManagerBase::CPUCullVertices(VertexLoaderBase* loader,
+                                        OpcodeDecoder::Primitive primitive, const u8* src,
+                                        u32 count)
+{
+  return m_cpu_cull.CullVertices(loader, primitive, src, count);
 }
 
 DataReader VertexManagerBase::PrepareForAdditionalData(OpcodeDecoder::Primitive primitive,
@@ -181,6 +189,16 @@ DataReader VertexManagerBase::PrepareForAdditionalData(OpcodeDecoder::Primitive 
     m_is_flushed = false;
   }
 
+  return DataReader(m_cur_buffer_pointer, m_end_buffer_pointer);
+}
+
+DataReader VertexManagerBase::DisableCullAll(u32 stride)
+{
+  if (m_cull_all)
+  {
+    m_cull_all = false;
+    ResetBuffer(stride);
+  }
   return DataReader(m_cur_buffer_pointer, m_end_buffer_pointer);
 }
 
@@ -536,6 +554,8 @@ void VertexManagerBase::Flush()
     // Now the vertices can be flushed to the GPU. Everything following the CommitBuffer() call
     // must be careful to not upload any utility vertices, as the binding will be lost otherwise.
     const u32 num_indices = m_index_generator.GetIndexLen();
+    if (num_indices == 0)
+      return;
     u32 base_vertex, base_index;
     CommitBuffer(m_index_generator.GetNumVerts(),
                  VertexLoaderManager::GetCurrentVertexFormat()->GetVertexStride(), num_indices,

kiddreads/Avalon

dolphin-ios/Source/Core/VideoCommon/VertexLoaderManager.cpp
@@ -30,6 +30,7 @@
 #include "VideoCommon/VertexLoaderBase.h"
 #include "VideoCommon/VertexManagerBase.h"
 #include "VideoCommon/VertexShaderManager.h"
+#include "VideoCommon/VideoConfig.h"
 #include "VideoCommon/XFMemory.h"
 
 namespace VertexLoaderManager
@@ -366,16 +367,32 @@ int RunVertices(int vtx_attr_group, OpcodeDecoder::Primitive primitive, int coun
     vertex_shader_manager.SetVertexFormat(loader->m_native_components,
                                           loader->m_native_vertex_format->GetVertexDeclaration());
 
-    // if cull mode is CULL_ALL, tell VertexManager to skip triangles and quads.
-    // They still need to go through vertex loading, because we need to calculate a zfreeze refrence
-    // slope.
-    bool cullall = (bpmem.genMode.cullmode == CullMode::All &&
-                    primitive < OpcodeDecoder::Primitive::GX_DRAW_LINES);
+    // CPUCull's performance increase comes from encoding fewer GPU commands, not sending less data
+    // Therefore it's only useful to check if culling could remove a flush
+    const bool can_cpu_cull = g_ActiveConfig.bCPUCull &&
+                              primitive < OpcodeDecoder::Primitive::GX_DRAW_LINES &&
+                              !g_vertex_manager->HasSendableVertices();
 
-    DataReader dst = g_vertex_manager->PrepareForAdditionalData(
-        primitive, count, loader->m_native_vtx_decl.stride, cullall);
+    // if cull mode is CULL_ALL, tell VertexManager to skip triangles and quads.
+    // They still need to go through vertex loading, because we need to calculate a zfreeze
+    // reference slope.
+    const bool cullall = (bpmem.genMode.cullmode == CullMode::All &&
+                          primitive < OpcodeDecoder::Primitive::GX_DRAW_LINES);
+
+    const int stride = loader->m_native_vtx_decl.stride;
+    DataReader dst = g_vertex_manager->PrepareForAdditionalData(primitive, count, stride,
+                                                                cullall || can_cpu_cull);
 
     count = loader->RunVertices(src, dst.GetPointer(), count);
+
+    if (can_cpu_cull && !cullall)
+    {
+      if (!g_vertex_manager->AreAllVerticesCulled(loader, primitive, dst.GetPointer(), count))
+      {
+        DataReader new_dst = g_vertex_manager->DisableCullAll(stride);
+        memmove(new_dst.GetPointer(), dst.GetPointer(), count * stride);
+      }
+    }
 
     g_vertex_manager->AddIndices(primitive, count);
     g_vertex_manager->FlushData(count, loader->m_native_vtx_decl.stride);

dolphin-ios/Source/Core/VideoCommon/VertexManagerBase.cpp
@@ -104,6 +104,7 @@ VertexManagerBase::~VertexManagerBase() = default;
 bool VertexManagerBase::Initialize()
 {
   m_index_generator.Init();
+  m_cpu_cull.Init();
   return true;
 }
 
@@ -115,6 +116,13 @@ u32 VertexManagerBase::GetRemainingSize() const
 void VertexManagerBase::AddIndices(OpcodeDecoder::Primitive primitive, u32 num_vertices)
 {
   m_index_generator.AddIndices(primitive, num_vertices);
+}
+
+bool VertexManagerBase::AreAllVerticesCulled(VertexLoaderBase* loader,
+                                             OpcodeDecoder::Primitive primitive, const u8* src,
+                                             u32 count)
+{
+  return m_cpu_cull.AreAllVerticesCulled(loader, primitive, src, count);
 }
 
 DataReader VertexManagerBase::PrepareForAdditionalData(OpcodeDecoder::Primitive primitive,
@@ -182,6 +190,16 @@ DataReader VertexManagerBase::PrepareForAdditionalData(OpcodeDecoder::Primitive 
     m_is_flushed = false;
   }
 
+  return DataReader(m_cur_buffer_pointer, m_end_buffer_pointer);
+}
+
+DataReader VertexManagerBase::DisableCullAll(u32 stride)
+{
+  if (m_cull_all)
+  {
+    m_cull_all = false;
+    ResetBuffer(stride);
+  }
   return DataReader(m_cur_buffer_pointer, m_end_buffer_pointer);
 }
 
@@ -546,6 +564,8 @@ void VertexManagerBase::Flush()
     // Now the vertices can be flushed to the GPU. Everything following the CommitBuffer() call
     // must be careful to not upload any utility vertices, as the binding will be lost otherwise.
     const u32 num_indices = m_index_generator.GetIndexLen();
+    if (num_indices == 0)
+      return;
     u32 base_vertex, base_index;
     CommitBuffer(m_index_generator.GetNumVerts(),
                  VertexLoaderManager::GetCurrentVertexFormat()->GetVertexStride(), num_indices,