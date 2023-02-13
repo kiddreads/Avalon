kiddreads/Avalon

commit 0b680b07aa9a06f7b9873d8f54a27b354d7e0f8e

DolphinQt: Properly lock CPU before accessing emulated memory
This fixes a problem I was having where using frame advance with the
debugger open would frequently cause panic alerts about invalid addresses
due to the CPU thread changing MSR.DR while the host thread was trying
to access memory.

To aid in tracking down all the places where we weren't properly locking
the CPU, I've created a new type (in Core.h) that you have to pass as a
reference or pointer to functions that require running as the CPU thread.

iCube/Source/Core/Core/HLE/HLE.cpp
@@ -152,12 +152,12 @@ void Reload(Core::System& system)
   PatchFunctions(system);
 }
 
-void Execute(u32 current_pc, u32 hook_index)
+void Execute(const Core::CPUThreadGuard& guard, u32 current_pc, u32 hook_index)
 {
   hook_index &= 0xFFFFF;
   if (hook_index > 0 && hook_index < os_patches.size())
   {
-    os_patches[hook_index].function();
+    os_patches[hook_index].function(guard);
   }
   else
   {

iCube/Source/Core/Core/HLE/HLE.h
@@ -9,12 +9,13 @@
 
 namespace Core
 {
+class CPUThreadGuard;
 class System;
-}
+}  // namespace Core
 
 namespace HLE
 {
-using HookFunction = void (*)();
+using HookFunction = void (*)(const Core::CPUThreadGuard&);
 
 enum class HookType
 {
@@ -46,7 +47,7 @@ void Reload(Core::System& system);
 void Patch(Core::System& system, u32 pc, std::string_view func_name);
 u32 UnPatch(Core::System& system, std::string_view patch_name);
 u32 UnpatchRange(Core::System& system, u32 start_addr, u32 end_addr);
-void Execute(u32 current_pc, u32 hook_index);
+void Execute(const Core::CPUThreadGuard& guard, u32 current_pc, u32 hook_index);
 
 // Returns the HLE hook index of the address
 u32 GetHookByAddress(u32 address);

commit 47f25eea6f097dccc513f6feee48312b9fef35a5

JIT: Fix calls to HLE::Execute.
This got broken in 0b680b07aa9a06f7b9873d8f54a27b354d7e0f8e.

iCube/Source/Core/Core/HLE/HLE.cpp
@@ -12,6 +12,7 @@
 
 #include "Core/Config/MainSettings.h"
 #include "Core/ConfigManager.h"
+#include "Core/Core.h"
 #include "Core/GeckoCode.h"
 #include "Core/HLE/HLE_Misc.h"
 #include "Core/HLE/HLE_OS.h"
@@ -163,6 +164,13 @@ void Execute(const Core::CPUThreadGuard& guard, u32 current_pc, u32 hook_index)
   {
     PanicAlertFmt("HLE system tried to call an undefined HLE function {}.", hook_index);
   }
+}
+
+void ExecuteFromJIT(u32 current_pc, u32 hook_index)
+{
+  ASSERT(Core::IsCPUThread());
+  Core::CPUThreadGuard guard;
+  Execute(guard, current_pc, hook_index);
 }
 
 u32 GetHookByAddress(u32 address)

iCube/Source/Core/Core/HLE/HLE.h
@@ -48,6 +48,7 @@ void Patch(Core::System& system, u32 pc, std::string_view func_name);
 u32 UnPatch(Core::System& system, std::string_view patch_name);
 u32 UnpatchRange(Core::System& system, u32 start_addr, u32 end_addr);
 void Execute(const Core::CPUThreadGuard& guard, u32 current_pc, u32 hook_index);
+void ExecuteFromJIT(u32 current_pc, u32 hook_index);
 
 // Returns the HLE hook index of the address
 u32 GetHookByAddress(u32 address);

iCube/Source/Core/Core/PowerPC/Jit64/Jit.cpp
@@ -483,7 +483,7 @@ void Jit64::HLEFunction(u32 hook_index)
   gpr.Flush();
   fpr.Flush();
   ABI_PushRegistersAndAdjustStack({}, 0);
-  ABI_CallFunctionCC(HLE::Execute, js.compilerPC, hook_index);
+  ABI_CallFunctionCC(HLE::ExecuteFromJIT, js.compilerPC, hook_index);
   ABI_PopRegistersAndAdjustStack({}, 0);
 }
 

iCube/Source/Core/Core/PowerPC/JitArm64/Jit.cpp
@@ -254,7 +254,7 @@ void JitArm64::HLEFunction(u32 hook_index)
   gpr.Flush(FlushMode::All, ARM64Reg::INVALID_REG);
   fpr.Flush(FlushMode::All, ARM64Reg::INVALID_REG);
 
-  MOVP2R(ARM64Reg::X8, &HLE::Execute);
+  MOVP2R(ARM64Reg::X8, &HLE::ExecuteFromJIT);
   MOVI2R(ARM64Reg::W0, js.compilerPC);
   MOVI2R(ARM64Reg::W1, hook_index);
   BLR(ARM64Reg::X8);