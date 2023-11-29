kiddreads/Avalon

commit 774db018ddfacc6190029dcf928841fad9ba5432

Jit: Some mcrxr optimizations

dolphin-ios/Source/Core/Core/PowerPC/Jit64/Jit_SystemRegisters.cpp
@@ -588,8 +588,8 @@ void Jit64::mcrxr(UGeckoInstruction inst)
   MOV(64, CROffset(inst.CRFD), R(RSCRATCH));
 
   // Clear XER[0-3]
-  MOV(8, PPCSTATE(xer_ca), Imm8(0));
-  MOV(8, PPCSTATE(xer_so_ov), Imm8(0));
+  static_assert(PPCSTATE_OFF(xer_ca) + 1 == PPCSTATE_OFF(xer_so_ov));
+  MOV(16, PPCSTATE(xer_ca), Imm8(0));
 }
 
 void Jit64::crXXX(UGeckoInstruction inst)

dolphin-ios/Source/Core/Core/PowerPC/JitArm64/JitArm64_SystemRegisters.cpp
@@ -143,7 +143,7 @@ void JitArm64::mcrxr(UGeckoInstruction inst)
   LDRB(IndexType::Unsigned, WB, PPC_REG, PPCSTATE_OFF(xer_so_ov));
 
   // [0 SO OV CA]
-  ADD(WA, WA, WB, ArithOption(WB, ShiftType::LSL, 1));
+  BFI(WA, WB, 1, 2);
   // [SO OV CA 0] << 3
   LSL(WA, WA, 4);
 
@@ -151,8 +151,8 @@ void JitArm64::mcrxr(UGeckoInstruction inst)
   LDR(XB, XB, XA);
 
   // Clear XER[0-3]
-  STRB(IndexType::Unsigned, ARM64Reg::WZR, PPC_REG, PPCSTATE_OFF(xer_ca));
-  STRB(IndexType::Unsigned, ARM64Reg::WZR, PPC_REG, PPCSTATE_OFF(xer_so_ov));
+  static_assert(PPCSTATE_OFF(xer_ca) + 1 == PPCSTATE_OFF(xer_so_ov));
+  STRH(IndexType::Unsigned, ARM64Reg::WZR, PPC_REG, PPCSTATE_OFF(xer_ca));
 
   gpr.Unlock(WA);
 }

commit 66768b7a2191c1bedb64b66bd9990b08135c05b8

Jit64: fix invalid instruction encoding
This is a recent regression introduced in
774db018ddfacc6190029dcf928841fad9ba5432.

dolphin-ios/Source/Core/Core/PowerPC/Jit64/Jit_SystemRegisters.cpp
@@ -585,7 +585,7 @@ void Jit64::mcrxr(UGeckoInstruction inst)
 
   // Clear XER[0-3]
   static_assert(PPCSTATE_OFF(xer_ca) + 1 == PPCSTATE_OFF(xer_so_ov));
-  MOV(16, PPCSTATE(xer_ca), Imm8(0));
+  MOV(16, PPCSTATE(xer_ca), Imm16(0));
 }
 
 void Jit64::crXXX(UGeckoInstruction inst)