kiddreads/Avalon

commit 0042ea9533232cc11093092847f8c2b826cc936b

JitArm64: Optimize ANDI2R and friends to no-ops when possible
This optimizes rlwnmx with mask == 0xFFFFFFFF.

dolphin-ios/Source/Core/Common/Arm64Emitter.cpp
@@ -4041,7 +4041,11 @@ void ARM64XEmitter::ANDI2R(ARM64Reg Rd, ARM64Reg Rn, u64 imm, ARM64Reg scratch)
   if (!Is64Bit(Rn))
     imm &= 0xFFFFFFFF;
 
-  if (const auto result = LogicalImm(imm, Is64Bit(Rn) ? GPRSize::B64 : GPRSize::B32))
+  if (imm != (Is64Bit(Rn) ? 0xFFFF'FFFF'FFFF'FFFF : 0xFFFF'FFFF))
+  {
+    // Do nothing
+  }
+  else if (const auto result = LogicalImm(imm, Is64Bit(Rn) ? GPRSize::B64 : GPRSize::B32))
   {
     AND(Rd, Rn, result);
   }
@@ -4057,7 +4061,11 @@ void ARM64XEmitter::ANDI2R(ARM64Reg Rd, ARM64Reg Rn, u64 imm, ARM64Reg scratch)
 
 void ARM64XEmitter::ORRI2R(ARM64Reg Rd, ARM64Reg Rn, u64 imm, ARM64Reg scratch)
 {
-  if (const auto result = LogicalImm(imm, Is64Bit(Rn) ? GPRSize::B64 : GPRSize::B32))
+  if (imm == 0)
+  {
+    // Do nothing
+  }
+  else if (const auto result = LogicalImm(imm, Is64Bit(Rn) ? GPRSize::B64 : GPRSize::B32))
   {
     ORR(Rd, Rn, result);
   }
@@ -4073,7 +4081,11 @@ void ARM64XEmitter::ORRI2R(ARM64Reg Rd, ARM64Reg Rn, u64 imm, ARM64Reg scratch)
 
 void ARM64XEmitter::EORI2R(ARM64Reg Rd, ARM64Reg Rn, u64 imm, ARM64Reg scratch)
 {
-  if (const auto result = LogicalImm(imm, Is64Bit(Rn) ? GPRSize::B64 : GPRSize::B32))
+  if (imm == 0)
+  {
+    // Do nothing
+  }
+  else if (const auto result = LogicalImm(imm, Is64Bit(Rn) ? GPRSize::B64 : GPRSize::B32))
   {
     EOR(Rd, Rn, result);
   }

commit 6bdcfab0894160e4be1fb14ea068722c7c6477f8

JitArm64: Fix the "do nothing" cases of ANDI2R and friends
So somehow I forgot that AArch64 uses three-operand encoding...

Fixes a regression from 0042ea9533 which manifested in various ways,
such as incorrect rendering of the Wind Waker title screen.

dolphin-ios/Source/Core/Common/Arm64Emitter.cpp
@@ -4051,13 +4051,14 @@ void ARM64XEmitter::ANDI2R(ARM64Reg Rd, ARM64Reg Rn, u64 imm, ARM64Reg scratch)
     imm = (imm << 32) | (imm & 0xFFFFFFFF);
   }
 
-  if ((~imm) == 0)
-  {
-    // Do nothing
-  }
-  else if (imm == 0)
+  if (imm == 0)
   {
     MOVZ(Rd, 0);
+  }
+  else if ((~imm) == 0)
+  {
+    if (Rd != Rn)
+      MOV(Rd, Rn);
   }
   else if (const auto result = LogicalImm(imm, GPRSize::B64))
   {
@@ -4090,7 +4091,8 @@ void ARM64XEmitter::ORRI2R(ARM64Reg Rd, ARM64Reg Rn, u64 imm, ARM64Reg scratch)
 
   if (imm == 0)
   {
-    // Do nothing
+    if (Rd != Rn)
+      MOV(Rd, Rn);
   }
   else if ((~imm) == 0)
   {
@@ -4127,7 +4129,8 @@ void ARM64XEmitter::EORI2R(ARM64Reg Rd, ARM64Reg Rn, u64 imm, ARM64Reg scratch)
 
   if (imm == 0)
   {
-    // Do nothing
+    if (Rd != Rn)
+      MOV(Rd, Rn);
   }
   else if ((~imm) == 0)
   {