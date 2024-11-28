2lambda123/dolphin-emu-dolphin

Add AR Code Whitelist Approval

Source/Core/Core/AchievementManager.cpp
@@ -23,6 +23,7 @@
 #include "Common/ScopeGuard.h"
 #include "Common/Version.h"
 #include "Common/WorkQueueThread.h"
+#include "Core/ActionReplay.h"
 #include "Core/Config/AchievementSettings.h"
 #include "Core/Core.h"
 #include "Core/GeckoCode.h"
@@ -432,6 +433,18 @@ Common::SHA1::Digest AchievementManager::GetPatchHash(const Gecko::GeckoCode& co
   return context->Finish();
 }
 
+Common::SHA1::Digest AchievementManager::GetPatchHash(const ActionReplay::ARCode& code) const
+{
+  auto context = Common::SHA1::CreateContext();
+  context->Update(Common::BitCastToArray<u8>(static_cast<u64>(code.ops.size())));
+  for (const auto& entry : code.ops)
+  {
+    context->Update(Common::BitCastToArray<u8>(entry.cmd_addr));
+    context->Update(Common::BitCastToArray<u8>(entry.value));
+  }
+  return context->Finish();
+}
+
 void AchievementManager::FilterApprovedPatches(std::vector<PatchEngine::Patch>& patches,
                                                const std::string& game_ini_id) const
 {
@@ -440,6 +453,12 @@ void AchievementManager::FilterApprovedPatches(std::vector<PatchEngine::Patch>& 
 
 void AchievementManager::FilterApprovedGeckoCodes(std::vector<Gecko::GeckoCode>& codes,
                                                   const std::string& game_ini_id) const
+{
+  FilterApprovedIni(codes, game_ini_id);
+}
+
+void AchievementManager::FilterApprovedARCodes(std::vector<ActionReplay::ARCode>& codes,
+                                               const std::string& game_ini_id) const
 {
   FilterApprovedIni(codes, game_ini_id);
 }

Source/Core/Core/AchievementManager.h
@@ -50,6 +50,11 @@ namespace Gecko
 class GeckoCode;
 }  // namespace Gecko
 
+namespace ActionReplay
+{
+struct ARCode;
+}  // namespace ActionReplay
+
 class AchievementManager
 {
 public:
@@ -132,6 +137,8 @@ public:
                              const std::string& game_ini_id) const;
   void FilterApprovedGeckoCodes(std::vector<Gecko::GeckoCode>& codes,
                                 const std::string& game_ini_id) const;
+  void FilterApprovedARCodes(std::vector<ActionReplay::ARCode>& codes,
+                             const std::string& game_ini_id) const;
 
   void SetSpectatorMode();
   std::string_view GetPlayerDisplayName() const;
@@ -191,6 +198,7 @@ private:
   void FilterApprovedIni(std::vector<T>& patches, const std::string& game_ini_id) const;
   Common::SHA1::Digest GetPatchHash(const PatchEngine::Patch& patch) const;
   Common::SHA1::Digest GetPatchHash(const Gecko::GeckoCode& patch) const;
+  Common::SHA1::Digest GetPatchHash(const ActionReplay::ARCode& patch) const;
 
   static void LeaderboardEntriesCallback(int result, const char* error_message,
                                          rc_client_leaderboard_entry_list_t* list,

kiddreads/Avalon

iCube/Source/Core/Core/AchievementManager.cpp
@@ -24,6 +24,7 @@
 #include "Common/ScopeGuard.h"
 #include "Common/Version.h"
 #include "Common/WorkQueueThread.h"
+#include "Core/ActionReplay.h"
 #include "Core/Config/AchievementSettings.h"
 #include "Core/Config/FreeLookSettings.h"
 #include "Core/Config/MainSettings.h"
@@ -474,6 +475,18 @@ Common::SHA1::Digest AchievementManager::GetCodeHash(const Gecko::GeckoCode& cod
   return context->Finish();
 }
 
+Common::SHA1::Digest AchievementManager::GetCodeHash(const ActionReplay::ARCode& code) const
+{
+  auto context = Common::SHA1::CreateContext();
+  context->Update(Common::BitCastToArray<u8>(static_cast<u64>(code.ops.size())));
+  for (const auto& entry : code.ops)
+  {
+    context->Update(Common::BitCastToArray<u8>(entry.cmd_addr));
+    context->Update(Common::BitCastToArray<u8>(entry.value));
+  }
+  return context->Finish();
+}
+
 void AchievementManager::FilterApprovedPatches(std::vector<PatchEngine::Patch>& patches,
                                                const std::string& game_ini_id) const
 {
@@ -486,8 +499,20 @@ void AchievementManager::FilterApprovedGeckoCodes(std::vector<Gecko::GeckoCode>&
   FilterApprovedIni(codes, game_ini_id);
 }
 
+void AchievementManager::FilterApprovedARCodes(std::vector<ActionReplay::ARCode>& codes,
+                                               const std::string& game_ini_id) const
+{
+  FilterApprovedIni(codes, game_ini_id);
+}
+
 bool AchievementManager::CheckApprovedGeckoCode(const Gecko::GeckoCode& code,
                                                 const std::string& game_ini_id) const
+{
+  return CheckApprovedCode(code, game_ini_id);
+}
+
+bool AchievementManager::CheckApprovedARCode(const ActionReplay::ARCode& code,
+                                             const std::string& game_ini_id) const
 {
   return CheckApprovedCode(code, game_ini_id);
 }

iCube/Source/Core/Core/AchievementManager.h
@@ -50,6 +50,11 @@ namespace Gecko
 class GeckoCode;
 }  // namespace Gecko
 
+namespace ActionReplay
+{
+struct ARCode;
+}  // namespace ActionReplay
+
 class AchievementManager
 {
 public:
@@ -135,7 +140,10 @@ public:
                              const std::string& game_ini_id) const;
   void FilterApprovedGeckoCodes(std::vector<Gecko::GeckoCode>& codes,
                                 const std::string& game_ini_id) const;
+  void FilterApprovedARCodes(std::vector<ActionReplay::ARCode>& codes,
+                             const std::string& game_ini_id) const;
   bool CheckApprovedGeckoCode(const Gecko::GeckoCode& code, const std::string& game_ini_id) const;
+  bool CheckApprovedARCode(const ActionReplay::ARCode& code, const std::string& game_ini_id) const;
 
   void SetSpectatorMode();
   std::string_view GetPlayerDisplayName() const;
@@ -197,6 +205,7 @@ private:
   bool CheckApprovedCode(const T& code, const std::string& game_ini_id) const;
   Common::SHA1::Digest GetCodeHash(const PatchEngine::Patch& patch) const;
   Common::SHA1::Digest GetCodeHash(const Gecko::GeckoCode& code) const;
+  Common::SHA1::Digest GetCodeHash(const ActionReplay::ARCode& code) const;
 
   static void LeaderboardEntriesCallback(int result, const char* error_message,
                                          rc_client_leaderboard_entry_list_t* list,
@@ -284,6 +293,12 @@ public:
 
   constexpr bool CheckApprovedGeckoCode(const Gecko::GeckoCode& code,
                                         const std::string& game_ini_id)
+  {
+    return true;
+  };
+
+  constexpr bool CheckApprovedARCode(const ActionReplay::ARCode& code,
+                                     const std::string& game_ini_id)
   {
     return true;
   };