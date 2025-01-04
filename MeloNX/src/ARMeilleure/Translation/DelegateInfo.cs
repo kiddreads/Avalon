natesworksdev/Ryujinx

use UnmanagedCallersOnly for delegates

src/ARMeilleure/Translation/DelegateInfo.cs
@@ -4,15 +4,10 @@ namespace ARMeilleure.Translation
 {
     class DelegateInfo
     {
-#pragma warning disable IDE0052 // Remove unread private member
-        private readonly Delegate _dlg; // Ensure that this delegate will not be garbage collected.
-#pragma warning restore IDE0052
-
         public IntPtr FuncPtr { get; }
 
-        public DelegateInfo(Delegate dlg, IntPtr funcPtr)
+        public DelegateInfo(IntPtr funcPtr)
         {
-            _dlg = dlg;
             FuncPtr = funcPtr;
         }
     }

src/ARMeilleure/Translation/EmitterContext.cs
@@ -97,7 +97,7 @@ namespace ARMeilleure.Translation
 
         public virtual Operand Call(MethodInfo info, params Operand[] callArgs)
         {
-            IntPtr funcPtr = Delegates.GetDelegateFuncPtr(info);
+            IntPtr funcPtr = info.MethodHandle.GetFunctionPointer();
 
             OperandType returnType = GetOperandType(info.ReturnType);
 

kiddreads/Avalon

MeloNX/src/ARMeilleure/Translation/DelegateInfo.cs
@@ -4,15 +4,9 @@ namespace ARMeilleure.Translation
 {
     class DelegateInfo
     {
-#pragma warning disable IDE0052 // Remove unread private member
-        private readonly Delegate _dlg; // Ensure that this delegate will not be garbage collected.
-#pragma warning restore IDE0052
-
-        public nint FuncPtr { get; }
-
-        public DelegateInfo(Delegate dlg, nint funcPtr)
+        public nint FuncPtr { get; private set; }
+        public DelegateInfo(nint funcPtr)
         {
-            _dlg = dlg;
             FuncPtr = funcPtr;
         }
     }

MeloNX/src/ARMeilleure/Translation/EmitterContext.cs
@@ -97,7 +97,7 @@ namespace ARMeilleure.Translation
 
         public virtual Operand Call(MethodInfo info, params Operand[] callArgs)
         {
-            nint funcPtr = Delegates.GetDelegateFuncPtr(info);
+            nint funcPtr = info.MethodHandle.GetFunctionPointer();
 
             OperandType returnType = GetOperandType(info.ReturnType);
 