lckybstrd/dolphin-emu2

Implement Triforce ID parsing

Source/Core/DiscIO/Volume.h
@@ -105,6 +105,7 @@ public:
   }
   virtual std::string GetGameID(const Partition& partition = PARTITION_NONE) const = 0;
   virtual std::string GetGameTDBID(const Partition& partition = PARTITION_NONE) const = 0;
+  virtual std::string GetTriID() const { return ""; }
   virtual std::string GetMakerID(const Partition& partition = PARTITION_NONE) const = 0;
   virtual std::optional<u16> GetRevision(const Partition& partition = PARTITION_NONE) const = 0;
   virtual std::string GetInternalName(const Partition& partition = PARTITION_NONE) const = 0;

Source/Core/DiscIO/VolumeGC.cpp
@@ -48,11 +48,14 @@ VolumeGC::VolumeGC(std::unique_ptr<BlobReader> reader)
     std::unique_ptr<FileInfo> file_info = tmp_fs->FindFileInfo("boot.id");
     if (!file_info)
       return;
-    u32 triforce_magic;  // "BTID"
+    BootID triforce_header;
     const u64 file_size = ReadFile(*this, PARTITION_NONE, file_info.get(),
-                                   reinterpret_cast<u8*>(&triforce_magic), sizeof(triforce_magic));
-    if (file_size >= 4 && triforce_magic == BTID_MAGIC)
+                                   reinterpret_cast<u8*>(&triforce_header), sizeof(BootID));
+    if (file_size >= 4 && triforce_header.magic == BTID_MAGIC)
+    {
       m_is_triforce = true;
+      m_tri_id = triforce_header.id;
+    }
   }
 }
 
@@ -90,6 +93,18 @@ std::string VolumeGC::GetGameTDBID(const Partition& partition) const
 
   // Normal case. Just return the usual game ID.
   return GetGameID(partition);
+}
+
+std::string VolumeGC::GetTriID() const
+{
+  if (m_is_triforce)
+  {
+    return (std::string(m_tri_id.data(), m_tri_id.size()));
+  }
+  else
+  {
+    return "";
+  }
 }
 
 Region VolumeGC::GetRegion() const

Source/Core/DiscIO/VolumeGC.h
@@ -34,6 +34,7 @@ public:
             const Partition& partition = PARTITION_NONE) const override;
   const FileSystem* GetFileSystem(const Partition& partition = PARTITION_NONE) const override;
   std::string GetGameTDBID(const Partition& partition = PARTITION_NONE) const override;
+  std::string GetTriID() const override;
   std::map<Language, std::string> GetShortNames() const override;
   std::map<Language, std::string> GetLongNames() const override;
   std::map<Language, std::string> GetShortMakers() const override;
@@ -76,6 +77,13 @@ private:
                                                     // (only one for BNR1 type)
   };
 
+  struct BootID
+  {
+    u32 magic;  // "BTID"
+    u32 padding[11];
+    std::array<char, 4> id;
+  };
+
   struct ConvertedGCBanner
   {
     ConvertedGCBanner();
@@ -105,6 +113,7 @@ private:
   std::unique_ptr<BlobReader> m_reader;
 
   bool m_is_triforce;
+  std::array<char, 4> m_tri_id;
 };
 
 }  // namespace DiscIO

Source/Core/UICommon/GameFile.h
@@ -76,6 +76,7 @@ public:
   const std::string& GetInternalName() const { return m_internal_name; }
   const std::string& GetGameID() const { return m_game_id; }
   const std::string& GetGameTDBID() const { return m_gametdb_id; }
+  std::string GetTriID() const { return m_tri_id; }
   u64 GetTitleID() const { return m_title_id; }
   const std::string& GetMakerID() const { return m_maker_id; }
   u16 GetRevision() const { return m_revision; }
@@ -157,6 +158,7 @@ private:
   std::string m_internal_name;
   std::string m_game_id;
   std::string m_gametdb_id;
+  std::string m_tri_id;
   u64 m_title_id{};
   std::string m_maker_id;
 

kiddreads/Avalon

iCube/Source/Core/DiscIO/Volume.h
@@ -96,6 +96,7 @@ public:
   }
   virtual std::string GetGameID(const Partition& partition = PARTITION_NONE) const = 0;
   virtual std::string GetGameTDBID(const Partition& partition = PARTITION_NONE) const = 0;
+  virtual std::string GetTriforceID() const { return ""; }
   virtual std::string GetMakerID(const Partition& partition = PARTITION_NONE) const = 0;
   virtual std::optional<u16> GetRevision(const Partition& partition = PARTITION_NONE) const = 0;
   virtual std::string GetInternalName(const Partition& partition = PARTITION_NONE) const = 0;

iCube/Source/Core/DiscIO/VolumeGC.cpp
@@ -48,11 +48,14 @@ VolumeGC::VolumeGC(std::unique_ptr<BlobReader> reader)
     std::unique_ptr<FileInfo> file_info = tmp_fs->FindFileInfo("boot.id");
     if (!file_info)
       return;
-    u32 triforce_magic;  // "BTID"
+    BootID triforce_header;
     const u64 file_size = ReadFile(*this, PARTITION_NONE, file_info.get(),
-                                   reinterpret_cast<u8*>(&triforce_magic), sizeof(triforce_magic));
-    if (file_size >= 4 && triforce_magic == BTID_MAGIC)
+                                   reinterpret_cast<u8*>(&triforce_header), sizeof(BootID));
+    if (file_size >= 4 && triforce_header.magic == BTID_MAGIC)
+    {
       m_is_triforce = true;
+      m_triforce_id = triforce_header.id;
+    }
   }
 }
 
@@ -88,6 +91,14 @@ std::string VolumeGC::GetGameTDBID(const Partition& partition) const
 
   // Normal case. Just return the usual game ID.
   return GetGameID(partition);
+}
+
+std::string VolumeGC::GetTriforceID() const
+{
+  if (m_is_triforce)
+    return (std::string(m_triforce_id.data(), m_triforce_id.size()));
+  else
+    return "";
 }
 
 Region VolumeGC::GetRegion() const

iCube/Source/Core/DiscIO/VolumeGC.h
@@ -34,6 +34,7 @@ public:
             const Partition& partition = PARTITION_NONE) const override;
   const FileSystem* GetFileSystem(const Partition& partition = PARTITION_NONE) const override;
   std::string GetGameTDBID(const Partition& partition = PARTITION_NONE) const override;
+  std::string GetTriforceID() const override;
   std::map<Language, std::string> GetShortNames() const override;
   std::map<Language, std::string> GetLongNames() const override;
   std::map<Language, std::string> GetShortMakers() const override;
@@ -76,6 +77,13 @@ private:
                                                     // (only one for BNR1 type)
   };
 
+  struct BootID
+  {
+    u32 magic;  // "BTID"
+    u32 padding[11];
+    std::array<char, 4> id;
+  };
+
   struct ConvertedGCBanner
   {
     ConvertedGCBanner();
@@ -105,6 +113,7 @@ private:
   std::unique_ptr<BlobReader> m_reader;
 
   bool m_is_triforce;
+  std::array<char, 4> m_triforce_id;
 };
 
 }  // namespace DiscIO

iCube/Source/Core/UICommon/GameFile.h
@@ -76,6 +76,7 @@ public:
   const std::string& GetInternalName() const { return m_internal_name; }
   const std::string& GetGameID() const { return m_game_id; }
   const std::string& GetGameTDBID() const { return m_gametdb_id; }
+  std::string GetTriforceID() const { return m_triforce_id; }
   u64 GetTitleID() const { return m_title_id; }
   const std::string& GetMakerID() const { return m_maker_id; }
   u16 GetRevision() const { return m_revision; }
@@ -159,6 +160,7 @@ private:
   std::string m_internal_name;
   std::string m_game_id;
   std::string m_gametdb_id;
+  std::string m_triforce_id;
   u64 m_title_id{};
   std::string m_maker_id;
 