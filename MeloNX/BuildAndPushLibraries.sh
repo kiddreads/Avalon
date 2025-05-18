4949fae88b40828c92a576e37df6d93be2056618
GreemDev <greemdev@ryujinx.app>
2025-05-18 03:10:38 -0500

    infra: Generate .nupkg when building ARMeilleure, Ryujinx.Common, and Ryujinx.Memory. Additionally added a script to push them to Ryubing GitLab package registry. This script is my use only since it assumes you have the necessary authentication locally & on the server.
    TODO: figure out a way to get proper versioning for them.

---
+++ MeloNX/BuildAndPushLibraries.sh
@@ -0,0 +1,19 @@
+function pub {
+  dotnet publish -c release
+}
+
+function package {
+  cd src/$1
+  pub
+  mv bin/Release/$1.1.0.0.nupkg ../../pkgs/$1.1.0.0.nupkg
+  cd ../../
+}
+
+rm -rf pkgs
+mkdir pkgs
+
+package ARMeilleure
+package Ryujinx.Common
+package Ryujinx.Memory
+
+dotnet nuget push pkgs/*.nupkg --source RyubingPkgs
