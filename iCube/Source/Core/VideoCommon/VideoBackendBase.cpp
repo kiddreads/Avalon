e79f79507e68adf80965222a03da6b34d4ef764c
brand175 <20964160+brand175@users.noreply.github.com>
2025-06-10 22:57:01 -0700

    Revert "VideoCommon: initialize and shutdown the CustomResourceManager when the video thread initializes and shuts down"
    This reverts commit ae70d38220ff878cee85ef9308d77b4015b1d87c.

--- iCube/Source/Core/VideoCommon/VideoBackendBase.cpp
+++ iCube/Source/Core/VideoCommon/VideoBackendBase.cpp
@@ -41,7 +41,6 @@
 #endif
 
 #include "VideoCommon/AbstractGfx.h"
-#include "VideoCommon/Assets/CustomResourceManager.h"
 #include "VideoCommon/AsyncRequests.h"
 #include "VideoCommon/BPStructs.h"
 #include "VideoCommon/BoundingBox.h"
@@ -342,16 +341,12 @@ bool VideoBackendBase::InitializeShared(std::unique_ptr<AbstractGfx> gfx,
   }
 
   g_shader_cache->InitializeShaderCache();
-  system.GetCustomResourceManager().Initialize();
 
   return true;
 }
 
 void VideoBackendBase::ShutdownShared()
 {
-  auto& system = Core::System::GetInstance();
-  system.GetCustomResourceManager().Shutdown();
-
   g_frame_dumper.reset();
   g_presenter.reset();
 
@@ -374,6 +369,7 @@ void VideoBackendBase::ShutdownShared()
 
   m_initialized = false;
 
+  auto& system = Core::System::GetInstance();
   VertexLoaderManager::Clear();
   system.GetFifo().Shutdown();
 }
