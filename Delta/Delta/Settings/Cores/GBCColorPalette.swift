5ba828157780159e9be2c6037e510c40ead4b20a
Caroline Moore <caroline@altstore.io>
2025-08-25 12:49:47 -0500

    Adds color previews to GBC palette picker
    Also adds palette descriptions and cleans up view

--- Delta/Delta/Settings/Cores/GBCColorPalette.swift
+++ Delta/Delta/Settings/Cores/GBCColorPalette.swift
@@ -95,18 +95,92 @@ extension GBCColorPalette
     {
         switch self
         {
-        case .brown: return String(localized: "Brown (Up)")
-        case .red: return String(localized: "Red (Up+A)")
-        case .darkBrown: return String(localized: "Dark Brown (Up+B)")
-        case .green: return String(localized: "Green (Right)")
-        case .darkGreen: return String(localized: "Dark Green (Right+A)")
-        case .reverse: return String(localized: "Reverse (Right+B)")
-        case .paleYellow: return String(localized: "Pale Yellow (Down)")
-        case .orange: return String(localized: "Orange (Down+A)")
-        case .yellow: return String(localized: "Yellow (Down+B)")
-        case .blue: return String(localized: "Blue (Left)")
-        case .darkBlue: return String(localized: "Dark Blue (Left+A)")
-        case .gray: return String(localized: "Gray (Left+B)")
+        case .brown: return String(localized: "Brown")
+        case .red: return String(localized: "Red")
+        case .darkBrown: return String(localized: "Dark Brown")
+        case .green: return String(localized: "Green")
+        case .darkGreen: return String(localized: "Dark Green")
+        case .reverse: return String(localized: "Reverse")
+        case .paleYellow: return String(localized: "Pale Yellow")
+        case .orange: return String(localized: "Orange")
+        case .yellow: return String(localized: "Yellow")
+        case .blue: return String(localized: "Blue")
+        case .darkBlue: return String(localized: "Dark Blue")
+        case .gray: return String(localized: "Gray")
+        }
+    }
+    
+    var localizedDescription: String
+    {
+        switch self
+        {
+        case .brown: return String(localized: "Up")
+        case .red: return String(localized: "Up+A")
+        case .darkBrown: return String(localized: "Up+B")
+        case .green: return String(localized: "Right")
+        case .darkGreen: return String(localized: "Right+A")
+        case .reverse: return String(localized: "Right+B")
+        case .paleYellow: return String(localized: "Down")
+        case .orange: return String(localized: "Down+A")
+        case .yellow: return String(localized: "Down+B")
+        case .blue: return String(localized: "Left")
+        case .darkBlue: return String(localized: "Left+A")
+        case .gray: return String(localized: "Left+B")
+        }
+    }
+    
+    var previewColors: (UIColor, UIColor)
+    {
+        switch self
+        {
+        case .brown:
+            let color0 = UIColor(hexString: "FFAD63")! // Tan
+            let color1 = UIColor(hexString: "843100")! // Chocolate
+            return (color0, color1)
+        case .red:
+            let color0 = UIColor(hexString: "FF8484")! // Pink
+            let color1 = UIColor(hexString: "943A3A")! // Maroon
+            return (color0, color1)
+        case .darkBrown:
+            let color0 = UIColor(hexString: "A58452")! // Camel
+            let color1 = UIColor(hexString: "6B5231")! // Coffee
+            return (color0, color1)
+        case .green:
+            let color0 = UIColor(hexString: "52FF00")! // Lime
+            let color1 = UIColor(hexString: "FF4200")! // Orange
+            return (color0, color1)
+        case .darkGreen:
+            let color0 = UIColor(hexString: "7BFF31")! // Lime
+            let color1 = UIColor(hexString: "0063C5")! // Blue
+            return (color0, color1)
+        case .reverse:
+            let color0 = UIColor(hexString: "000000")! // Black
+            let color1 = UIColor(hexString: "FFDE00")! // Yellow
+            return (color0, color1)
+        case .paleYellow:
+            let color0 = UIColor(hexString: "FFFFA5")! // Cream
+            let color1 = UIColor(hexString: "FF9494")! // Pink
+            return (color0, color1)
+        case .orange:
+            let color0 = UIColor(hexString: "FFFF00")! // Yellow
+            let color1 = UIColor(hexString: "FF0000")! // Red
+            return (color0, color1)
+        case .yellow:
+            let color0 = UIColor(hexString: "FFFF00")! // Yellow
+            let color1 = UIColor(hexString: "3A2900")! // Brown
+            return (color0, color1)
+        case .blue:
+            let color0 = UIColor(hexString: "63A5FF")! // Sky
+            let color1 = UIColor(hexString: "0000FF")! // Blue
+            return (color0, color1)
+        case .darkBlue:
+            let color0 = UIColor(hexString: "8C8CDE")! // Lavender
+            let color1 = UIColor(hexString: "52528C")! // Indigo
+            return (color0, color1)
+        case .gray:
+            let color0 = UIColor(hexString: "949494")! // Gray
+            let color1 = UIColor(hexString: "000000")! // Black
+            return (color0, color1)
         }
     }
 }
