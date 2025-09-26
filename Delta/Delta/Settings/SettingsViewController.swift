kiddreads/Avalon

commit ceccfd4cdd4c7b2e062e86668791d8e789ff3f43

Adds setting to open games in new window

Delta/Delta/Settings/SettingsViewController.swift
@@ -67,6 +67,12 @@ private extension SettingsViewController
         case experimentalFeatures
     }
     
+    enum MultitaskingRow: Int, CaseIterable
+    {
+        case pauseWhileInactive
+        case opensGamesInNewWindow
+    }
+    
     enum PatreonRow: Int, CaseIterable
     {
         case joinPatreon
@@ -101,6 +107,7 @@ class SettingsViewController: UITableViewController
     @IBOutlet private var thumbstickHapticFeedbackEnabledSwitch: UISwitch!
     @IBOutlet private var previewsEnabledSwitch: UISwitch!
     @IBOutlet private var quickGesturesSwitch: UISwitch!
+    @IBOutlet private var opensGamesInNewWindowSwitch: UISwitch!
     
     @IBOutlet private var versionLabel: UILabel!
     
@@ -250,6 +257,8 @@ private extension SettingsViewController
         self.supportExternalDisplaysSwitch.isOn = Settings.supportsExternalDisplays
         self.topScreenOnlySwitch.isOn = Settings.features.dsAirPlay.topScreenOnly
         self.layoutScreensHorizontallySwitch.isOn = (Settings.features.dsAirPlay.layoutAxis == .horizontal)
+        
+        self.opensGamesInNewWindowSwitch.isOn = Settings.opensGamesInNewWindow
         
         self.syncingServiceLabel.text = Settings.syncingService?.localizedName
         
@@ -427,6 +436,11 @@ private extension SettingsViewController
         Settings.isQuickGesturesEnabled = sender.isOn
     }
     
+    @IBAction func toggleOpensGamesInNewWindow(_ sender: UISwitch)
+    {
+        Settings.opensGamesInNewWindow = sender.isOn
+    }
+    
     func openTwitter(username: String)
     {
         let twitterAppURL = URL(string: "twitter://user?screen_name=" + username)!
@@ -591,7 +605,7 @@ private extension SettingsViewController
                 self.tableView.selectRow(at: selectedIndexPath, animated: true, scrollPosition: .none)
             }
             
-        case .localControllerPlayerIndex, .preferredControllerSkin, .translucentControllerSkinOpacity, .respectSilentMode, .isButtonHapticFeedbackEnabled, .isThumbstickHapticFeedbackEnabled, .isAltJITEnabled: break
+        case .localControllerPlayerIndex, .preferredControllerSkin, .translucentControllerSkinOpacity, .respectSilentMode, .isButtonHapticFeedbackEnabled, .isThumbstickHapticFeedbackEnabled, .isAltJITEnabled, .opensGamesInNewWindow: break
         default: break
         }
     }
@@ -645,6 +659,8 @@ extension SettingsViewController
             }
             
             return numberOfRows
+            
+        case .multitasking: return MultitaskingRow.allCases.count
             
         case .syncing where !isSectionHidden(section): return SyncManager.shared.coordinator?.account == nil ? 1 : super.tableView(tableView, numberOfRowsInSection: sectionIndex)
         case .advanced where !isSectionHidden(section):

Delta/Delta/Base.lproj/Settings.storyboard
@@ -358,8 +358,38 @@
                             </tableViewSection>
                             <tableViewSection headerTitle="Multitasking" footerTitle="Automatically pause games when they are not the active window." id="vbO-8P-7Mc">
                                 <cells>
-                                    <tableViewCell clipsSubviews="YES" contentMode="scaleToFill" selectionStyle="none" indentationWidth="10" reuseIdentifier="SwitchCell" id="ei2-rB-x25" customClass="SwitchTableViewCell">
+                                    <tableViewCell clipsSubviews="YES" contentMode="scaleToFill" selectionStyle="none" indentationWidth="10" reuseIdentifier="SwitchCell" id="Spz-wL-TZ6" customClass="SwitchTableViewCell">
                                         <rect key="frame" x="16" y="1152" width="343" height="44"/>
+                                        <autoresizingMask key="autoresizingMask"/>
+                                        <tableViewCellContentView key="contentView" opaque="NO" clipsSubviews="YES" multipleTouchEnabled="YES" contentMode="center" tableViewCell="Spz-wL-TZ6" id="HTu-0K-Ot8">
+                                            <rect key="frame" x="0.0" y="0.0" width="343" height="44"/>
+                                            <autoresizingMask key="autoresizingMask"/>
+                                            <subviews>
+                                                <label opaque="NO" userInteractionEnabled="NO" contentMode="left" horizontalHuggingPriority="251" verticalHuggingPriority="251" text="Open Games in New Window" textAlignment="natural" lineBreakMode="tailTruncation" baselineAdjustment="alignBaselines" adjustsFontSizeToFit="NO" translatesAutoresizingMaskIntoConstraints="NO" id="yQG-GF-gkg">
+                                                    <rect key="frame" x="16" y="12" width="254" height="20.5"/>
+                                                    <fontDescription key="fontDescription" type="system" pointSize="17"/>
+                                                    <nil key="textColor"/>
+                                                    <nil key="highlightedColor"/>
+                                                </label>
+                                                <switch opaque="NO" contentMode="scaleToFill" horizontalHuggingPriority="750" verticalHuggingPriority="750" contentHorizontalAlignment="center" contentVerticalAlignment="center" on="YES" translatesAutoresizingMaskIntoConstraints="NO" id="lLe-6t-hH8">
+                                                    <rect key="frame" x="278" y="6.5" width="51" height="31"/>
+                                                    <color key="onTintColor" name="Purple"/>
+                                                    <connections>
+                                                        <action selector="toggleOpensGamesInNewWindow:" destination="eHi-aO-uGS" eventType="primaryActionTriggered" id="Nls-TZ-zv4"/>
+                                                    </connections>
+                                                </switch>
+                                            </subviews>
+                                            <constraints>
+                                                <constraint firstAttribute="trailingMargin" secondItem="lLe-6t-hH8" secondAttribute="trailing" id="602-DV-2o2"/>
+                                                <constraint firstItem="lLe-6t-hH8" firstAttribute="centerY" secondItem="HTu-0K-Ot8" secondAttribute="centerY" id="6hm-gH-Xxh"/>
+                                                <constraint firstItem="yQG-GF-gkg" firstAttribute="centerY" secondItem="HTu-0K-Ot8" secondAttribute="centerY" id="EnE-6U-Diq"/>
+                                                <constraint firstItem="lLe-6t-hH8" firstAttribute="leading" secondItem="yQG-GF-gkg" secondAttribute="trailing" constant="8" symbolic="YES" id="Iiu-YJ-vBo"/>
+                                                <constraint firstItem="yQG-GF-gkg" firstAttribute="leading" secondItem="HTu-0K-Ot8" secondAttribute="leadingMargin" id="RgE-fv-z9C"/>
+                                            </constraints>
+                                        </tableViewCellContentView>
+                                    </tableViewCell>
+                                    <tableViewCell clipsSubviews="YES" contentMode="scaleToFill" selectionStyle="none" indentationWidth="10" reuseIdentifier="SwitchCell" id="ei2-rB-x25" customClass="SwitchTableViewCell">
+                                        <rect key="frame" x="16" y="1196" width="343" height="44"/>
                                         <autoresizingMask key="autoresizingMask"/>
                                         <tableViewCellContentView key="contentView" opaque="NO" clipsSubviews="YES" multipleTouchEnabled="YES" contentMode="center" tableViewCell="ei2-rB-x25" id="I6x-Y3-r6U">
                                             <rect key="frame" x="0.0" y="0.0" width="343" height="44"/>
@@ -913,6 +943,7 @@ Menu + Long-Press: Quick Load</string>
                         <outlet property="controllerOpacitySlider" destination="whi-If-wFf" id="6Cx-HY-xLG"/>
                         <outlet property="exportLogActivityIndicatorView" destination="x5w-dE-ucW" id="u5a-K3-2ag"/>
                         <outlet property="layoutScreensHorizontallySwitch" destination="Wd1-1b-Ee4" id="8LS-Ix-lQz"/>
+                        <outlet property="opensGamesInNewWindowSwitch" destination="lLe-6t-hH8" id="UiO-r7-5IU"/>
                         <outlet property="pauseWhileInactiveSwitch" destination="eeQ-UP-b2W" id="Gde-DL-tO2"/>
                         <outlet property="previewsEnabledSwitch" destination="OJE-9e-9i3" id="Ndg-eN-PPs"/>
                         <outlet property="quickGesturesSwitch" destination="6LN-kh-Afv" id="1Vu-6Q-793"/>

Delta/Delta/Settings/Settings.swift
@@ -36,6 +36,7 @@ extension Settings.Name
     static let isQuickGesturesEnabled: Settings.Name = "isQuickGesturesEnabled"
     static let preferredWFCServer: Settings.Name = "preferredWFCServer"
     static let customWFCServer: Settings.Name = "customWFCServer"
+    static let opensGamesInNewWindow: Settings.Name = "opensGamesInNewWindow"
 }
 
 extension Settings
@@ -69,6 +70,7 @@ struct Settings
                         #keyPath(UserDefaults.pauseWhileInactive): true,
                         #keyPath(UserDefaults.supportsExternalDisplays): true,
                         #keyPath(UserDefaults.isQuickGesturesEnabled): true,
+                        #keyPath(UserDefaults.opensGamesInNewWindow): true,
                         Settings.preferredCoreSettingsKey(for: .ds): MelonDS.core.identifier] as [String : Any]
         
         #if BETA
@@ -274,6 +276,17 @@ extension Settings
         set {
             UserDefaults.standard.isQuickGesturesEnabled = newValue
             NotificationCenter.default.post(name: Settings.didChangeNotification, object: nil, userInfo: [NotificationUserInfoKey.name: Name.isQuickGesturesEnabled])
+        }
+    }
+    
+    static var opensGamesInNewWindow: Bool {
+        get {
+            let opensGamesInNewWindow = UserDefaults.standard.opensGamesInNewWindow
+            return opensGamesInNewWindow
+        }
+        set {
+            UserDefaults.standard.opensGamesInNewWindow = newValue
+            NotificationCenter.default.post(name: Settings.didChangeNotification, object: nil, userInfo: [NotificationUserInfoKey.name: Name.opensGamesInNewWindow])
         }
     }
     
@@ -586,4 +599,6 @@ private extension UserDefaults
     
     @NSManaged var preferredWFCServer: String?
     @NSManaged var customWFCServer: String?
+    
+    @NSManaged var opensGamesInNewWindow: Bool
 }

commit c279d64171dd6ee6cda8392b1c607021a36b810d

Fixes multitasking section appearing in Settings when it should be hidden
(Caused in ceccfd4c)

Delta/Delta/Settings/SettingsViewController.swift
@@ -671,7 +671,7 @@ extension SettingsViewController
             
             return numberOfRows
             
-        case .multitasking: return MultitaskingRow.allCases.count
+        case .multitasking where !isSectionHidden(section): return MultitaskingRow.allCases.count
             
         case .syncing where !isSectionHidden(section): return SyncManager.shared.coordinator?.account == nil ? 1 : super.tableView(tableView, numberOfRowsInSection: sectionIndex)
         case .advanced where !isSectionHidden(section):