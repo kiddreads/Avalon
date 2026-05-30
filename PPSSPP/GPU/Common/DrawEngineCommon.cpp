MarkrPearce96/ppsspp

Implement minZ/maxZ through user clip planes

GPU/Common/DrawEngineCommon.cpp
@@ -316,7 +316,7 @@ bool DrawEngineCommon::TestBoundingBox(const void *vdata, const void *inds, int 
 	}
 
 	// Note: near/far are not checked without clamp/clip enabled, so we skip those planes.
-	int totalPlanes = gstate.isDepthClampEnabled() ? 6 : 4;
+	int totalPlanes = gstate.isDepthClipEnabled() ? 6 : 4;
 	for (int plane = 0; plane < totalPlanes; plane++) {
 		int inside = 0;
 		int out = 0;

kiddreads/Avalon

PPSSPP/GPU/Common/DrawEngineCommon.cpp
@@ -306,7 +306,7 @@ bool DrawEngineCommon::TestBoundingBox(const void *vdata, const void *inds, int 
 		}
 	}
 
-	int countToCheck = gstate.isDepthClampEnabled() ? 6 : 4;
+	int countToCheck = gstate.isDepthClipEnabled() ? 6 : 4;
 #if 0
 	// For debugging, the exclusive check. This should make it obvious where the culling borders are in screen space.
 	for (int i = 0; i < countToCheck; i++) {